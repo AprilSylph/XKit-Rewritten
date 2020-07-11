eb96230352e3808ce1366b2b371b04b6b940dea4
April Sylph <april@sylph.tech>
2020-07-11 16:02:35 +0100

    trim trailing whitespace

--- xkit.js
+++ xkit.js
@@ -3,10 +3,10 @@ const redpop = [...document.scripts].some({src} => src.match('/pop/'));
 
 async function run_script(name) {
   const { main, stylesheet } = await import(getURL(`/src/scripts/${name}.js`));
-  
+
   main()
   .catch(console.error);
-  
+
   if (stylesheet) {
     browser.tabs.insertCSS({file: stylesheet})
     .catch(console.error);
@@ -15,10 +15,10 @@ async function run_script(name) {
 
 async function destroy_script(name) {
   const { clean, stylesheet } = await import(getURL(`/src/scripts/${name}.js`));
-  
+
   clean()
   .catch(console.error);
-  
+
   if (stylesheet) {
     browser.tabs.removeCSS({file: stylesheet})
     .catch(console.error);
@@ -30,12 +30,12 @@ async function onStorageChanged(changes, areaName) {
   if (!enabledScripts || areaName !== 'local') {
     return;
   }
-  
+
   const {oldValue, newValue} = enabledScripts;
-  
+
   const newlyEnabled = newValue.filter(x => oldValue.includes(x) === false);
   const newlyDisabled = oldValue.filter(x => newValue.includes(x) === false);
-  
+
   newlyEnabled.forEach(run_script);
   newlyDisabled.forEach(destroy_script);
 }
@@ -47,7 +47,7 @@ async function init() {
   if (!enabledScripts) {
     return;
   }
-  
+
   enabledScripts.forEach(run_script);
 };
 

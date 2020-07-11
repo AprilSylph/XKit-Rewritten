0ac62a4a3c7819a01ef9e8ee29a4435afc15de3a
April Sylph <april@sylph.tech>
2020-07-11 16:23:47 +0100

    create dev/copy-libs.sh

---
+++ dev/copy-libs.sh
@@ -0,0 +1,4 @@
+cp node_modules/webextension-polyfill/dist/browser-polyfill.min.js lib/
+cp node_modules/jquery/dist/jquery.min.js lib/
+cp node_modules/moment/dist/moment.js lib/
+chmod +x lib/**.js

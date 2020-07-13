f437cc10e94dfda426940f5628ad8036bacb728c
April Sylph <april@sylph.tech>
2020-07-13 17:34:44 +0100

    fix example comment to not break file

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,6 +1,4 @@
-// this file is for reference only; to see it work, you must manually set enabledScripts in xkit.js/init()
-
-(function() {
+(function() { // this file is for reference only; to see it work, you must manually set enabledScripts in xkit.js/init()
   const hello = "world";
 
   const main = async function() {

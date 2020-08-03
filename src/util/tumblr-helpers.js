3cdb88af9d08a62bcf4bce3cdb3a0ffb5788442f
April Sylph <april@sylph.tech>
2020-08-03 20:02:25 +0100

    declare tumblr as global in apiFetch shortcut

--- src/util/tumblr-helpers.js
+++ src/util/tumblr-helpers.js
@@ -1,7 +1,7 @@
 (function() {
   const apiFetch = async function(...args) {
     const { inject } = await fakeImport('/src/util/inject.js');
-    return inject(
+    return inject( /* globals tumblr */
       async (pathname, queryParams, body) => await tumblr.apiFetch(pathname, queryParams, body),
       args,
     );

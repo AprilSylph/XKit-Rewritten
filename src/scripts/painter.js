128c05925b7246fbe6f58523fe088064811f4c90
invalidCards <842080+invalidCards@users.noreply.github.com>
2020-08-11 18:13:09 +0200

    Add pref. listener & use regex to validate colours

--- src/scripts/painter.js
+++ src/scripts/painter.js
@@ -4,6 +4,8 @@
   let reblogColour;
   let likedColour;
 
+  const colourRegex = /^[A-F0-9]{6}$/i;
+
   const paint = async function() {
     const { timelineObject } = await fakeImport('/src/util/react-props.js');
 
@@ -15,18 +17,18 @@
       const {canDelete, liked, rebloggedFromId} = await timelineObject(post_id);
 
       let coloursToApply = [];
-      if (canDelete && ownColour) {
+      if (canDelete && ownColour && colourRegex.test(ownColour)) {
         coloursToApply.push(ownColour);
       }
-      if (liked && likedColour) {
+      if (liked && likedColour && colourRegex.test(likedColour)) {
         coloursToApply.push(likedColour);
       }
       if (rebloggedFromId) {
-        if (reblogColour) {
+        if (reblogColour && colourRegex.test(reblogColour)) {
           coloursToApply.push(reblogColour);
         }
       } else {
-        if (likedColour) {
+        if (originalColour && colourRegex.test(originalColour)) {
           coloursToApply.push(originalColour);
         }
       }
@@ -37,8 +39,8 @@
 
       const step = 100/coloursToApply.length;
       let borderImage = 'linear-gradient(to right';
-      coloursToApply.forEach((colour, i) => 
-        borderImage += `, ${colour} ${step*i}% ${step*(i+1)}%`
+      coloursToApply.forEach((colour, i) =>
+        borderImage += `, #${colour} ${step*i}% ${step*(i+1)}%`
       );
       borderImage += ')';
 
@@ -49,7 +51,36 @@
     });
   }
 
+  const strip = function() {
+    $('.xkit_painter_painted article')
+    .css('border-top', '')
+    .css('border-image-source', '')
+    .css('border-image-slice', '');
+    $('.xkit_painter_painted').removeClass('xkit_painter_painted');
+  }
+
+  const fallback = function(value, fallback) {
+    return typeof value !== undefined ? value : fallback;
+  }
+
+  const onStorageChanged = function(changes, areaName) {
+    const {'painter.preferences': preferences} = changes;
+    if (!preferences || areaName !== 'local') {
+      return;
+    }
+
+    const {newValue} = preferences;
+    ownColour = fallback(newValue.own, ownColour);
+    originalColour = fallback(newValue.original, originalColour);
+    reblogColour = fallback(newValue.reblog, reblogColour);
+    likedColour = fallback(newValue.liked, likedColour);
+
+    strip();
+    paint();
+  }
+
   const main = async function() {
+    browser.storage.onChanged.addListener(onStorageChanged);
     const {'painter.preferences': preferences = {}} = await browser.storage.local.get('painter.preferences');
     ownColour = preferences.own;
     originalColour = preferences.original;
@@ -64,11 +95,7 @@
   const clean = async function() {
     const { postListener } = await fakeImport('/src/util/mutations.js');
     postListener.removeListener(paint);
-    $('.xkit_painter_painted article')
-    .css('border-top', '')
-    .css('border-image-source', '')
-    .css('border-image-slice', '');
-    $('.xkit_painter_painted').removeClass('xkit_painter_painted');
+    strip();
   }
 
   return { main, clean };

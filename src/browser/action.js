e82b1986b9358d9c7be7e6f11ae64cedddaae45c
April Sylph <april@sylph.tech>
2020-08-12 12:34:42 +0100

    Revert "create color-type preference"
    This reverts commit f166b38a064146462c784a06cc012906cd2bdaac.

--- src/browser/action.js
+++ src/browser/action.js
@@ -33,7 +33,6 @@ async function writePreference(event) {
         savedPreferences[preferenceName] = event.target.checked;
         break;
       case 'text':
-      case 'color':
         savedPreferences[preferenceName] = event.target.value;
         break;
     }
@@ -113,7 +112,6 @@ async function renderScripts() {
         const inputType = {
           checkbox: 'input',
           text: 'input',
-          color: 'input',
           select: 'select'
         }[preference.type];
 
@@ -139,11 +137,6 @@ async function renderScripts() {
             preferenceInput.value = savedPreference;
             preferenceListItem.appendChild(preferenceLabel);
             preferenceListItem.appendChild(preferenceInput);
-            break;
-          case 'color':
-            preferenceInput.value = savedPreference;
-            preferenceListItem.appendChild(preferenceInput);
-            preferenceListItem.appendChild(preferenceLabel);
             break;
           case 'select':
             for (const [value, text] of Object.entries(preference.options)) {

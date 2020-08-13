2c64b54087e139ed12ed94fdef736024e4fd28ab
April Sylph <april@sylph.tech>
2020-08-13 17:03:47 +0100

    correct apiFetch arguments

--- src/util/tumblr-helpers.js
+++ src/util/tumblr-helpers.js
@@ -6,7 +6,7 @@
   const apiFetch = async function(...args) {
     const { inject } = await fakeImport('/src/util/inject.js');
     return inject(
-      async (pathname, queryParams, body) => await window.tumblr.apiFetch(pathname, queryParams, body),
+      async (resource, init) => await window.tumblr.apiFetch(resource, init),
       args,
     );
   }

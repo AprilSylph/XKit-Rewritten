9dd803a4128819bd7d651e21017877cb34d5e8d0
April Sylph <april@sylph.tech>
2020-08-18 16:07:17 +0100

    new tweak: Hide Tumblr Radar

---
+++ src/tweaks/hide_radar.js
@@ -0,0 +1,37 @@
+(function() {
+  let radarLabel;
+
+  const css = '.xkit-tweaks-radar-hidden { display: none; }';
+
+  const checkForRadar = function() {
+    [...document.querySelectorAll('aside > div > h1:not(.xkit-tweaks-radar-processed)')]
+    .filter(h1 => {
+      h1.classList.add('xkit-tweaks-radar-processed');
+      return h1.textContent === radarLabel;
+    })
+    .forEach(h1 => h1.parentNode.classList.add('xkit-tweaks-radar-hidden'));
+  };
+
+  const main = async function() {
+    const { baseContainerListener } = await fakeImport('/src/util/mutations.js');
+    const { translate } = await fakeImport('/src/util/language-data.js');
+    const { addStyle } = await fakeImport('/src/util/misc.js');
+
+    radarLabel = await translate('Radar');
+    baseContainerListener.addListener(checkForRadar);
+    checkForRadar();
+    addStyle(css);
+  };
+
+  const clean = async function() {
+    const { baseContainerListener } = await fakeImport('/src/util/mutations.js');
+    const { removeStyle } = await fakeImport('/src/util/misc.js');
+
+    baseContainerListener.removeListener(checkForRadar);
+    removeStyle(css);
+    $('.xkit-tweaks-radar-processed').removeClass('xkit-tweaks-radar-processed');
+    $('.xkit-tweaks-radar-hidden').removeClass('xkit-tweaks-radar-hidden');
+  };
+
+  return { main, clean };
+})();

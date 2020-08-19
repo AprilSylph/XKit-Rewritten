f2fea6f014c476877cb7099f10c79bf25ae9e659
April Sylph <april@sylph.tech>
2020-08-19 15:40:15 +0100

    new tweak: more subtle mini-follow buttons in posts

---
+++ src/scripts/tweaks/subtle_follow.js
@@ -0,0 +1,19 @@
+(function() {
+  let css;
+
+  const run = async function() {
+    const { descendantSelector } = await fakeImport('/src/util/css-map.js');
+    const { addStyle } = await fakeImport('/src/util/misc.js');
+
+    const selector = await descendantSelector('post', 'followButton');
+    css = `${selector} { color: var(--gray-40); }`;
+    addStyle(css);
+  };
+
+  const destroy = async function() {
+    const { removeStyle } = await fakeImport('/src/util/misc.js');
+    removeStyle(css);
+  };
+
+  return { run, destroy };
+})();

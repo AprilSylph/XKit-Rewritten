6b87d0cb4adb19a656e804f7c65974e3466ec331
invalidCards <842080+invalidCards@users.noreply.github.com>
2020-08-21 16:10:44 +0100

    Apply suggestions from code review
    Co-authored-by: April Sylph <april@sylph.tech>

--- src/scripts/quick_queue.js
+++ src/scripts/quick_queue.js
@@ -1,14 +1,14 @@
 (function() {
   let controlsSelector;
   const queuePath = '<path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-8h4v2h-6V7h2v5z"/>';
-  const crossPath = '<path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"/>';
+  const errorCrossPath = '<path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"/>';
   const loadPath = '<path d="M5.463 4.433A9.961 9.961 0 0 1 12 2c5.523 0 10 4.477 10 10 0 2.136-.67 4.116-1.81 5.74L17 12h3A8 8 0 0 0 6.46 6.228l-.997-1.795zm13.074 15.134A9.961 9.961 0 0 1 12 22C6.477 22 2 17.523 2 12c0-2.136.67-4.116 1.81-5.74L7 12H4a8 8 0 0 0 13.54 5.772l.997 1.795z"/>';
   const draftPath = '<path d="M21 8v12.993A1 1 0 0 1 20.007 22H3.993A.993.993 0 0 1 3 21.008V2.992C3 2.455 3.449 2 4.002 2h10.995L21 8zm-2 1h-5V4H5v16h14V9z"/>';
 
   const quickQueue = async function(event) {
     const postElement = event.target.closest('div[data-id]');
     if (!postElement) {
-      event.target.innerHTML = crossPath;
+      event.target.innerHTML = errorCrossPath;
       event.target.setAttribute('fill', 'var(--red)');
       return;
     }
@@ -36,7 +36,7 @@
         event.target.innerHTML = queuePath;
         event.target.setAttribute('fill', 'var(--green)');
       } else {
-        event.target.innerHTML = crossPath;
+        event.target.innerHTML = errorCrossPath;
         event.target.setAttribute('fill', 'var(--red)');
       }
     } catch (e) {
@@ -55,16 +55,16 @@
             event.target.innerHTML = draftPath;
             event.target.setAttribute('fill', 'var(--green)');
           } else {
-            event.target.innerHTML = crossPath;
+            event.target.innerHTML = errorCrossPath;
             event.target.setAttribute('fill', 'var(--red)');
           }
         } catch (err) {
-          event.target.innerHTML = crossPath;
+          event.target.innerHTML = errorCrossPath;
           event.target.setAttribute('fill', 'var(--red)');
           console.error(err.body);
         }
       } else {
-        event.target.innerHTML = crossPath;
+        event.target.innerHTML = errorCrossPath;
         event.target.setAttribute('fill', 'var(--red)');
         console.error(e.body);
       }

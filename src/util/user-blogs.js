5fe92f75a988481127f5c065a530350d2138590e
invalidCards <842080+invalidCards@users.noreply.github.com>
2020-08-31 17:50:50 +0100

    Add user blogs utility (#6)

---
+++ src/util/user-blogs.js
@@ -0,0 +1,36 @@
+(function() {
+  let userBlogs;
+
+  /**
+   * @return {Object[]} - An array of blog objects the current user has post access to
+   */
+  const fetchUserBlogs = async function() {
+    if (!userBlogs) {
+      const { apiFetch } = await fakeImport('/src/util/tumblr-helpers.js');
+      const response = await apiFetch('/v2/user/info');
+      if (response.meta.status === 200) {
+        userBlogs = response.response.user.blogs;
+      }
+    }
+
+    return userBlogs;
+  };
+
+  /**
+   * @return {String[]} - An array of blog names the current user has post access to
+   */
+  const fetchUserBlogNames = async function() {
+    const blogs = await fetchUserBlogs();
+    return blogs.map(blog => blog.name);
+  };
+
+  /**
+   * @return {Object} - The default ("main") blog for the user
+   */
+  const fetchDefaultBlog = async function() {
+    const blogs = await fetchUserBlogs();
+    return blogs.find(blog => blog.primary === true);
+  };
+
+  return { fetchUserBlogs, fetchUserBlogNames, fetchDefaultBlog };
+})();

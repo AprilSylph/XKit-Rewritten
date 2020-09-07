3e60601690037be2264b5ac07484215120349996
April Sylph <april@sylph.tech>
2020-09-07 17:39:39 +0100

    build helper for getting preference values

---
+++ src/util/preferences.js
@@ -0,0 +1,21 @@
+(function() {
+  const getPreferences = async function(scriptName) {
+    const scriptManifestURL = browser.runtime.getURL(`/src/scripts/${scriptName}.json`);
+    const scriptManifestFile = await fetch(scriptManifestURL);
+    const scriptManifest = await scriptManifestFile.json();
+
+    const {preferences = {}} = scriptManifest;
+    const preferenceValues = {};
+
+    for (const [key, preference] of Object.entries(preferences)) {
+      const storageKey = `${scriptName}.preferences.${key}`;
+      const {[storageKey]: savedPreference} = await browser.storage.local.get(storageKey);
+
+      preferenceValues[key] = savedPreference === undefined ? preference.default : savedPreference;
+    }
+
+    return preferenceValues;
+  };
+
+  return { getPreferences };
+})();

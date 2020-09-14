35b64700e4b94286076f30adf9e703e20a214e15
April Sylph <april@sylph.tech>
2020-09-14 21:05:09 +0100

    fakeImport: prevent modules from being evaluated more than once
    since fakeImport is already an async function, returning promises makes 
    its usage syntax no different, while allowing fakeImport to set 
    modules[path] to a usable value as soon as the path is requested for the 
    first time
    
    Co-Authored-By: marcustyphoon <marcustyphoon@gmail.com>

--- src/fake_import.js
+++ src/fake_import.js
@@ -4,15 +4,11 @@
 {
   const modules = {};
   fakeImport = async function (path) { // eslint-disable-line no-global-assign
-    if (!Object.prototype.hasOwnProperty.call(modules, path)) {
+    return modules[path] || (modules[path] = (async () => {
       const url = browser.runtime.getURL(path);
       const file = await fetch(url);
       const fakeModule = await file.text();
-      const returnedFakeModule = new Function(`'use strict'; return ${fakeModule} //# sourceURL=${url}`)();
-
-      modules[path] = modules[path] || returnedFakeModule;
-    }
-
-    return modules[path];
+      return new Function(`'use strict'; return ${fakeModule} //# sourceURL=${url}`)();
+    })());
   };
 }

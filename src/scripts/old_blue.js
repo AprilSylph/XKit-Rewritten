5ed6544f1f356d8f67750394d40af4bf227af169
April Sylph <april@sylph.tech>
2020-09-16 14:05:50 +0100

    new script: Old Blue
    NOTE: this is not intended to be a permanent addition to the gallery; it 
    would be best suited to a different addon which handles Tumblr palettes 
    more extensively

---
+++ src/scripts/old_blue.js
@@ -0,0 +1,11 @@
+(function () {
+  const main = async function () {
+    // Pure userstyle; nothing to do
+  };
+
+  const clean = async function () {
+    // Pure userstyle; nothing to undo
+  };
+
+  return { main, clean, stylesheet: true };
+})();

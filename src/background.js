416215b54b6c8d1b980f8fd8d0802ee2d1bc4814
April Sylph <april@sylph.tech>
2020-09-25 17:08:15 +0100

    background script: set every preference explicitly in storage
    this fixes an issue where modifying a preference from default and then 
    downloading from storage.sync would not revert that modification
    
    util/preferences.js will still fall back to script manifest defaults for 
    robustness

--- src/background.js
+++ src/background.js
@@ -8,3 +8,26 @@ browser.storage.onChanged.addListener((changes, areaName) => {
 
   browser.storage.local.set({ storageLastModified });
 });
+
+(async function () {
+  const { getURL } = browser.runtime;
+
+  const indexUrl = getURL('/src/scripts/_index.json');
+  const indexFile = await fetch(indexUrl);
+  const installedScripts = await indexFile.json();
+
+  for (const scriptName of installedScripts) {
+    const url = getURL(`/src/scripts/${scriptName}.json`);
+    const file = await fetch(url);
+    const { preferences = {} } = await file.json();
+
+    for (const [key, preference] of Object.entries(preferences)) {
+      const storageKey = `${scriptName}.preferences.${key}`;
+      const { [storageKey]: savedPreference } = await browser.storage.local.get(storageKey);
+
+      if (savedPreference === undefined) {
+        browser.storage.local.set({ [storageKey]: preference.default });
+      }
+    }
+  }
+})();

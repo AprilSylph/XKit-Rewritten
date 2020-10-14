a2a2a69bb6d8760d88fdbe806edbeabcb0f5e610
April Sylph <april@sylph.tech>
2020-10-14 18:19:13 +0100

    finish sidebar section framework
    - add support for click event handlers
    - fix count class not being added
    - create internal sidebar element
    - automatically add new sidebar sections to sidebar element, don't 
    return them
    - automatically add sidebar element to page upon base-container 
    mutations
    - automatically add in-channel class to sidebar element when blog stats 
    are detected
    - add method for removing previously-added sidebar sections
    - rename createSidebarItem to addSidebarItem
    - add jsdoc to addSidebarItem

--- src/util/sidebar.js
+++ src/util/sidebar.js
@@ -1,5 +1,19 @@
 (function () {
-  const createSidebarItem = function ({ id, title, items }) {
+  const sidebarItems = Object.assign(document.createElement('div'), { id: 'xkit-sidebar' });
+
+  /* eslint-disable jsdoc/check-param-names */
+  /**
+   * @param {object} options - Destructured, not used directly
+   * @param {string} options.id - Unique ID for the sidebar section
+   * @param {string} options.title - Human-readable sidebar section heading
+   * @param {object[]} options.items - Array of item objects to construct clickable links in the sidebar section
+   * @param {string} options.items.label - Human-readable link text
+   * @param {string} [options.items.href] - Link address for this item
+   * @param {Function} [options.items.onClick] - Click event handler for this item (ignored if href is specified)
+   * @param {string} [options.items.count] - Human-readable additional link text
+   * @param {boolean} [options.items.carrot] - Whether to include a right-facing arrow on the link (ignored if count is specified)
+   */
+  const addSidebarItem = function ({ id, title, items }) {
     const sidebarItem = document.createElement('div');
     sidebarItem.classList.add('xkit-sidebar-item');
     sidebarItem.id = id;
@@ -17,7 +31,15 @@
       sidebarList.appendChild(sidebarListItem);
 
       const link = document.createElement('a');
-      link.href = item.href || '#';
+      if (item.href) {
+        link.href = item.href;
+      } else if (item.onClick instanceof Function) {
+        link.href = '#';
+        link.addEventListener('click', event => {
+          event.preventDefault();
+          item.onClick(event);
+        });
+      }
       sidebarListItem.appendChild(link);
 
       const label = document.createElement('span');
@@ -26,6 +48,7 @@
 
       if (item.count !== undefined) {
         const count = document.createElement('span');
+        count.classList.add('count');
         count.textContent = item.count;
         link.appendChild(count);
       } else if (item.carrot === true) {
@@ -41,8 +64,32 @@
       }
     }
 
-    return sidebarItem;
+    sidebarItems.appendChild(sidebarItem);
   };
 
-  return { createSidebarItem };
+  const removeSidebarItem = id => sidebarItems.removeChild(sidebarItems.querySelector(`#${id}`));
+
+  (async function () {
+    const { onBaseContainerMutated } = await fakeImport('/src/util/mutations.js');
+
+    const addSidebarToPage = () => {
+      const aside = document.querySelector('aside');
+      const target = aside.children[1] || null;
+
+      if (aside.querySelector('#xkit-sidebar') === null) {
+        aside.insertBefore(sidebarItems, target);
+      }
+
+      if (aside.querySelector(':scope > div > aside') !== null) {
+        sidebarItems.classList.add('in-channel');
+      } else {
+        sidebarItems.classList.remove('in-channel');
+      }
+    };
+
+    onBaseContainerMutated.addListener(addSidebarToPage);
+    addSidebarToPage();
+  })();
+
+  return { addSidebarItem, removeSidebarItem };
 })();

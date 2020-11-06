c47c3048b54074d526cc3482198b6d50088cb560
April Sylph <april@sylph.tech>
2020-11-06 17:12:21 +0000

    new Tweak: Highlight contributed content on reblogs

---
+++ src/scripts/tweaks/highlight_contributed_content.js
@@ -0,0 +1,15 @@
+(function () {
+  const css = '[data-is-contributed-content="true"] { border-bottom: 1px solid transparent; background-color: var(--follow); }';
+
+  const main = async function () {
+    const { addStyle } = await fakeImport('/util/interface.js');
+    addStyle(css);
+  };
+
+  const clean = async function () {
+    const { removeStyle } = await fakeImport('/util/interface.js');
+    removeStyle(css);
+  };
+
+  return { main, clean };
+})();

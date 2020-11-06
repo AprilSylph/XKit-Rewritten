04d017b2026f3bd0224cc5fb4fa16e02f1a776b0
April Sylph <april@sylph.tech>
2020-11-06 18:37:10 +0000

    AccessKit: add option to remove NPF fonts

---
+++ src/scripts/accesskit/no_user_fonts.js
@@ -0,0 +1,21 @@
+(function () {
+  let css;
+  const main = async function () {
+    const { descendantSelector } = await fakeImport('/util/css_map.js');
+    const { addStyle } = await fakeImport('/util/interface.js');
+
+    const quoteSelector = await descendantSelector('textBlock', 'quote');
+    const chatSelector = await descendantSelector('textBlock', 'chat');
+    const quirkySelector = await descendantSelector('textBlock', 'quirky');
+
+    css = `${quoteSelector}, ${chatSelector}, ${quirkySelector} { font-family: var(--font-family); }`;
+    addStyle(css);
+  };
+
+  const clean = async function () {
+    const { removeStyle } = await fakeImport('/util/interface.js');
+    removeStyle(css);
+  };
+
+  return { main, clean };
+})();

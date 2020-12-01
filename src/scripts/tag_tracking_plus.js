3caab6db90f50171eaf180636782ffee6b6e7685
April Sylph <april@sylph.tech>
2020-12-01 13:31:13 +0000

    Tag Tracking+: limit search API call to 20 posts
    fixes unread counts not appearing; limit now appears to be a required 
    field for this endpoint

--- src/scripts/tag_tracking_plus.js
+++ src/scripts/tag_tracking_plus.js
@@ -64,7 +64,7 @@
         const sidebarCount = sidebarItemElement.querySelector(`a[href="/tagged/${encodeURIComponent(name)}"] .count`);
         if (sidebarCount.textContent.includes('+')) { return; }
 
-        const { response: { posts: { data, links } } } = await apiFetch('/v2/mobile/search/posts', { queryParams: { mode: 'recent', query: name } });
+        const { response: { posts: { data, links } } } = await apiFetch('/v2/mobile/search/posts', { queryParams: { limit: 20, mode: 'recent', query: name } });
         let unreadCount = 0;
 
         for (const post of data) {

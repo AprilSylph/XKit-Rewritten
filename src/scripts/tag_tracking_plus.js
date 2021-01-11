c7a7e884285523da6e652947db22233bc2f7fcc4
April Sylph <april@sylph.tech>
2021-01-11 11:53:09 +0000

    Tag Tracking+: overhaul script, put unread counts on search dropdown

--- src/scripts/tag_tracking_plus.js
+++ src/scripts/tag_tracking_plus.js
@@ -1,8 +1,10 @@
 (function () {
   const storageKey = 'tag_tracking_plus.trackedTagTimestamps';
   const excludeClass = 'xkit-tag-tracking-plus-done';
-  let sidebarItemElement;
-  let intervalID;
+
+  let searchResultSelector;
+  let tagTextSelector;
+  let tagsYouFollowString;
 
   const processPosts = async function () {
     const { searchParams } = new URL(location);
@@ -16,9 +18,7 @@
 
     const currentTag = decodeURIComponent(location.pathname.split('/')[2].replace(/\+/g, ' '));
     const { response: { following } } = await apiFetch('/v2/user/tags/following', { queryParams: { tag: currentTag } });
-    if (!following) {
-      return;
-    }
+    if (!following) { return; }
 
     const { [storageKey]: timestamps = {} } = await browser.storage.local.get(storageKey);
     for (const postElement of getPostElements({ excludeClass, noPeepr: true, includeFiltered: true })) {
@@ -29,75 +29,77 @@
         timestamps[currentTag] = timestamp;
       }
     }
-    await browser.storage.local.set({ [storageKey]: timestamps });
+    browser.storage.local.set({ [storageKey]: timestamps });
+  };
 
-    const sidebarCount = sidebarItemElement.querySelector(`a[href="/tagged/${encodeURIComponent(currentTag)}"] .count`);
-    if (sidebarCount !== null) {
-      sidebarCount.textContent = '';
-    }
+  const processTagLinks = async function () {
+    const searchResultElement = document.querySelector(searchResultSelector);
+    if (!searchResultElement || searchResultElement.classList.contains(excludeClass)) { return; }
+
+    searchResultElement.classList.add(excludeClass);
+    const { [storageKey]: timestamps = {} } = await browser.storage.local.get(storageKey);
+
+    const tagsYouFollowHeading = [...searchResultElement.querySelectorAll('h3')].find(h3 => h3.textContent === tagsYouFollowString);
+    if (!tagsYouFollowHeading) { return; }
+    tagsYouFollowHeading.dataset.followedTags = true;
+
+    const tagLinkElements = searchResultElement.querySelectorAll('[data-followed-tags] ~ [href^="/tagged/"]');
+    if (!tagLinkElements) { return; }
+
+    const { apiFetch } = await fakeImport('/util/tumblr_helpers.js');
+
+    tagLinkElements.forEach(async tagLinkElement => {
+      const tag = tagLinkElement.querySelector(tagTextSelector).textContent;
+      const savedTimestamp = timestamps[tag] || 0;
+
+      const { response: { timeline: { elements } } } = await apiFetch(`/v2/hubs/${tag}/timeline`, { queryParams: { limit: 20, sort: 'recent' } });
+      let unreadCount = 0;
+
+      for (const post of elements) {
+        const { timestamp } = post;
+        if (timestamp <= savedTimestamp) {
+          break;
+        } else {
+          unreadCount++;
+        }
+      }
+
+      if (unreadCount === 0) {
+        unreadCount = '';
+      } else if (unreadCount === elements.length) {
+        unreadCount += '+';
+      }
+
+      const unreadCountElement = Object.assign(document.createElement('span'), {
+        style: 'margin-left: auto; margin-right: 1ch; opacity: 0.65;',
+        textContent: `${unreadCount}`,
+      });
+
+      tagLinkElement.firstElementChild.appendChild(unreadCountElement);
+    });
   };
 
   const main = async function () {
-    const { apiFetch } = await fakeImport('/util/tumblr_helpers.js');
-    const { addSidebarItem } = await fakeImport('/util/sidebar.js');
-    const { onNewPosts } = await fakeImport('/util/mutations.js');
+    const { keyToCss } = await fakeImport('/util/css_map.js');
+    const { onNewPosts, onBaseContainerMutated } = await fakeImport('/util/mutations.js');
+    const { translate } = await fakeImport('/util/language_data.js');
 
-    const { response: { tags } } = await apiFetch('/v2/user/tags');
-
-    const sidebarItem = {
-      id: 'tag-tracking-plus',
-      title: 'Tracked Tags',
-      items: tags.map(({ name }) => ({ label: name, href: `/tagged/${encodeURIComponent(name)}`, count: '' })),
-    };
-
-    sidebarItemElement = addSidebarItem(sidebarItem);
+    searchResultSelector = await keyToCss('searchResult');
+    tagTextSelector = await keyToCss('tagText');
+    tagsYouFollowString = await translate('Tags you follow');
 
     onNewPosts.addListener(processPosts);
-    await processPosts();
+    processPosts();
 
-    const updateUnreadCounts = async () => {
-      const { [storageKey]: timestamps = {} } = await browser.storage.local.get(storageKey);
-      tags.forEach(async ({ name }) => {
-        if (!timestamps[name]) {
-          timestamps[name] = 0;
-        }
-
-        const sidebarCount = sidebarItemElement.querySelector(`a[href="/tagged/${encodeURIComponent(name)}"] .count`);
-        if (sidebarCount.textContent.includes('+')) { return; }
-
-        const { response: { timeline: { elements } } } = await apiFetch(`/v2/hubs/${name}/timeline`, { queryParams: { limit: 20, sort: 'recent' } });
-        let unreadCount = 0;
-
-        for (const post of elements) {
-          const { timestamp } = post;
-          if (timestamp <= timestamps[name]) {
-            break;
-          } else {
-            unreadCount++;
-          }
-        }
-
-        if (unreadCount === 0) {
-          unreadCount = '';
-        } else if (unreadCount === elements.length) {
-          unreadCount += '+';
-        }
-
-        sidebarCount.textContent = unreadCount;
-      });
-    };
-
-    intervalID = setInterval(updateUnreadCounts, 600000);
-    updateUnreadCounts();
+    onBaseContainerMutated.addListener(processTagLinks);
+    processTagLinks();
   };
 
   const clean = async function () {
-    const { removeSidebarItem } = await fakeImport('/util/sidebar.js');
-    const { onNewPosts } = await fakeImport('/util/mutations.js');
+    const { onNewPosts, onBaseContainerMutated } = await fakeImport('/util/mutations.js');
 
-    removeSidebarItem('tag-tracking-plus');
     onNewPosts.removeListener(processPosts);
-    clearInterval(intervalID);
+    onBaseContainerMutated.removeListener(processTagLinks);
   };
 
   return { main, clean };

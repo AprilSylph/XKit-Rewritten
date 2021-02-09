e33110b39979fb04c4aa211e6ab747b4fc489676
April Sylph <april@sylph.tech>
2021-02-09 19:37:19 +0000

    Seen Posts: add option to only dim post avatars

--- src/scripts/seen_posts.js
+++ src/scripts/seen_posts.js
@@ -1,6 +1,7 @@
 (function () {
   const excludeClass = 'xkit-seen-posts-done';
   const dimClass = 'xkit-seen-posts-seen';
+  const onlyDimAvatarsClass = 'xkit-seen-posts-only-dim-avatar';
 
   const dimPosts = async function () {
     const storageKey = 'seen_posts.seenPosts';
@@ -25,19 +26,42 @@
     browser.storage.local.set({ [storageKey]: seenPosts });
   };
 
+  const onStorageChanged = async function (changes, areaName) {
+    if (areaName !== 'local') {
+      return;
+    }
+
+    const { 'seen_posts.preferences.onlyDimAvatars': onlyDimAvatarsChanges } = changes;
+
+    if (onlyDimAvatarsChanges) {
+      const { newValue: onlyDimAvatars } = onlyDimAvatarsChanges;
+      const addOrRemove = onlyDimAvatars ? 'add' : 'remove';
+      document.body.classList[addOrRemove](onlyDimAvatarsClass);
+    }
+  };
+
   const main = async function () {
+    browser.storage.onChanged.addListener(onStorageChanged);
+    const { getPreferences } = await fakeImport('/util/preferences.js');
     const { onNewPosts } = await fakeImport('/util/mutations.js');
+
+    const { onlyDimAvatars } = await getPreferences('seen_posts');
+    if (onlyDimAvatars) {
+      document.body.classList.add(onlyDimAvatarsClass);
+    }
 
     onNewPosts.addListener(dimPosts);
     dimPosts();
   };
 
   const clean = async function () {
+    browser.storage.onChanged.removeListener(onStorageChanged);
     const { onNewPosts } = await fakeImport('/util/mutations.js');
 
     onNewPosts.removeListener(dimPosts);
     $(`.${excludeClass}`).removeClass(excludeClass);
     $(`.${dimClass}`).removeClass(dimClass);
+    $(`.${onlyDimAvatarsClass}`).removeClass(onlyDimAvatarsClass);
   };
 
   return { main, clean, stylesheet: true };

33bc19ea59a8a43387100fc2d4579c3086947c32
April Sylph <april@sylph.tech>
2021-03-01 09:51:40 +0000

    AccessKit: update usage of palette variables in carousel option
    fixes #85

--- src/scripts/accesskit/boring_tag_chiclets.js
+++ src/scripts/accesskit/boring_tag_chiclets.js
@@ -6,7 +6,7 @@
     const { addStyle } = await fakeImport('/util/interface.js');
 
     const tagChicletWrapperSelector = await keyToCss('tagChicletWrapper');
-    css = `${tagChicletWrapperSelector} { background-image: none !important; color: var(--black); background-color: var(--secondary-accent); }`;
+    css = `${tagChicletWrapperSelector} { background-image: none !important; color: rgb(var(--black)); background-color: rgb(var(--secondary-accent)); }`;
 
     addStyle(css);
   };

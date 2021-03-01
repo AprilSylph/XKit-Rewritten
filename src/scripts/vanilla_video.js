6c97ca1ef64a67ce8d377da8aac1e6a3e8ed4ef2
April Sylph <april@sylph.tech>
2021-03-01 10:10:25 +0000

    Vanilla Video: rewrite script, prevent autoplay outright

--- src/scripts/vanilla_video.js
+++ src/scripts/vanilla_video.js
@@ -1,41 +1,38 @@
 (function () {
-  const addVideoControls = async function () {
-    [...document.querySelectorAll('video:not([controls])')]
-      .forEach(video => {
-        video.controls = true;
-        video.loop = false;
-        video.autoplay = false;
-        video.pause();
-        video.currentTime = 0;
-      });
-  };
+  const excludeClass = 'xkit-vanilla-video-done';
+  const videoClass = 'xkit-vanilla-video-player';
 
-  const stopVideoAutoplay = async function () {
-    [...document.querySelectorAll('video[controls][autoplay]')]
-      .forEach(video => {
-        video.autoplay = false;
-        video.pause();
-        video.currentTime = 0;
+  const cloneVideoElements = async function () {
+    [...document.querySelectorAll(`video:not(.${excludeClass})`)].forEach(async videoElement => {
+      videoElement.classList.add(excludeClass);
+
+      const newVideoElement = Object.assign(document.createElement('video'), {
+        controls: true,
+        crossorigin: videoElement.crossorigin,
+        playsinline: true,
+        poster: videoElement.poster,
+        src: videoElement.currentSrc,
+        style: videoElement.style,
+        className: videoClass
       });
+
+      videoElement.parentNode.parentNode.prepend(newVideoElement);
+    });
   };
 
   const main = async function () {
-    const { onNewPosts, onPostsMutated } = await fakeImport('/util/mutations.js');
+    const { onNewPosts } = await fakeImport('/util/mutations.js');
 
-    onNewPosts.addListener(addVideoControls);
-    onPostsMutated.addListener(stopVideoAutoplay);
-
-    addVideoControls();
+    onNewPosts.addListener(cloneVideoElements);
+    cloneVideoElements();
   };
 
   const clean = async function () {
-    const { onNewPosts, onPostsMutated } = await fakeImport('/util/mutations.js');
+    const { onNewPosts } = await fakeImport('/util/mutations.js');
 
-    onNewPosts.removeListener(addVideoControls);
-    onPostsMutated.removeListener(stopVideoAutoplay);
-
-    [...document.querySelectorAll('video[controls]')]
-      .forEach(video => { video.controls = false; });
+    onNewPosts.removeListener(cloneVideoElements);
+    $(`.${videoClass}`).remove();
+    $(`.${excludeClass}`).removeClass(excludeClass);
   };
 
   return { main, clean, stylesheet: true };

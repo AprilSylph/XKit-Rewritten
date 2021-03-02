2d14558a2ca4e5742ec98364c104a40c2629a765
April Sylph <april@sylph.tech>
2021-03-02 15:58:02 +0000

    release notes dev script: print commits in chronological order

--- dev/release-notes.sh
+++ dev/release-notes.sh
@@ -1,2 +1,2 @@
 GITHUB_REPOSITORY="AprilSylph/XKit-Rewritten"
-git log $(git describe --tags --abbrev=0)..HEAD --pretty --format="- [\`%h\`](https://github.com/$GITHUB_REPOSITORY/commit/%H) %s (%an)" --follow src/ > release-notes.md
+git log $(git describe --tags --abbrev=0)..HEAD --reverse --pretty --format="- [\`%h\`](https://github.com/$GITHUB_REPOSITORY/commit/%H) %s (%an)" --follow src/ > release-notes.md

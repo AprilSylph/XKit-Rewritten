d25fba3d6aeb60c88934657797ce9c2a23aa7982
April Sylph <april@sylph.tech>
2021-05-20 10:13:49 +0100

    background, manifest: de-implement browser.notifications

--- src/background.js
+++ src/background.js
@@ -1,16 +1,3 @@
-const onMessageCommands = {
-  'notifications:create': ({ id, options = {} }) => browser.notifications.create(id, options),
-  'notifications:clear': ({ id }) => browser.notifications.clear(id)
-};
-
-const onMessageCommandList = Object.keys(onMessageCommands);
-
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (onMessageCommandList.includes(message.command)) {
-    onMessageCommands[message.command](message.arguments);
-  }
-});
-
 browser.storage.onChanged.addListener((changes, areaName) => {
   if (areaName !== 'local' || Object.keys(changes).includes('storageLastModified')) {
     return;

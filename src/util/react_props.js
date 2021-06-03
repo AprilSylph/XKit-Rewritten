42917e4d1609f9b8dedcc7b0d3762d5bb3298ba4
April Sylph <april@sylph.tech>
2021-06-03 12:36:23 +0100

    react props utils: remove givenPath function

--- src/util/react_props.js
+++ src/util/react_props.js
@@ -58,35 +58,3 @@ export const exposeTimelines = async () => inject(async () => {
     timelineElement.dataset.timeline = givenPath;
   });
 });
-
-/**
- * @param {object} postElement - A post's HTMLDivElement
- * @returns {string} That post's buried endpointApiRequest.givenPath property
- */
-export const givenPath = async function (postElement) {
-  if (postElement.parentNode.dataset.timeline !== undefined) {
-    return postElement.parentNode.dataset.timeline;
-  }
-
-  const xkitTempId = `${new Date().getTime()}${Math.random()}`;
-  Object.assign(postElement.dataset, { xkitTempId });
-
-  return inject(async tempId => {
-    const localPostElement = document.querySelector(`[data-xkit-temp-id="${tempId}"]`);
-    delete localPostElement.dataset.xkitTempId;
-
-    const reactKey = Object.keys(localPostElement).find(key => key.startsWith('__reactInternalInstance'));
-    let fiber = localPostElement[reactKey];
-    let tries = 0;
-
-    while (fiber.memoizedProps.endpointApiRequest === undefined && tries <= 20) {
-      fiber = fiber.return;
-      tries++;
-    }
-
-    const { givenPath } = fiber.memoizedProps.endpointApiRequest;
-
-    localPostElement.parentNode.dataset.timeline = givenPath;
-    return givenPath;
-  }, [xkitTempId]);
-};

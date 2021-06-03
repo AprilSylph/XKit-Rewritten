ee654b4af276026d11bc09a60801c015d2ec0b93
April Sylph <april@sylph.tech>
2021-06-03 12:32:53 +0100

    Show Originals: use exposeTimelines + timeline filter

--- src/scripts/show_originals.js
+++ src/scripts/show_originals.js
@@ -1,5 +1,5 @@
 import { getPostElements } from '../util/interface.js';
-import { timelineObjectMemoized, givenPath } from '../util/react_props.js';
+import { timelineObjectMemoized, exposeTimelines } from '../util/react_props.js';
 import { getPreferences } from '../util/preferences.js';
 import { onNewPosts } from '../util/mutations.js';
 
@@ -13,27 +13,15 @@ let whitelistedUsernames;
 const processPosts = async function () {
   const whitelist = whitelistedUsernames.split(',').map(username => username.trim());
 
-  getPostElements({ excludeClass, includeFiltered: true }).forEach(async postElement => {
-    const timeline = await givenPath(postElement);
-    if (timeline !== '/v2/timeline/dashboard') { return; }
+  await exposeTimelines();
 
+  getPostElements({ excludeClass, timeline: /\/v2\/timeline\/dashboard/, includeFiltered: true }).forEach(async postElement => {
     const { rebloggedRootId, canEdit, content, blogName } = await timelineObjectMemoized(postElement.dataset.id);
 
-    if (!rebloggedRootId) {
-      return;
-    }
-
-    if (showOwnReblogs && canEdit) {
-      return;
-    }
-
-    if (showReblogsWithContributedContent && content.length > 0) {
-      return;
-    }
-
-    if (whitelist.includes(blogName)) {
-      return;
-    }
+    if (!rebloggedRootId) { return; }
+    if (showOwnReblogs && canEdit) { return; }
+    if (showReblogsWithContributedContent && content.length > 0) { return; }
+    if (whitelist.includes(blogName)) { return; }
 
     postElement.classList.add(hiddenClass);
   });

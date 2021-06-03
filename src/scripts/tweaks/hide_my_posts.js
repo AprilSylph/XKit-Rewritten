4635430ec85f0b31ea78802d3b233aa43f7ae2a8
April Sylph <april@sylph.tech>
2021-06-03 12:35:44 +0100

    Hide own posts tweak: use exposeTimelines + timeline filter

--- src/scripts/tweaks/hide_my_posts.js
+++ src/scripts/tweaks/hide_my_posts.js
@@ -1,7 +1,7 @@
 import { fetchDefaultBlog } from '../../util/user_blogs.js';
 import { onNewPosts } from '../../util/mutations.js';
 import { addStyle, removeStyle, getPostElements } from '../../util/interface.js';
-import { givenPath, timelineObjectMemoized } from '../../util/react_props.js';
+import { exposeTimelines, timelineObjectMemoized } from '../../util/react_props.js';
 
 const excludeClass = 'xkit-tweaks-hide-my-posts-done';
 const hiddenClass = 'xkit-tweaks-hide-my-posts-hidden';
@@ -10,10 +10,8 @@ const css = `.${hiddenClass} article { display: none; }`;
 let defaultBlog;
 
 const processPosts = async function () {
-  getPostElements({ excludeClass }).forEach(async postElement => {
-    const timeline = await givenPath(postElement);
-    if (timeline !== '/v2/timeline/dashboard') { return; }
-
+  await exposeTimelines();
+  getPostElements({ excludeClass, timeline: /\/v2\/timeline\/dashboard/ }).forEach(async postElement => {
     const { canEdit, isSubmission, postAuthor } = await timelineObjectMemoized(postElement.dataset.id);
 
     if (canEdit && (isSubmission || postAuthor === defaultBlog || postAuthor === undefined)) {

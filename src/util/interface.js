b1bfb0180513019843fb20f6eb4bd442cf254383
April Sylph <april@sylph.tech>
2021-06-03 12:29:59 +0100

    getPostElements: add timeline regex option

--- src/util/interface.js
+++ src/util/interface.js
@@ -1,17 +1,22 @@
 /**
  * @param {object} options - Arguments object (destructured, not used directly)
  * @param {string} options.excludeClass - Classname to exclude and add
- * @param {boolean} options.noPeepr - Whether to exclude posts in #glass-container (optional)
- * @param {boolean} options.includeFiltered - Whether to include filtered posts (optional)
+ * @param {object} [options.timeline] - RegExp which posts' closest data-timeline attribute must match (use exposeTimelines() first)
+ * @param {boolean} [options.noPeepr] - Whether to exclude posts in #glass-container
+ * @param {boolean} [options.includeFiltered] - Whether to include filtered posts
  * @returns {Array} - Array of post elements matching the query options
  */
-export const getPostElements = function ({ excludeClass, noPeepr = false, includeFiltered = false }) {
+export const getPostElements = function ({ excludeClass, timeline, noPeepr = false, includeFiltered = false }) {
   if (!excludeClass) {
     return [];
   }
 
   const selector = `${noPeepr ? '#base-container > :not(#glass-container)' : ''} [data-id]:not(.${excludeClass})`;
   let postElements = [...document.querySelectorAll(selector)];
+
+  if (timeline !== undefined) {
+    postElements = postElements.filter(postElement => timeline.test(postElement.closest('[data-timeline]').dataset.timeline));
+  }
 
   if (!includeFiltered) {
     postElements = postElements.filter(postElement => postElement.querySelector('article footer') !== null);

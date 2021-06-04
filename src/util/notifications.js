4e99f35ec1b59810c5b44422f2bde1061130897f
April Sylph <april@sylph.tech>
2021-06-04 14:00:45 +0100

    notifications util: fix issues with toasts on peepr

--- src/util/notifications.js
+++ src/util/notifications.js
@@ -2,11 +2,11 @@ import { onBaseContainerMutated } from './mutations.js';
 import { keyToCss } from './css_map.js';
 
 const toastContainer = Object.assign(document.createElement('div'), { id: 'xkit-toasts' });
-const peeprSelectorPromise = keyToCss('peepr');
+const drawerContentSelectorPromise = keyToCss('drawerContent');
 
 const addToastContainerToPage = async () => {
-  const peeprSelector = await peeprSelectorPromise;
-  const targetNode = document.body.querySelector(peeprSelector) || document.body.querySelector('aside') || document.body;
+  const drawerContentSelector = await drawerContentSelectorPromise;
+  const targetNode = document.body.querySelector(drawerContentSelector) || document.body.querySelector('aside') || document.body;
   if (targetNode.contains(toastContainer) === false) {
     targetNode.appendChild(toastContainer);
   }

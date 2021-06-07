ce14ec95f56fb33b2cf5bfc4a166805ad8ae0324
April Sylph <april@sylph.tech>
2021-06-07 14:36:01 +0100

    popup: remove main min-width
    fixes #152

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -52,5 +52,7 @@ document.getElementById('filter').addEventListener('input', event => {
 const versionElement = document.getElementById('version');
 versionElement.textContent = `v${browser.runtime.getManifest().version} (Beta)`;
 
-const main = document.querySelector('main');
-main.style.minWidth = `${main.getBoundingClientRect().width}px`;
+window.onload = () => {
+  const main = document.querySelector('main');
+  main.style.minWidth = `${main.getBoundingClientRect().width}px`;
+};

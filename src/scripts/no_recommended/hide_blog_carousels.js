2e4151cc233754938833717e85769608cbd262c3
April Sylph <april@sylph.tech>
2021-06-07 14:16:24 +0100

    No Recommended: hide carousels more reliably
    fixes #151

--- src/scripts/no_recommended/hide_blog_carousels.js
+++ src/scripts/no_recommended/hide_blog_carousels.js
@@ -2,27 +2,27 @@ import { keyToCss } from '../../util/css_map.js';
 import { addStyle, removeStyle } from '../../util/interface.js';
 import { onNewPosts } from '../../util/mutations.js';
 
-const excludeClass = 'xkit-no-recommended-blog-carousels-done';
 const hiddenClass = 'xkit-no-recommended-blog-carousels-hidden';
 
 const css = `.${hiddenClass} > div { display: none; }`;
 
 let blogCarouselSelector;
+let listTimelineObjectSelector;
 
 const hideBlogCarousels = async function () {
-  [...document.querySelectorAll('[data-id] + :not([data-id]) + :not([data-id])')]
-    .filter(timelineObject => {
-      timelineObject.classList.add(excludeClass);
-      return timelineObject.querySelector(blogCarouselSelector) !== null;
-    })
-    .forEach(timelineObject => {
-      timelineObject.classList.add(hiddenClass);
-      timelineObject.previousElementSibling.classList.add(hiddenClass);
+  [...document.querySelectorAll(blogCarouselSelector)]
+    .map(blogCarousel => blogCarousel.closest(listTimelineObjectSelector))
+    .filter(element => element !== null)
+    .forEach(listTimelineObject => {
+      listTimelineObject.classList.add(hiddenClass);
+      listTimelineObject.previousElementSibling.classList.add(hiddenClass);
     });
 };
 
 export const main = async function () {
   blogCarouselSelector = await keyToCss('blogCarousel');
+  listTimelineObjectSelector = await keyToCss('listTimelineObject');
+
   addStyle(css);
 
   onNewPosts.addListener(hideBlogCarousels);
@@ -33,6 +33,5 @@ export const clean = async function () {
   removeStyle(css);
 
   onNewPosts.removeListener(hideBlogCarousels);
-  $(`.${excludeClass}`).removeClass(excludeClass);
   $(`.${hiddenClass}`).removeClass(hiddenClass);
 };
--- src/scripts/no_recommended/hide_tag_carousels.js
+++ src/scripts/no_recommended/hide_tag_carousels.js
@@ -2,27 +2,27 @@ import { keyToCss } from '../../util/css_map.js';
 import { addStyle, removeStyle } from '../../util/interface.js';
 import { onNewPosts } from '../../util/mutations.js';
 
-const excludeClass = 'xkit-no-recommended-tag-carousels-done';
 const hiddenClass = 'xkit-no-recommended-tag-carousels-hidden';
 
 const css = `.${hiddenClass} > div { display: none; }`;
 
 let tagCardCarouselContainerSelector;
+let listTimelineObjectSelector;
 
 const hideTagCarousels = async function () {
-  [...document.querySelectorAll('[data-id] + :not([data-id]) + :not([data-id])')]
-    .filter(timelineObject => {
-      timelineObject.classList.add(excludeClass);
-      return timelineObject.querySelector(tagCardCarouselContainerSelector) !== null;
-    })
-    .forEach(timelineObject => {
-      timelineObject.classList.add(hiddenClass);
-      timelineObject.previousElementSibling.classList.add(hiddenClass);
+  [...document.querySelectorAll(tagCardCarouselContainerSelector)]
+    .map(tagCardCarouselContainer => tagCardCarouselContainer.closest(listTimelineObjectSelector))
+    .filter(element => element !== null)
+    .forEach(listTimelineObject => {
+      listTimelineObject.classList.add(hiddenClass);
+      listTimelineObject.previousElementSibling.classList.add(hiddenClass);
     });
 };
 
 export const main = async function () {
   tagCardCarouselContainerSelector = await keyToCss('tagCardCarouselContainer');
+  listTimelineObjectSelector = await keyToCss('listTimelineObject');
+
   addStyle(css);
 
   onNewPosts.addListener(hideTagCarousels);
@@ -33,6 +33,5 @@ export const clean = async function () {
   removeStyle(css);
 
   onNewPosts.removeListener(hideTagCarousels);
-  $(`.${excludeClass}`).removeClass(excludeClass);
   $(`.${hiddenClass}`).removeClass(hiddenClass);
 };

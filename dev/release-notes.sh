ed760a63f5bfd6311519ab21b488ccb8ef460a6b
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2021-08-02 15:56:55 +0100

    release notes dev script: remove commit links
    GitHub automatically transforms hashes into commit links;
    doing the linking manually breaks rich commit previews

--- dev/release-notes.sh
+++ dev/release-notes.sh
@@ -1,2 +1,1 @@
-GITHUB_REPOSITORY="AprilSylph/XKit-Rewritten"
-git log $(git describe --tags --abbrev=0)..HEAD --reverse --pretty --format="- [\`%h\`](https://github.com/$GITHUB_REPOSITORY/commit/%H) %s (%an)" --follow src/ > release-notes.md
+git log $(git describe --tags --abbrev=0)..HEAD --reverse --pretty --format="- %h %s (%an)" --follow src/ > release-notes.md

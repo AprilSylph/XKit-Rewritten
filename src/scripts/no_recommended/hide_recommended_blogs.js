fbe4947b342e5099cd67f7f0c77883343bf10f8b
turtleseason <48301106+turtleseason@users.noreply.github.com>
2021-08-11 14:04:31 -0700

    Hide sidebar widget on tag pages

--- src/scripts/no_recommended/hide_recommended_blogs.js
+++ src/scripts/no_recommended/hide_recommended_blogs.js
@@ -1,11 +1,15 @@
 import { onBaseContainerMutated } from '../../util/mutations.js';
 import { translate } from '../../util/language_data.js';
 import { addStyle, removeStyle } from '../../util/interface.js';
+import { descendantSelector } from '../../util/css_map.js';
 
 const excludeClass = 'xkit-no-recommended-blogs-done';
 const hiddenClass = 'xkit-no-recommended-blogs-hidden';
 
 let recommendedBlogsLabel;
+let recommendedBlogsPattern;
+
+let tagPageTitleSelector;
 
 const css = `.${hiddenClass} { display: none; }`;
 
@@ -16,10 +20,24 @@ const checkForRecommendedBlogs = function () {
       return h1.textContent === recommendedBlogsLabel;
     })
     .forEach(h1 => h1.parentNode.classList.add(hiddenClass));
+
+  [...document.querySelectorAll(tagPageTitleSelector)]
+    .filter(title => !title.classList.contains(excludeClass))
+    .filter(title => {
+      title.classList.add(excludeClass);
+      return recommendedBlogsPattern.test(title.textContent);
+    })
+    .forEach(title => title.parentNode.classList.add(hiddenClass));
 };
 
 export const main = async function () {
   recommendedBlogsLabel = await translate('Check out these blogs');
+
+  const tagPageLabel = await translate('Top %1$s blogs');
+  recommendedBlogsPattern = new RegExp(`^${tagPageLabel.replace('%1$s', '.*')}$`);
+
+  tagPageTitleSelector = await descendantSelector('desktopContainer', 'title');
+
   onBaseContainerMutated.addListener(checkForRecommendedBlogs);
   checkForRecommendedBlogs();
   addStyle(css);

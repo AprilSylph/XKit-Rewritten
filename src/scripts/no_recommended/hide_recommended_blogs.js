0cef6f097d5a6b29e61ba5ea8dc2bdc4d600ffae
turtleseason <48301106+turtleseason@users.noreply.github.com>
2021-08-11 17:50:46 -0700

    Use aria-label selector

--- src/scripts/no_recommended/hide_recommended_blogs.js
+++ src/scripts/no_recommended/hide_recommended_blogs.js
@@ -1,42 +1,30 @@
 import { onBaseContainerMutated } from '../../util/mutations.js';
 import { translate } from '../../util/language_data.js';
 import { addStyle, removeStyle } from '../../util/interface.js';
-import { descendantSelector } from '../../util/css_map.js';
 
 const excludeClass = 'xkit-no-recommended-blogs-done';
 const hiddenClass = 'xkit-no-recommended-blogs-hidden';
 
-let recommendedBlogsLabel;
-let recommendedBlogsPattern;
-
-let tagPageTitleSelector;
+let checkOutTheseBlogsSelector;
+let topBlogsSelector;
 
 const css = `.${hiddenClass} { display: none; }`;
 
 const checkForRecommendedBlogs = function () {
-  [...document.querySelectorAll(`aside > div > h1:not(.${excludeClass})`)]
-    .filter(h1 => {
-      h1.classList.add(excludeClass);
-      return h1.textContent === recommendedBlogsLabel;
-    })
-    .forEach(h1 => h1.parentNode.classList.add(hiddenClass));
-
-  [...document.querySelectorAll(tagPageTitleSelector)]
-    .filter(title => !title.classList.contains(excludeClass))
-    .filter(title => {
-      title.classList.add(excludeClass);
-      return recommendedBlogsPattern.test(title.textContent);
-    })
-    .forEach(title => title.parentNode.classList.add(hiddenClass));
+  [...document.querySelectorAll(`${checkOutTheseBlogsSelector}:not(.${excludeClass}), ${topBlogsSelector}:not(.${excludeClass})`)]
+    .forEach(ul => {
+      ul.classList.add(excludeClass);
+      ul.parentNode.classList.add(hiddenClass);
+    });
 };
 
 export const main = async function () {
-  recommendedBlogsLabel = await translate('Check out these blogs');
+  const checkOutTheseBlogsLabel = await translate('Check out these blogs');
+  checkOutTheseBlogsSelector = `aside ul[aria-label="${checkOutTheseBlogsLabel}"]`;
 
-  const tagPageLabel = await translate('Top %1$s blogs');
-  recommendedBlogsPattern = new RegExp(`^${tagPageLabel.replace('%1$s', '.*')}$`);
-
-  tagPageTitleSelector = await descendantSelector('desktopContainer', 'title');
+  const topBlogsLabel = await translate('Top %1$s blogs');
+  const [topBlogsPrefix, topBlogsSuffix] = topBlogsLabel.split('%1$s');
+  topBlogsSelector = `aside ul${topBlogsPrefix ? `[aria-label^="${topBlogsPrefix}"]` : ''}${topBlogsSuffix ? `[aria-label$="${topBlogsSuffix}"]` : ''}`;
 
   onBaseContainerMutated.addListener(checkForRecommendedBlogs);
   checkForRecommendedBlogs();

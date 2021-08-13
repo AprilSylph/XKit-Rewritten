5a71ba3ac568764aa1bae5eea3d3102cfeafb99f
turtleseason <48301106+turtleseason@users.noreply.github.com>
2021-08-13 15:36:27 -0700

    No Recommended: Hide blogs in sidebar on tag pages (#203)

--- src/scripts/no_recommended/hide_recommended_blogs.js
+++ src/scripts/no_recommended/hide_recommended_blogs.js
@@ -5,7 +5,8 @@ import { addStyle, removeStyle } from '../../util/interface.js';
 const excludeClass = 'xkit-no-recommended-blogs-done';
 const hiddenClass = 'xkit-no-recommended-blogs-hidden';
 
-let recommendedBlogsLabel;
+let checkOutTheseBlogsLabel;
+let topBlogsSelector;
 
 const css = `.${hiddenClass} { display: none; }`;
 
@@ -13,13 +14,24 @@ const checkForRecommendedBlogs = function () {
   [...document.querySelectorAll(`aside > div > h1:not(.${excludeClass})`)]
     .filter(h1 => {
       h1.classList.add(excludeClass);
-      return h1.textContent === recommendedBlogsLabel;
+      return h1.textContent === checkOutTheseBlogsLabel;
     })
     .forEach(h1 => h1.parentNode.classList.add(hiddenClass));
+
+  [...document.querySelectorAll(`${topBlogsSelector}:not(.${excludeClass})`)]
+    .forEach(ul => {
+      ul.classList.add(excludeClass);
+      ul.parentNode.classList.add(hiddenClass);
+    });
 };
 
 export const main = async function () {
-  recommendedBlogsLabel = await translate('Check out these blogs');
+  checkOutTheseBlogsLabel = await translate('Check out these blogs');
+
+  const topBlogsLabel = await translate('Top %1$s blogs');
+  const [topBlogsPrefix, topBlogsSuffix] = topBlogsLabel.split('%1$s');
+  topBlogsSelector = `aside ul${topBlogsPrefix ? `[aria-label^="${topBlogsPrefix}"]` : ''}${topBlogsSuffix ? `[aria-label$="${topBlogsSuffix}"]` : ''}`;
+
   onBaseContainerMutated.addListener(checkForRecommendedBlogs);
   checkForRecommendedBlogs();
   addStyle(css);

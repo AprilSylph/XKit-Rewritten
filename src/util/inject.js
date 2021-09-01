ed0c83aa8528c8e906699bb099a4da930fcf7ed5
Marcus <marcustyphoon@gmail.com>
2021-09-01 16:14:50 -0700

    destructure origin

--- src/util/inject.js
+++ src/util/inject.js
@@ -2,9 +2,9 @@ let nonce;
 const callbacks = new Map();
 
 const messageHandler = event => {
-  const { result, exception, xkitCallbackNonce } = event.data;
+  const { origin, result, exception, xkitCallbackNonce } = event.data;
 
-  if (event.origin === `${location.protocol}//${location.host}` && callbacks.has(xkitCallbackNonce)) {
+  if (origin === `${location.protocol}//${location.host}` && callbacks.has(xkitCallbackNonce)) {
     const [resolve, reject] = callbacks.get(xkitCallbackNonce);
     callbacks.delete(xkitCallbackNonce);
     if (callbacks.size === 0) {

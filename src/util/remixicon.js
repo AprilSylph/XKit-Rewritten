3cedaa063a9de881fa8f373f341a3d5497a5d0f0
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2021-09-06 14:57:30 +0100

    remixicon util: only add symbolset once
    fixes the symbols being re-added unnecessarily if the addon is reloaded

--- src/util/remixicon.js
+++ src/util/remixicon.js
@@ -1,10 +1,15 @@
-fetch(browser.runtime.getURL('/lib/remixicon/remixicon.symbol.svg'))
-  .then(response => response.text())
-  .then(responseText => {
-    const responseDocument = (new DOMParser()).parseFromString(responseText, 'image/svg+xml');
-    const symbols = responseDocument.firstElementChild;
-    document.head.appendChild(symbols);
-  });
+const symbolsUrl = browser.runtime.getURL('/lib/remixicon/remixicon.symbol.svg');
+
+if (document.querySelector(`svg[data-src="${symbolsUrl}"]`) === null) {
+  fetch(symbolsUrl)
+    .then(response => response.text())
+    .then(responseText => {
+      const responseDocument = (new DOMParser()).parseFromString(responseText, 'image/svg+xml');
+      const symbols = responseDocument.firstElementChild;
+      symbols.dataset.src = symbolsUrl;
+      document.head.appendChild(symbols);
+    });
+}
 
 export const buildSvg = function (symbolId) {
   const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');

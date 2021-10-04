e4dce348f63e2c0d80aec4c60e3eb08db60a99eb
invalidCards <842080+invalidCards@users.noreply.github.com>
2021-10-04 12:47:25 +0200

    Add line separator tweak (#295)
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

---
+++ src/scripts/tweaks/caught_up_line.js
@@ -0,0 +1,47 @@
+import { keyToCss } from '../../util/css_map.js';
+import { buildStyle } from '../../util/interface.js';
+import { onNewPosts } from '../../util/mutations.js';
+
+const hiddenClass = 'xkit-tweaks-caught-up-line-title';
+const borderClass = 'xkit-tweaks-caught-up-line-border';
+
+const styleElement = buildStyle(`
+  .${hiddenClass} > div { display: none; }
+  .${borderClass} > div {
+    box-sizing: content-box;
+    height: 0px;
+    overflow-y: hidden;
+    border-top: 4px solid rgb(var(--white-on-dark));
+  }
+`);
+
+let tagChicletCarouselItemSelector;
+let listTimelineObjectSelector;
+
+const createCaughtUpLine = async function () {
+  [...document.querySelectorAll(tagChicletCarouselItemSelector)]
+    .map(tagChicletCarouselItem => tagChicletCarouselItem.closest(listTimelineObjectSelector))
+    .filter((element, index, array) => array.indexOf(element) === index)
+    .forEach(listTimelineObject => {
+      listTimelineObject?.classList.add(borderClass);
+      listTimelineObject?.previousElementSibling.classList.add(hiddenClass);
+    });
+};
+
+export const main = async function () {
+  tagChicletCarouselItemSelector = await keyToCss('tagChicletCarouselItem');
+  listTimelineObjectSelector = await keyToCss('listTimelineObject');
+
+  document.head.append(styleElement);
+
+  onNewPosts.addListener(createCaughtUpLine);
+  createCaughtUpLine();
+};
+
+export const clean = async function () {
+  styleElement.remove();
+
+  onNewPosts.removeListener(createCaughtUpLine);
+  $(`.${hiddenClass}`).removeClass(hiddenClass);
+  $(`.${borderClass}`).removeClass(borderClass);
+};

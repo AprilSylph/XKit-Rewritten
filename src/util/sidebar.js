db0b87e74c87b3680a433a2fefae5334a7f4cb98
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2021-12-06 11:59:54 +0000

    somewhat simplify sidebar util
    - simplify sidebarItem construction
    - simplify sidebarTitle construction
      - remove .xkit-sidebar-title class
      - use .xkit-sidebar-item > h1 for styling instead
    - define sidebar row building function
    - move sidebarRowOptions typedef to that function
    - map rows directly into .append() instead of looping through it

--- src/util/sidebar.js
+++ src/util/sidebar.js
@@ -6,59 +6,59 @@ const sidebarItems = Object.assign(document.createElement('div'), { id: 'xkit-si
 /**
  * @typedef {object} sidebarRowOptions
  * @property {string} label - Human-readable link text
- * @property {Function} onclick - Click event handler for this item
+ * @property {Function} onclick - Click event handler for this row
  * @property {string} [count] - Human-readable additional link text
  * @property {boolean} [carrot] - Whether to include a right-facing arrow on the link (ignored if count is specified)
- * @param {object} options - Destructured
+ * @param {sidebarRowOptions} options - Sidebar row options
+ * @returns {HTMLLIElement} The constructed sidebar row
+ */
+const buildSidebarRow = function ({ label, onclick, count, carrot }) {
+  const sidebarListItem = document.createElement('li');
+
+  const button = document.createElement('button');
+  button.addEventListener('click', onclick);
+  sidebarListItem.append(button);
+
+  const labelSpan = Object.assign(document.createElement('span'), { textContent: label });
+  button.append(labelSpan);
+
+  if (count !== undefined) {
+    const countSpan = Object.assign(document.createElement('span'), {
+      className: 'count',
+      textContent: count
+    });
+    button.append(countSpan);
+  } else if (carrot === true) {
+    const carrotSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    carrotSvg.setAttribute('viewBox', '0 0 13 20.1');
+    carrotSvg.setAttribute('width', '12');
+    carrotSvg.setAttribute('height', '12');
+    button.append(carrotSvg);
+
+    const carrotPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    carrotPath.setAttribute('d', 'M0 2.9l7.2 7.2-7.1 7.1L3 20.1l7.1-7.1 2.9-2.9L2.9 0 0 2.9');
+    carrotSvg.append(carrotPath);
+  }
+
+  return sidebarListItem;
+};
+
+/**
+ * @param {object} options - Sidebar item options
  * @param {string} options.id - Unique ID for the sidebar item
  * @param {string} options.title - Human-readable sidebar item heading
  * @param {sidebarRowOptions[]} options.rows - Row options objects to construct clickable links in the sidebar item
  * @returns {HTMLDivElement} The constructed sidebar item, for future referencing
  */
 export const addSidebarItem = function ({ id, title, rows }) {
-  const sidebarItem = document.createElement('div');
-  sidebarItem.classList.add('xkit-sidebar-item');
-  sidebarItem.id = id;
-
-  const sidebarTitle = document.createElement('h1');
-  sidebarTitle.classList.add('xkit-sidebar-title');
-  sidebarTitle.textContent = title;
-  sidebarItem.appendChild(sidebarTitle);
-
+  const sidebarItem = Object.assign(document.createElement('div'), { id, className: 'xkit-sidebar-item' });
+  const sidebarTitle = Object.assign(document.createElement('h1'), { textContent: title });
   const sidebarList = document.createElement('ul');
-  sidebarItem.appendChild(sidebarList);
 
-  for (const row of rows) {
-    const sidebarListItem = document.createElement('li');
-    sidebarList.appendChild(sidebarListItem);
+  sidebarItems.append(sidebarItem);
+  sidebarItem.append(sidebarTitle, sidebarList);
+  sidebarList.append(...rows.map(buildSidebarRow));
 
-    const button = document.createElement('button');
-    button.addEventListener('click', row.onclick);
-    sidebarListItem.appendChild(button);
-
-    const label = document.createElement('span');
-    label.textContent = row.label;
-    button.appendChild(label);
-
-    if (row.count !== undefined) {
-      const count = document.createElement('span');
-      count.classList.add('count');
-      count.textContent = row.count;
-      button.appendChild(count);
-    } else if (row.carrot === true) {
-      const carrot = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-      carrot.setAttribute('viewBox', '0 0 13 20.1');
-      carrot.setAttribute('width', '12');
-      carrot.setAttribute('height', '12');
-      button.appendChild(carrot);
-
-      const carrotPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-      carrotPath.setAttribute('d', 'M0 2.9l7.2 7.2-7.1 7.1L3 20.1l7.1-7.1 2.9-2.9L2.9 0 0 2.9');
-      carrot.appendChild(carrotPath);
-    }
-  }
-
-  sidebarItems.appendChild(sidebarItem);
   return sidebarItem;
 };
 

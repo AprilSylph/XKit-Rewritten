6d9f396698f07c51493d715efc765c73d2f9dee5
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2021-12-27 10:26:01 +0000

    CleanFeed: add local flagging option
    resolves #356

--- src/scripts/cleanfeed.js
+++ src/scripts/cleanfeed.js
@@ -1,14 +1,20 @@
 import { onNewPosts } from '../util/mutations.js';
 import { keyToCss } from '../util/css_map.js';
-import { getPostElements } from '../util/interface.js';
+import { getPostElements, buildStyle } from '../util/interface.js';
+import { translate } from '../util/language_data.js';
 import { timelineObjectMemoized } from '../util/react_props.js';
 import { getPreferences } from '../util/preferences.js';
 
-let blockingMode;
-let reblogSelector;
-
 const excludeClass = 'xkit-cleanfeed-done';
 const hiddenClass = 'xkit-cleanfeed-filtered';
+
+const styleElement = buildStyle();
+
+let blockingMode;
+let localFlagging;
+
+let localFlaggedBlogs;
+let reblogSelector;
 
 const processPosts = async function () {
   getPostElements({ excludeClass }).forEach(async postElement => {
@@ -17,16 +23,16 @@ const processPosts = async function () {
       return;
     }
 
-    const { blog: { isAdult }, trail } = await timelineObjectMemoized(postElement.dataset.id);
+    const { blog: { name, isAdult }, trail } = await timelineObjectMemoized(postElement.dataset.id);
 
-    if (isAdult) {
+    if (isAdult || localFlaggedBlogs.includes(name)) {
       postElement.classList.add(hiddenClass);
       return;
     }
 
     const reblogs = postElement.querySelectorAll(reblogSelector);
     trail.forEach((trailItem, i) => {
-      if (trailItem.blog?.isAdult) {
+      if (trailItem.blog?.isAdult || localFlaggedBlogs.includes(trailItem.blog?.name)) {
         reblogs[i].classList.add(hiddenClass);
       }
     });
@@ -34,16 +40,21 @@ const processPosts = async function () {
 };
 
 export const main = async function () {
+  ({ blockingMode, localFlagging } = await getPreferences('cleanfeed'));
+  localFlaggedBlogs = localFlagging.split(',').map(username => username.trim());
+
+  const avatarText = await translate('Avatar');
+  styleElement.textContent = localFlaggedBlogs.map(username => `[title="${username}"] img[alt="${avatarText}"] { filter: blur(20px); }`).join('');
+  document.head.append(styleElement);
+
   reblogSelector = await keyToCss('reblog');
-
-  ({ blockingMode } = await getPreferences('cleanfeed'));
-
   onNewPosts.addListener(processPosts);
   processPosts();
 };
 
 export const clean = async function () {
   onNewPosts.removeListener(processPosts);
+  styleElement.remove();
 
   $(`.${excludeClass}`).removeClass(excludeClass);
   $(`.${hiddenClass}`).removeClass(hiddenClass);

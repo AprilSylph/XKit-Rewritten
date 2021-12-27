0daecad67894ab5b1207e573b1ca2a4334451d78
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2021-12-27 09:29:28 +0000

    No Recommended: hide "More like this" in lightbox
    resolves #372

---
+++ src/scripts/no_recommended/hide_lightbox_related.js
@@ -0,0 +1,19 @@
+import { descendantSelector } from '../../util/css_map.js';
+import { buildStyle } from '../../util/interface.js';
+
+const styleElement = buildStyle();
+
+export const main = async function () {
+  const flexLayoutSelector = await descendantSelector('glass', 'flexLayout');
+  const mainSectionSelector = await descendantSelector('glass', 'flexLayout', 'mainSection');
+  const relatedPostsSectionSelector = await descendantSelector('glass', 'flexLayout', 'relatedPostsSection');
+
+  styleElement.textContent = `
+    ${flexLayoutSelector}, ${mainSectionSelector} { margin-top: 0; }
+    ${relatedPostsSectionSelector} { display: none; }
+  `;
+
+  document.head.append(styleElement);
+};
+
+export const clean = async () => styleElement.remove();

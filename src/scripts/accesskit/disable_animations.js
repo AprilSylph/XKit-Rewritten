f60317bbb21f95189b21c5aa8cc54b70e087fda4
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2022-01-21 09:53:02 +0000

    AccessKit: hide floating heart animation
    resolves #397

--- src/scripts/accesskit/disable_animations.js
+++ src/scripts/accesskit/disable_animations.js
@@ -1,4 +1,17 @@
+import { keyToCss } from '../../util/css_map.js';
+import { buildStyle } from '../../util/interface.js';
+
 const className = 'accesskit-disable-animations';
 
-export const main = async () => document.body.classList.add(className);
-export const clean = async () => document.body.classList.remove(className);
+const styleElement = buildStyle();
+keyToCss('postLikeHeartAnimation').then(selector => { styleElement.textContent = `${selector} { display: none; }`; });
+
+export const main = async () => {
+  document.body.classList.add(className);
+  document.head.append(styleElement);
+};
+
+export const clean = async () => {
+  document.body.classList.remove(className);
+  styleElement.remove();
+};

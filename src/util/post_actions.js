3e196e02745c1eba8680ae227570d37da13f8e66
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2022-01-25 10:44:31 +0000

    post actions util: use pageModifications

--- src/util/post_actions.js
+++ src/util/post_actions.js
@@ -1,5 +1,5 @@
 import { buildSvg } from './remixicon.js';
-import { onGlassContainerMutated } from './mutations.js';
+import { pageModifications } from './mutations.js';
 import { keyToCss } from './css_map.js';
 
 const excludeClass = 'xkit-post-actions-done';
@@ -10,8 +10,7 @@ const postOptions = {};
 let postActionsSelector;
 let postFormButtonSelector;
 
-const addPostOptions = () => {
-  const postFormButton = document.querySelector(postFormButtonSelector);
+const addPostOptions = ([postFormButton] = [document.querySelector(postFormButtonSelector)]) => {
   if (!postFormButton || postFormButton.classList.contains(excludeClass)) { return; }
   postFormButton.classList.add(excludeClass);
 
@@ -32,8 +31,7 @@ const addPostOptions = () => {
   postActionsSelector = await keyToCss('postActions');
   postFormButtonSelector = await keyToCss('postFormButton');
 
-  onGlassContainerMutated.addListener(addPostOptions);
-  addPostOptions();
+  pageModifications.register(postFormButtonSelector, addPostOptions);
 })();
 
 /**

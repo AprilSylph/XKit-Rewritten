d503de024e4863beb19fddfb3ceea8dd35c411b8
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2022-01-27 10:31:15 +0000

    No Recommended: hide recommended blogs faster
    tag page recommended blog hiding is currently broken by the aria-label
    for that element being empty, so the new logic is technically untested.
    a ticket has been filed with tumblr support regarding this.

--- src/scripts/no_recommended/hide_recommended_blogs.js
+++ src/scripts/no_recommended/hide_recommended_blogs.js
@@ -1,45 +1,36 @@
-import { onBaseContainerMutated } from '../../util/mutations.js';
+import { pageModifications } from '../../util/mutations.js';
 import { translate } from '../../util/language_data.js';
 import { buildStyle } from '../../util/interface.js';
 
-const excludeClass = 'xkit-no-recommended-blogs-done';
 const hiddenClass = 'xkit-no-recommended-blogs-hidden';
 
 let checkOutTheseBlogsLabel;
-let topBlogsSelector;
 
 const styleElement = buildStyle(`.${hiddenClass} { display: none; }`);
 
-const checkForRecommendedBlogs = function () {
-  [...document.querySelectorAll(`aside > div > h1:not(.${excludeClass})`)]
-    .filter(h1 => {
-      h1.classList.add(excludeClass);
-      return h1.textContent === checkOutTheseBlogsLabel;
-    })
+const hideDashboardRecommended = function (sidebarTitles) {
+  sidebarTitles
+    .filter(h1 => h1.textContent === checkOutTheseBlogsLabel)
     .forEach(h1 => h1.parentNode.classList.add(hiddenClass));
-
-  [...document.querySelectorAll(`${topBlogsSelector}:not(.${excludeClass})`)]
-    .forEach(ul => {
-      ul.classList.add(excludeClass);
-      ul.parentNode.classList.add(hiddenClass);
-    });
 };
+
+const hideTagPageRecommended = topBlogsLists => topBlogsLists.forEach(ul => ul.parentNode.classList.add(hiddenClass));
 
 export const main = async function () {
   checkOutTheseBlogsLabel = await translate('Check out these blogs');
+  pageModifications.register('aside > div > h1', hideDashboardRecommended);
 
   const topBlogsLabel = await translate('Top %1$s blogs');
   const [topBlogsPrefix, topBlogsSuffix] = topBlogsLabel.split('%1$s');
-  topBlogsSelector = `aside ul${topBlogsPrefix ? `[aria-label^="${topBlogsPrefix}"]` : ''}${topBlogsSuffix ? `[aria-label$="${topBlogsSuffix}"]` : ''}`;
+  const topBlogsSelector = `aside ul${topBlogsPrefix ? `[aria-label^="${topBlogsPrefix}"]` : ''}${topBlogsSuffix ? `[aria-label$="${topBlogsSuffix}"]` : ''}`;
+  pageModifications.register(topBlogsSelector, hideTagPageRecommended);
 
-  onBaseContainerMutated.addListener(checkForRecommendedBlogs);
-  checkForRecommendedBlogs();
   document.head.append(styleElement);
 };
 
 export const clean = async function () {
-  onBaseContainerMutated.removeListener(checkForRecommendedBlogs);
+  pageModifications.unregister(hideDashboardRecommended);
+  pageModifications.unregister(hideTagPageRecommended);
   styleElement.remove();
-  $(`.${excludeClass}`).removeClass(excludeClass);
   $(`.${hiddenClass}`).removeClass(hiddenClass);
 };

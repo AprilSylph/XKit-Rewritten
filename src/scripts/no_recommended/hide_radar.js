1553340e018fba1103637f3679930c967c8ada68
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2022-01-27 09:52:34 +0000

    No Recommended: hide radar faster

--- src/scripts/no_recommended/hide_radar.js
+++ src/scripts/no_recommended/hide_radar.js
@@ -1,33 +1,27 @@
-import { onBaseContainerMutated } from '../../util/mutations.js';
+import { pageModifications } from '../../util/mutations.js';
 import { translate } from '../../util/language_data.js';
 import { buildStyle } from '../../util/interface.js';
 
-const excludeClass = 'xkit-no-recommended-radar-done';
 const hiddenClass = 'xkit-no-recommended-radar-hidden';
 
 let radarLabel;
 
 const styleElement = buildStyle(`.${hiddenClass} { display: none; }`);
 
-const checkForRadar = function () {
-  [...document.querySelectorAll(`aside > div > h1:not(.${excludeClass})`)]
-    .filter(h1 => {
-      h1.classList.add(excludeClass);
-      return h1.textContent === radarLabel;
-    })
+const checkForRadar = function (sidebarTitles) {
+  sidebarTitles
+    .filter(h1 => h1.textContent === radarLabel)
     .forEach(h1 => h1.parentNode.classList.add(hiddenClass));
 };
 
 export const main = async function () {
   radarLabel = await translate('Radar');
-  onBaseContainerMutated.addListener(checkForRadar);
-  checkForRadar();
+  pageModifications.register('aside > div > h1', checkForRadar);
   document.head.append(styleElement);
 };
 
 export const clean = async function () {
-  onBaseContainerMutated.removeListener(checkForRadar);
+  pageModifications.unregister(checkForRadar);
   styleElement.remove();
-  $(`.${excludeClass}`).removeClass(excludeClass);
   $(`.${hiddenClass}`).removeClass(hiddenClass);
 };

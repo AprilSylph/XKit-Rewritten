a47772b66ed75d5cc170de0ca50f7b8d0e67f2ca
marcustyphoon <marcustyphoon@gmail.com>
2022-02-01 03:07:26 -0800

    Use then()
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

--- src/scripts/tweaks/hide_note_count_types.js
+++ src/scripts/tweaks/hide_note_count_types.js
@@ -2,13 +2,7 @@ import { keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 
 const styleElement = buildStyle();
+keyToCss('noteCountTypes').then(selector => { styleElement.textContent = `${selector} { display: none !important; }`; });
 
-export const main = async function () {
-  const selector = await keyToCss('noteCountTypes');
-  styleElement.textContent = `${selector} { display: none !important; }`;
-  document.head.append(styleElement);
-};
-
-export const clean = async function () {
-  styleElement.remove();
-};
+export const main = async ()  => document.head.append(styleElement);
+export const clean = async () => styleElement.remove();

3838ddf887801a9c179f83d4c86576b57146819f
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2022-02-01 11:51:06 +0000

    css map util: remove descendantSelector

--- src/util/css_map.js
+++ src/util/css_map.js
@@ -1,5 +1,4 @@
 import { getCssMap } from './tumblr_helpers.js';
-import { cartesian } from './misc.js';
 
 /**
  * @param {string} key - The source name of an element
@@ -30,30 +29,4 @@ export const keyToCss = async function (key) {
 export const asyncSelector = async function (strings, ...expressions) {
   const resolvedExpressions = await Promise.all(expressions);
   return strings.map((string, index) => `${string}${resolvedExpressions[index] || ''}`).join('');
-};
-
-/**
- * Constructs a descendant selector which accounts for all possible
- * combinations of each source name. For example, if 'main' and 'timeline'
- * both map to two generated classnames:
- * keyToClasses('main') = [ "_2U2YY", "_27pa2" ]
- * keyToClasses('timeline') = [ "cfpPU", "_3ItSq" ]
- * then passing ('main', 'timeline') to this function will return
- * "._2U2YY .cfpPU, ._2U2YY ._3ItS, ._27pa2 .cfpPU, ._27pa2 ._3ItSq"
- * which targets any 'timeline' contained in any 'main'.
- *
- * @param {...string} keys - One or more element source names
- * @returns {Promise<string>} - A CSS selector
- */
-export const descendantSelector = async function (...keys) {
-  const sets = [];
-
-  for (const key of keys) {
-    const set = await keyToClasses(key);
-    sets.push(set.map(className => `.${className}`));
-  }
-
-  return cartesian(...sets)
-    .map(selectors => selectors.join(' '))
-    .join(', ');
 };

9d8601a81a59ef80789b937d890db55e205c84cc
marcustyphoon <marcustyphoon@gmail.com>
2022-02-02 01:13:24 -0800

    CSS utilities: Accept multiple keys (#441)
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

--- src/scripts/accesskit/no_user_fonts.js
+++ src/scripts/accesskit/no_user_fonts.js
@@ -4,11 +4,9 @@ import { buildStyle } from '../../util/interface.js';
 const styleElement = buildStyle();
 
 export const main = async function () {
-  const quoteSelector = await resolveExpressions`${keyToCss('textBlock')} ${keyToCss('quote')}`;
-  const chatSelector = await resolveExpressions`${keyToCss('textBlock')} ${keyToCss('chat')}`;
-  const quirkySelector = await resolveExpressions`${keyToCss('textBlock')} ${keyToCss('quirky')}`;
+  const selector = await resolveExpressions`${keyToCss('textBlock')} ${keyToCss('quote', 'chat', 'quirky')}`;
 
-  styleElement.textContent = `${quoteSelector}, ${chatSelector}, ${quirkySelector} { font-family: var(--font-family); }`;
+  styleElement.textContent = `${selector} { font-family: var(--font-family); }`;
   document.head.append(styleElement);
 };
 

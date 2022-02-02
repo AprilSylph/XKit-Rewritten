54a22f6b697555ed77db42856cfe61be930d1aeb
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2022-02-02 15:04:04 +0000

    No Recommended: hide blog carousels faster
    - use pageModifications to process blog carousels directly
    - use resolveExpressions to only target blog carousels within
      listTimelineObject elements

--- src/scripts/no_recommended/hide_blog_carousels.js
+++ src/scripts/no_recommended/hide_blog_carousels.js
@@ -1,6 +1,6 @@
-import { keyToCss } from '../../util/css_map.js';
+import { keyToCss, resolveExpressions } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
-import { onNewPosts } from '../../util/mutations.js';
+import { pageModifications } from '../../util/mutations.js';
 
 const hiddenClass = 'xkit-no-recommended-blog-carousels-hidden';
 
@@ -9,31 +9,26 @@ const styleElement = buildStyle(`
   .${hiddenClass} > div { visibility: hidden; position: absolute; max-width: 100%; }
 `);
 
-let blogCarouselSelector;
 let listTimelineObjectSelector;
 
-const hideBlogCarousels = async function () {
-  [...document.querySelectorAll(blogCarouselSelector)]
-    .map(blogCarousel => blogCarousel.closest(listTimelineObjectSelector))
-    .filter(listTimelineObject => listTimelineObject.matches(`[data-id] ~ ${listTimelineObject.tagName.toLowerCase()}`))
-    .forEach(listTimelineObject => {
-      listTimelineObject?.classList.add(hiddenClass);
-      listTimelineObject?.previousElementSibling.classList.add(hiddenClass);
-    });
-};
+const hideBlogCarousels = blogCarousels => blogCarousels
+  .map(blogCarousel => blogCarousel.closest(listTimelineObjectSelector))
+  .filter(listTimelineObject => listTimelineObject.matches(`[data-id] ~ ${listTimelineObject.tagName.toLowerCase()}`))
+  .forEach(listTimelineObject => {
+    listTimelineObject.classList.add(hiddenClass);
+    listTimelineObject.previousElementSibling.classList.add(hiddenClass);
+  });
 
 export const main = async function () {
-  blogCarouselSelector = await keyToCss('blogCarousel');
-  listTimelineObjectSelector = await keyToCss('listTimelineObject');
-
   document.head.append(styleElement);
 
-  onNewPosts.addListener(hideBlogCarousels);
+  listTimelineObjectSelector = await keyToCss('listTimelineObject');
+  const blogCarouselSelector = await resolveExpressions`${listTimelineObjectSelector} ${keyToCss('blogCarousel')}`;
+  pageModifications.register(blogCarouselSelector, hideBlogCarousels);
 };
 
 export const clean = async function () {
+  pageModifications.unregister(hideBlogCarousels);
   styleElement.remove();
-
-  onNewPosts.removeListener(hideBlogCarousels);
   $(`.${hiddenClass}`).removeClass(hiddenClass);
 };

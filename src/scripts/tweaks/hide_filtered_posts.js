b7e601942791ac1bb56b472a40d056b68997a1f1
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2022-02-02 16:31:05 +0000

    Tweaks: get posts from pageModifications
    affects "Hide filtered posts entirely"
    and "Hide my own posts on the dashboard"
    
    - accept arguments in processPosts()s
    - use filterPostElements

--- src/scripts/tweaks/hide_filtered_posts.js
+++ src/scripts/tweaks/hide_filtered_posts.js
@@ -1,22 +1,21 @@
 import { onNewPosts } from '../../util/mutations.js';
-import { buildStyle, getPostElements } from '../../util/interface.js';
+import { buildStyle, filterPostElements } from '../../util/interface.js';
 
 import { timelineObjectMemoized } from '../../util/react_props.js';
 
 const excludeClass = 'xkit-tweaks-hide-filtered-posts-done';
 const includeFiltered = true;
+
 const hiddenClass = 'xkit-tweaks-hide-filtered-posts-hidden';
 const styleElement = buildStyle(`.${hiddenClass} article { display: none; }`);
 
-const processPosts = async function () {
-  getPostElements({ excludeClass, includeFiltered }).forEach(async postElement => {
-    const { filtered } = await timelineObjectMemoized(postElement.dataset.id);
+const processPosts = postElements => filterPostElements(postElements, { excludeClass, includeFiltered }).forEach(async postElement => {
+  const { filtered } = await timelineObjectMemoized(postElement.dataset.id);
 
-    if (filtered !== undefined && Object.keys(filtered).length !== 0) {
-      postElement.classList.add(hiddenClass);
-    }
-  });
-};
+  if (filtered !== undefined && Object.keys(filtered).length !== 0) {
+    postElement.classList.add(hiddenClass);
+  }
+});
 
 export const main = async function () {
   onNewPosts.addListener(processPosts);
--- src/scripts/tweaks/hide_my_posts.js
+++ src/scripts/tweaks/hide_my_posts.js
@@ -1,17 +1,19 @@
 import { getPrimaryBlogName } from '../../util/user_blogs.js';
 import { onNewPosts } from '../../util/mutations.js';
-import { buildStyle, getPostElements } from '../../util/interface.js';
+import { buildStyle, filterPostElements } from '../../util/interface.js';
 import { exposeTimelines, timelineObjectMemoized } from '../../util/react_props.js';
 
 const excludeClass = 'xkit-tweaks-hide-my-posts-done';
+const timeline = /\/v2\/timeline\/dashboard/;
+
 const hiddenClass = 'xkit-tweaks-hide-my-posts-hidden';
 const styleElement = buildStyle(`.${hiddenClass} article { display: none; }`);
 
 let primaryBlogName;
 
-const processPosts = async function () {
+const processPosts = async function (postElements) {
   await exposeTimelines();
-  getPostElements({ excludeClass, timeline: /\/v2\/timeline\/dashboard/ }).forEach(async postElement => {
+  filterPostElements(postElements, { excludeClass, timeline }).forEach(async postElement => {
     const { canEdit, isSubmission, postAuthor } = await timelineObjectMemoized(postElement.dataset.id);
 
     if (canEdit && (isSubmission || postAuthor === primaryBlogName || postAuthor === undefined)) {

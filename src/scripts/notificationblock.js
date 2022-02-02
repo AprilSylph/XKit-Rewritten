84c7a79b36d1cf279585906aea67cb952366dc42
Marcus <marcustyphoon@gmail.com>
2022-02-02 04:49:57 -0800

    Properly name processNotifications injection

--- src/scripts/notificationblock.js
+++ src/scripts/notificationblock.js
@@ -19,7 +19,7 @@ const buildCss = () => blockedPostTargetIDs.length === 0
   ? ''
   : blockedPostTargetIDs.map(id => `[data-target-post-id="${id}"]`).join(', ').concat(' { display: none; }');
 
-const unburyTargetPostIds = async (notificationSelector) => {
+const unburyTargetPostIds = async function unburyTargetPostIds (notificationSelector) {
   [...document.querySelectorAll(notificationSelector)]
     .filter(({ dataset: { targetPostId } }) => targetPostId === undefined)
     .forEach(async notificationElement => {

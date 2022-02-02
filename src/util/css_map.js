e706342968d6621abf855db170ea7dadfd868fcc
Marcus <marcustyphoon@gmail.com>
2022-02-01 23:01:38 -0800

    keyToCss: accept multiple keys

--- src/util/css_map.js
+++ src/util/css_map.js
@@ -13,8 +13,9 @@ export const keyToClasses = async function (key) {
  * @param {string} key - The source name of an element
  * @returns {Promise<string>} - A CSS :is() selector which targets all elements with that source name
  */
-export const keyToCss = async function (key) {
-  const classes = await keyToClasses(key);
+export const keyToCss = async function (...keys) {
+  const classArrays = await Promise.all(keys.map(keyToClasses));
+  const classes = classArrays.flat(1);
   return `:is(${classes.map(className => `.${className}`).join(', ')})`;
 };
 

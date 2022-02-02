7395376dcb55f71e21c72e16d56bd8eaa3a22d6b
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2022-02-02 15:13:31 +0000

    No Recommended: hide tag carousels faster
    - use pageModifications to process carousel wrappers directly
    - make hideTagCarousels synchronous
    - only target carousel wrappers within listTimelineObject elements

--- src/scripts/no_recommended/hide_tag_carousels.js
+++ src/scripts/no_recommended/hide_tag_carousels.js
@@ -1,38 +1,33 @@
-import { keyToCss } from '../../util/css_map.js';
+import { keyToCss, resolveExpressions } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
-import { onNewPosts } from '../../util/mutations.js';
+import { pageModifications } from '../../util/mutations.js';
 
 const hiddenClass = 'xkit-no-recommended-tag-carousels-hidden';
 
 const styleElement = buildStyle(`.${hiddenClass} > div { display: none; }`);
 
-let carouselWrapperSelector;
 let tagCardCarouselItemSelector;
 let listTimelineObjectSelector;
 
-const hideTagCarousels = async function () {
-  [...document.querySelectorAll(carouselWrapperSelector)]
-    .filter(carouselWrapper => carouselWrapper.querySelector(tagCardCarouselItemSelector) !== null)
-    .map(carouselWrapper => carouselWrapper.closest(listTimelineObjectSelector))
-    .forEach(listTimelineObject => {
-      listTimelineObject?.classList.add(hiddenClass);
-      listTimelineObject?.previousElementSibling.classList.add(hiddenClass);
-    });
-};
+const hideTagCarousels = carouselWrappers => carouselWrappers
+  .filter(carouselWrapper => carouselWrapper.querySelector(tagCardCarouselItemSelector) !== null)
+  .map(carouselWrapper => carouselWrapper.closest(listTimelineObjectSelector))
+  .forEach(listTimelineObject => {
+    listTimelineObject.classList.add(hiddenClass);
+    listTimelineObject.previousElementSibling.classList.add(hiddenClass);
+  });
 
 export const main = async function () {
-  carouselWrapperSelector = await keyToCss('carouselWrapper');
-  tagCardCarouselItemSelector = await keyToCss('tagCardCarouselItem');
-  listTimelineObjectSelector = await keyToCss('listTimelineObject');
-
   document.head.append(styleElement);
 
-  onNewPosts.addListener(hideTagCarousels);
+  tagCardCarouselItemSelector = await keyToCss('tagCardCarouselItem');
+  listTimelineObjectSelector = await keyToCss('listTimelineObject');
+  const carouselWrapperSelector = await resolveExpressions`${listTimelineObjectSelector} ${keyToCss('carouselWrapper')}`;
+  pageModifications.register(carouselWrapperSelector, hideTagCarousels);
 };
 
 export const clean = async function () {
+  pageModifications.unregister(hideTagCarousels);
   styleElement.remove();
-
-  onNewPosts.removeListener(hideTagCarousels);
   $(`.${hiddenClass}`).removeClass(hiddenClass);
 };

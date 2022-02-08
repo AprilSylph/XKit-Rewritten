41254011ddf21ec8ae180eed70640bfaa62f487a
Marcus <marcustyphoon@gmail.com>
2022-02-08 06:17:22 -0800

    Revert "Fix footer redesign margin properly"
    This reverts commit 9172a80634b2f3b44cc5483c06d3a3eb182e0f9f.

--- src/scripts/tweaks/footer_unredesign.js
+++ src/scripts/tweaks/footer_unredesign.js
@@ -1,42 +1,33 @@
-import { keyToClasses, keyToCss, resolveExpressions } from '../../util/css_map.js';
+import { keyToClasses, keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 import { pageModifications } from '../../util/mutations.js';
 
 let footerRedesignClasses;
 let footerRedesignSelector;
 
-const keyToNot = async function (...keys) {
-  const classes = await keyToClasses(...keys);
-  return classes.map(className => `:not(.${className})`).join('');
-};
+const styleElement = buildStyle(`
+.xkit-control-button-container {
+  margin-left: 20px;
+}
 
-const styleElement = buildStyle();
-resolveExpressions`
-  ${keyToCss('postActivityWrapper')}${keyToNot('postActivityWrapperOpen')} {
-    margin-top: 0;
-  }
-  .xkit-control-button-container {
-    margin-left: 20px;
-  }
+[role="dialog"] #quick-reblog,
+[role="dialog"] #quick-tags {
+  top: 50% !important;
+  bottom: unset !important;
+  right: 100% !important;
+  transform: translate(-20px, -50%) !important;
+}
 
-  [role="dialog"] #quick-reblog,
-  [role="dialog"] #quick-tags {
+@media only screen and (max-width: 650px) {
+  #quick-reblog,
+  #quick-tags {
     top: 50% !important;
     bottom: unset !important;
     right: 100% !important;
     transform: translate(-20px, -50%) !important;
   }
-
-  @media only screen and (max-width: 650px) {
-    #quick-reblog,
-    #quick-tags {
-      top: 50% !important;
-      bottom: unset !important;
-      right: 100% !important;
-      transform: translate(-20px, -50%) !important;
-    }
-  }
-`.then(css => { styleElement.textContent = css; console.log(styleElement.textContent); });
+}
+`);
 
 const removeFooterRedesign = elements => {
   elements.forEach(element => {

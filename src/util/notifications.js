fcca7684ae9c8293a37b29979f711cc2684a6c52
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2022-02-24 10:03:18 +0000

    notifications util: adapt for new blog view
    - target new blog view sidebar; resolve #483
    - change how target node is found (require display !== none)
    - only check target node's immediate children for toast container

--- src/util/notifications.js
+++ src/util/notifications.js
@@ -9,12 +9,14 @@ const addToastContainerToPage = async () => {
   const drawerContentSelector = await drawerContentSelectorPromise;
   const sidebarSelector = await sidebarSelectorPromise;
 
-  const targetNode =
-    document.body.querySelector(drawerContentSelector) ||
-    document.body.querySelector(sidebarSelector) ||
-    document.body;
+  const targetNode = [
+    document.body.querySelector(`${drawerContentSelector} ${sidebarSelector}`),
+    document.body.querySelector(drawerContentSelector),
+    document.body.querySelector(sidebarSelector),
+    document.body
+  ].find(candidateNode => candidateNode !== null && getComputedStyle(candidateNode).display !== 'none');
 
-  if (targetNode.contains(toastContainer) === false) {
+  if ([...targetNode.children].includes(toastContainer) === false) {
     toastContainer.dataset.inSidebar = targetNode.matches(sidebarSelector);
     targetNode.append(toastContainer);
   }
@@ -27,10 +29,13 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
  */
 export const notify = async textContent => {
   await addToastContainerToPage();
+
   const toast = Object.assign(document.createElement('div'), { textContent, className: 'visible' });
   toastContainer.append(toast);
+
   await sleep(4000);
   toast.classList.remove('visible');
+
   await sleep(1000);
   toast.remove();
 };

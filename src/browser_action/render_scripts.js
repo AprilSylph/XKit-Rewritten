2b4b4c07fc1df9f74c72807c0319087b246465c0
Marcus <marcustyphoon@gmail.com>
2022-03-02 06:43:56 -0800

    Fix typo

--- src/browser_action/render_scripts.js
+++ src/browser_action/render_scripts.js
@@ -132,7 +132,7 @@ const renderScripts = async function () {
   for (const scriptName of [...orderedEnabledScripts, ...disabledScripts]) {
     const url = getURL(`/scripts/${scriptName}.json`);
     const file = await fetch(url);
-    const { title = scriptName, description = '', icon = {}, help = '', relatedTerms = [], preferences = {}, } = await file.json();
+    const { title = scriptName, description = '', icon = {}, help = '', relatedTerms = [], preferences = {} } = await file.json();
 
     const scriptTemplateClone = document.getElementById('script').content.cloneNode(true);
 

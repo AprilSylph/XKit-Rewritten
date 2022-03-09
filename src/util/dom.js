38fbb9fd75bbc63d4b18ad21954c66db60a9553c
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2022-03-09 13:27:06 +0000

    dom util: return normalized elements

--- src/util/dom.js
+++ src/util/dom.js
@@ -2,7 +2,7 @@
  * Create elements with simple syntax
  *
  * @param {string} tagName - Type of element to create
- * @param {object} [attributes] - Property-value pairs to set as HTML attributes (e.g. { href: '/' })
+ * @param {object} [attributes] - Property-value pairs to set as HTML/XML attributes (e.g. { href: '/' })
  * @param {object} [events] - Property-value pairs to set as event listeners (e.g. { click: () => {} })
  * @param {(Node|string)[]} [children] - Zero or more valid children
  * @returns {Element} Element created to specification
@@ -16,5 +16,6 @@ export function dom (tagName, attributes = {}, events = {}, children = []) {
   events && Object.entries(events).forEach(([type, listener]) => element.addEventListener(type, listener));
   children && element.replaceChildren(...children);
 
+  element.normalize();
   return element;
 }

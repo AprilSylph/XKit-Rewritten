ab37cdd222fa2283eac32e1ecf4fdf7b4f9c0c15
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2022-03-21 12:03:56 +0000

    Open in Tabs: translate blog view links
    resolves #500

--- src/scripts/open_in_tabs.js
+++ src/scripts/open_in_tabs.js
@@ -1,7 +1,21 @@
+const onClickExternalLink = event => event.stopPropagation();
+
+const onClickBlogViewLink = event => {
+  event.stopPropagation();
+  event.preventDefault();
+
+  const { pathname } = new URL(event.target.href);
+  const [blogName, postId] = pathname.split('/').slice(3);
+
+  window.open(`https://${blogName}.tumblr.com/post/${postId}`);
+};
+
 export const main = async function () {
-  $('#base-container').on('click', 'a[role="link"][target="_blank"]', event => event.stopPropagation());
+  $('#base-container').on('click', 'a[role="link"][target="_blank"]', onClickExternalLink);
+  $('#base-container').on('click', 'a[href^="/blog/view/"]', onClickBlogViewLink);
 };
 
 export const clean = async function () {
-  $('#base-container').off('click', 'a[role="link"][target="_blank"]');
+  $('#base-container').off('click', 'a[role="link"][target="_blank"]', onClickExternalLink);
+  $('#base-container').off('click', 'a[href^="/blog/view/"]', onClickBlogViewLink);
 };

43cda87504bb1d84591f3ac358cb88cf8c30ed5e
Marcus <marcustyphoon@gmail.com>
2022-04-15 11:28:54 -0700

    Implement control button hiding (wrong button for now)

---
+++ src/scripts/tweaks/hide_blaze.js
@@ -0,0 +1,30 @@
+import { keyToCss } from '../../util/css_map.js';
+import { buildStyle, filterPostElements } from '../../util/interface.js';
+import { translate } from '../../util/language_data.js';
+import { onNewPosts } from '../../util/mutations.js';
+
+const hiddenClass = 'xkit-blaze-hidden';
+let controlIconSelector;
+let blazeIconSelector;
+
+const styleElement = buildStyle(`.${hiddenClass} { display: none; }`);
+
+const processPosts = postElements => filterPostElements(postElements).forEach(postElement => {
+  const blazeIcon = postElement.querySelector(blazeIconSelector);
+  blazeIcon?.closest(controlIconSelector)?.classList.add(hiddenClass);
+});
+
+export const main = async function () {
+  document.head.append(styleElement);
+  controlIconSelector = await keyToCss('controlIcon');
+  const blazeLabel = await translate('Reblog');
+  blazeIconSelector = `${controlIconSelector} [aria-label="${blazeLabel}"]`;
+
+  onNewPosts.addListener(processPosts);
+};
+
+export const clean = async function () {
+  onNewPosts.removeListener(processPosts);
+  styleElement.remove();
+  $(`.${hiddenClass}`).removeClass(hiddenClass);
+};

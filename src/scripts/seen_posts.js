fd837e414e0a2672ecd7df93cdeac825748838c4
Marcus <marcustyphoon@gmail.com>
2022-04-21 14:37:25 -0700

    Revert PR and implement onStorageChanged method

--- src/scripts/seen_posts.js
+++ src/scripts/seen_posts.js
@@ -12,41 +12,42 @@ const onlyDimAvatarsClass = 'xkit-seen-posts-only-dim-avatar';
 const storageKey = 'seen_posts.seenPosts';
 let seenPosts = [];
 
-const markPosts = (postElements) => {
-  for (const postElement of postElements) {
+const dimPosts = function (postElements) {
+  for (const postElement of filterPostElements(postElements, { excludeClass, timeline, includeFiltered })) {
     const { id } = postElement.dataset;
+
     if (seenPosts.includes(id)) {
       postElement.classList.add(dimClass);
+    } else {
+      seenPosts.push(id);
     }
   }
-};
 
-const dimPosts = async function (elements) {
-  const postElements = filterPostElements(elements, { excludeClass, timeline, includeFiltered });
-  markPosts(postElements);
-
-  ({ [storageKey]: seenPosts = [] } = await browser.storage.local.get(storageKey));
-  markPosts(postElements);
-
-  const newPostIds = postElements
-    .map(postElement => postElement.dataset.id)
-    .filter(id => !seenPosts.includes(id));
-  seenPosts.push(...newPostIds);
   seenPosts.splice(0, seenPosts.length - 10000);
+
   browser.storage.local.set({ [storageKey]: seenPosts });
 };
 
 export const onStorageChanged = async function (changes, areaName) {
-  const { 'seen_posts.preferences.onlyDimAvatars': onlyDimAvatarsChanges } = changes;
+  const {
+    'seen_posts.preferences.onlyDimAvatars': onlyDimAvatarsChanges,
+    [storageKey]: seenPostsChanges
+  } = changes;
 
   if (onlyDimAvatarsChanges && onlyDimAvatarsChanges.oldValue !== undefined) {
     const { newValue: onlyDimAvatars } = onlyDimAvatarsChanges;
     const addOrRemove = onlyDimAvatars ? 'add' : 'remove';
     document.body.classList[addOrRemove](onlyDimAvatarsClass);
   }
+
+  if (seenPostsChanges) {
+    ({ newValue: seenPosts } = seenPostsChanges);
+  }
 };
 
 export const main = async function () {
+  ({ [storageKey]: seenPosts = [] } = await browser.storage.local.get(storageKey));
+
   const { onlyDimAvatars } = await getPreferences('seen_posts');
   if (onlyDimAvatars) {
     document.body.classList.add(onlyDimAvatarsClass);

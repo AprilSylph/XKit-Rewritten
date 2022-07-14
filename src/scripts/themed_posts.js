58695c0d968e38b8009329ada6fc909e85e6d04c
marcustyphoon <marcustyphoon@gmail.com>
2022-07-14 14:42:53 -0700

    Themed Posts: Implement per-trail-item theming (#629)
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

--- src/scripts/themed_posts.js
+++ src/scripts/themed_posts.js
@@ -1,16 +1,20 @@
-import { buildStyle, filterPostElements, blogViewSelector } from '../util/interface.js';
+import { buildStyle, filterPostElements, blogViewSelector, postSelector } from '../util/interface.js';
 import { getPreferences } from '../util/preferences.js';
 import { onNewPosts } from '../util/mutations.js';
 import { timelineObject } from '../util/react_props.js';
+import { keyToCss } from '../util/css_map.js';
 
 const styleElement = buildStyle();
 const blogs = new Set();
 const groupsFromHex = /^#(?<red>[A-Fa-f0-9]{1,2})(?<green>[A-Fa-f0-9]{1,2})(?<blue>[A-Fa-f0-9]{1,2})$/;
+const reblogSelector = keyToCss('reblog');
 
 let enableOnPeepr;
 let blacklistedUsernames;
 
 let blacklist;
+
+let reblogTrailTheming = true;
 
 const hexToRGB = (hex) => {
   const { red, green, blue } = hex.match(groupsFromHex).groups;
@@ -24,40 +28,66 @@ const processPosts = async function (postElements) {
   filterPostElements(postElements, { includeFiltered: true }).forEach(async postElement => {
     if (postElement.matches(blogViewSelector) && !enableOnPeepr) return;
 
-    const { blog: { name, theme } } = await timelineObject(postElement);
+    const { blog, trail = [] } = await timelineObject(postElement);
 
-    if (blacklist.includes(name)) return;
+    const blogData = [
+      blog,
+      ...reblogTrailTheming ? trail.map(item => item.blog) : []
+    ];
 
-    if (!blogs.has(name)) {
-      blogs.add(name);
+    blogData.forEach(({ name, theme }) => {
+      if (blacklist.includes(name)) return;
 
-      const {
-        backgroundColor,
-        titleColor,
-        linkColor
-      } = theme;
+      if (!blogs.has(name)) {
+        blogs.add(name);
 
-      const backgroundColorRGB = hexToRGB(backgroundColor);
-      const titleColorRGB = hexToRGB(titleColor);
-      const linkColorRGB = hexToRGB(linkColor);
+        const {
+          backgroundColor,
+          titleColor,
+          linkColor
+        } = theme;
 
-      styleElement.textContent += `
-        [data-xkit-themed="${name}"] {
-          --white: ${backgroundColorRGB};
-          --black: ${titleColorRGB};
-          --accent: ${linkColorRGB};
-          --color-primary-link: rgb(var(--accent));
-        }
-      `;
+        const backgroundColorRGB = hexToRGB(backgroundColor);
+        const titleColorRGB = hexToRGB(titleColor);
+        const linkColorRGB = hexToRGB(linkColor);
+
+        styleElement.textContent += `
+          [data-xkit-themed="${name}"] {
+            --white: ${backgroundColorRGB};
+            --black: ${titleColorRGB};
+            --accent: ${linkColorRGB};
+            --color-primary-link: rgb(var(--accent));
+          }
+        `;
+      }
+    });
+
+    postElement.dataset.xkitThemed = blog.name ?? '';
+
+    if (reblogTrailTheming) {
+      [...postElement.querySelectorAll(reblogSelector)].forEach((reblog, i) => {
+        reblog.dataset.xkitThemed = trail[i]?.blog?.name ?? '';
+      });
     }
-
-    postElement.dataset.xkitThemed = name;
   });
 };
 
 export const main = async function () {
-  ({ enableOnPeepr, blacklistedUsernames } = await getPreferences('themed_posts'));
+  ({ reblogTrailTheming, enableOnPeepr, blacklistedUsernames } = await getPreferences('themed_posts'));
   blacklist = blacklistedUsernames.split(',').map(username => username.trim());
+
+  if (reblogTrailTheming) {
+    styleElement.textContent += `
+      ${postSelector} ${reblogSelector} {
+        display: flow-root;
+        margin-top: 0;
+      }
+
+      ${postSelector} ${reblogSelector}:not(:last-child) > :last-child {
+        margin-bottom: 15px;
+      }
+    `;
+  }
 
   document.head.append(styleElement);
   onNewPosts.addListener(processPosts);

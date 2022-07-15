f36143973790aefcc52ee4517e575cb345e9b80a
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2022-07-15 22:23:24 +0100

    Apply suggestions from code review

--- src/scripts/tweaks/hide_activity_notification_badge.js
+++ src/scripts/tweaks/hide_activity_notification_badge.js
@@ -1,9 +1,12 @@
-import { keyToClasses } from '../../util/css_map.js';
+import { keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 import { translate } from '../../util/language_data.js';
 
-const badgeSelector = keyToClasses('notificationBadge').map(cssClass => `button[aria-label="${translate('Activity')}"] .${cssClass}`).join(',');
-const styleElement = buildStyle(`${badgeSelector} { display: none; }`);
+const styleElement = buildStyle(`
+button[aria-label="${translate('Activity')}"] ${keyToCss('notificationBadge')} {
+  display: none;
+}
+`);
 
 export const main = async () => document.head.append(styleElement);
 export const clean = async () => styleElement.remove();

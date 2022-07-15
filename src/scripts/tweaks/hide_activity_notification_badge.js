29ac29c2c1dbc97773a47eadce32a33cf3108e1a
Kelly Clowers <dev@kelly.clowers.org>
2022-07-15 14:26:02 -0700

    Add tweak 'Hide activity notification badge' (#639)
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

---
+++ src/scripts/tweaks/hide_activity_notification_badge.js
@@ -0,0 +1,12 @@
+import { keyToCss } from '../../util/css_map.js';
+import { buildStyle } from '../../util/interface.js';
+import { translate } from '../../util/language_data.js';
+
+const styleElement = buildStyle(`
+button[aria-label="${translate('Activity')}"] ${keyToCss('notificationBadge')} {
+  display: none;
+}
+`);
+
+export const main = async () => document.head.append(styleElement);
+export const clean = async () => styleElement.remove();

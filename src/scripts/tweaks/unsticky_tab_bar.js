b1b4c83f1106728e2cadaa2edcfadadee7370559
marcustyphoon <marcustyphoon@gmail.com>
2022-07-23 14:59:24 -0700

    Remove slide transform effect
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

--- src/scripts/tweaks/unsticky_tab_bar.js
+++ src/scripts/tweaks/unsticky_tab_bar.js
@@ -2,7 +2,7 @@ import { keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 
 const styleElement = buildStyle(`
-  ${keyToCss('timelineHeader')}${keyToCss('sticky')} { position: static; }
+  ${keyToCss('timelineHeader')}${keyToCss('sticky')} { position: static; transform: none; }
   ${keyToCss('post')} ${keyToCss('stickyContainer')} ${keyToCss('avatar')}${keyToCss('avatarUnderTabs')} { top: 69px; }
 `);
 

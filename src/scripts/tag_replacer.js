edc1bf114aed0a85df36fee5651c519ce53e15e6
Marcus <marcustyphoon@gmail.com>
2022-07-26 01:49:10 -0700

    Remove ads from api response

--- src/scripts/tag_replacer.js
+++ src/scripts/tag_replacer.js
@@ -137,7 +137,8 @@ const replaceTag = async ({ uuid, oldTag, newTag, appendOnly }) => {
   while (resource) {
     await Promise.all([
       apiFetch(resource).then(({ response }) => {
-        taggedPosts.push(...response.posts);
+        const posts = response.posts.filter(object => object.objectType === 'post');
+        taggedPosts.push(...posts);
         resource = response.links?.next?.href;
 
         gatherStatus.textContent = `Found ${taggedPosts.length} posts${resource ? '...' : '.'}`;

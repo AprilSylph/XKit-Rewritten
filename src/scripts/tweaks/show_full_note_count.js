b2d2236bb4cfbff9297e6419a5549e4af113eebe
Marcus <marcustyphoon@gmail.com>
2022-08-01 11:20:51 -0700

    Add full note count tweak

---
+++ src/scripts/tweaks/show_full_note_count.js
@@ -0,0 +1,42 @@
+import { pageModifications } from '../../util/mutations.js';
+import { translate } from '../../util/language_data.js';
+import { buildStyle } from '../../util/interface.js';
+import { timelineObject } from '../../util/react_props.js';
+import { keyToCss } from '../../util/css_map.js';
+
+const numberFormat = new Intl.NumberFormat();
+
+const notes = translate('notes');
+const note = translate('note');
+
+const styleElement = buildStyle(`
+  [data-full-notes] {
+    font-size: 0px;
+  }
+
+  [data-full-notes]::after {
+    content: attr(data-full-notes);
+    font-size: 1rem;
+  }
+`);
+
+const formatNoteElements = function (noteElements) {
+  noteElements.forEach(async noteElement => {
+    const { noteCount } = await timelineObject(noteElement);
+    if (!noteCount) return;
+
+    const label = noteCount === 1 ? note : notes;
+    noteElement.dataset.fullNotes = `${numberFormat.format(noteCount)} ${label}`;
+  });
+};
+
+export const main = async function () {
+  document.head.append(styleElement);
+  pageModifications.register(`article footer ${keyToCss('noteCountContainer')} > span`, formatNoteElements);
+};
+
+export const clean = async function () {
+  styleElement.remove();
+  pageModifications.unregister(formatNoteElements);
+  $('[data-full-notes]').removeAttr('data-full-notes');
+};

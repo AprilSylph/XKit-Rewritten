e2366ea62c4a923df9cbd298b6a7620621732aa5
Marcus <marcustyphoon@gmail.com>
2022-08-05 18:14:58 -0700

    Remove junk from API response

--- src/scripts/tag_tracking_plus.js
+++ src/scripts/tag_tracking_plus.js
@@ -59,7 +59,7 @@ const processTagLinks = async function ([searchResultElement]) {
     const savedTimestamp = timestamps[tag] || 0;
 
     const { response: { timeline: { elements = [], links } } } = await apiFetch(`/v2/hubs/${tag}/timeline`, { queryParams: { limit: 20, sort: 'recent' } });
-    const posts = elements.filter(({ objectType }) => objectType === 'post');
+    const posts = elements.filter(({ objectType, tags }) => objectType === 'post' && tags.includes(tag));
     let unreadCount = 0;
 
     for (const { timestamp } of posts) {

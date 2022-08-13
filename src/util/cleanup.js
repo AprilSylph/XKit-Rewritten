64f257ef9f3774c40a4ccf457482fb873621cb41
Marcus <marcustyphoon@gmail.com>
2022-08-13 11:28:32 -0700

    enable multiple parameters

--- src/util/cleanup.js
+++ src/util/cleanup.js
@@ -2,37 +2,48 @@
  * Removes all elements on the current page with the given css class.
  * i.e. jQuery $(`.${className}`).remove();
  *
- * @param {string} className - class name to select elements to remove
+ * @param {...string} classNames - one or more class names to select elements to remove
  * @returns {void}
  */
-export const remove = className =>
-  [...document.querySelectorAll(`.${className}`)].forEach(element => element.remove());
+export const remove = (...classNames) =>
+  [...document.querySelectorAll(classNames.map(className => `.${className}`).join(','))]
+    .forEach(element => element.remove());
 
 /**
  * Removes the given css class from all elements on the current page that have it.
  * i.e. jQuery $(`.${className}`).removeClass(className);
  *
- * @param {string} className - class name to remove
+ * @param {...string} classNames - one or more class names to remove
  * @returns {void}
  */
-export const removeClass = className =>
-  [...document.querySelectorAll(`.${className}`)].forEach(element => element.classList.remove(className));
+export const removeClass = (...classNames) =>
+  classNames.forEach(className =>
+    [...document.querySelectorAll(`.${className}`)].forEach(element =>
+      element.classList.remove(className)
+    )
+  );
 
 /**
  * Removes the given data attribute from all elements on the current page that have it.
  * i.e. jQuery $(`[data-${dashStylename}]`).removeAttr(`data-${dashStylename}`);
  *
- * @param {string} dashStylename - data attribute to remove, in dash-style
+ * @param {...string} dashStylenames - one or more data attributes to remove, in dash-style
  * @returns {void}
  */
-export const removeAttr = dashStylename =>
-  [...document.querySelectorAll(`[data-${dashStylename}]`)].forEach(element => element.removeAttribute(`data-${dashStylename}`));
+export const removeAttr = (...dashStylenames) =>
+  dashStylenames.forEach(dashStylename =>
+    [...document.querySelectorAll(`[data-${dashStylename}]`)].forEach(element =>
+      element.removeAttribute(`data-${dashStylename}`)
+    )
+  );
 
 /**
  * Removes the given data attribute from all elements on the current page that have it.
  *
- * @param {string} camelCaseName - data attribute to remove, in camelCase
+ * @param {...string} camelCaseNames - one or more data attributes to remove, in camelCase
  * @returns {void}
  */
-export const removeDataset = camelCaseName =>
-  removeAttr(camelCaseName.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`));
+export const removeDataset = (...camelCaseNames) =>
+  camelCaseNames.forEach(camelCaseName =>
+    removeAttr(camelCaseName.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`))
+  );

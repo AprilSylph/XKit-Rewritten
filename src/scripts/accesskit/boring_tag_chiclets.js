ac4de64a38d0db5f1ad64735d9aa2e88fbaa7c33
Marcus <marcustyphoon@gmail.com>
2022-08-15 17:24:45 -0700

    Fix tag chiclet deanimation

--- src/scripts/accesskit/boring_tag_chiclets.js
+++ src/scripts/accesskit/boring_tag_chiclets.js
@@ -1,9 +1,18 @@
 import { keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 
-const styleElement = buildStyle(`${keyToCss('tagChicletWrapper')} {
-  background-image: none !important; color: rgb(var(--black)); background-color: rgb(var(--secondary-accent));
-}`);
+const styleElement = buildStyle(`
+  ${keyToCss('tagChicletWrapper')} {
+    background-image: none !important;
+    color: rgb(var(--black));
+    background-color: rgb(var(--secondary-accent));
+  }
+
+  ${keyToCss('tagChicletWrapper')} > video,
+  ${keyToCss('tagChicletWrapper')} > ${keyToCss('tagChicletBgShader')} {
+    display: none !important;
+  }
+`);
 
 export const main = async () => document.head.append(styleElement);
 export const clean = async () => styleElement.remove();

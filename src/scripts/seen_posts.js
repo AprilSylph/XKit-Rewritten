bd765a4b542f841add7389c315fecadece8e0138
Marcus <marcustyphoon@gmail.com>
2022-08-15 18:47:46 -0700

    Implement seen posts intersection observer

--- src/scripts/seen_posts.js
+++ src/scripts/seen_posts.js
@@ -12,20 +12,53 @@ const onlyDimAvatarsClass = 'xkit-seen-posts-only-dim-avatar';
 const storageKey = 'seen_posts.seenPosts';
 let seenPosts = [];
 
+/** @type {Map<IntersectionObserver, ?number>} */
+const observersAndTimers = new Map();
+
+const markAsSeen = (postElement, observer) => {
+  postElement.style.border = '3px solid blue';
+
+  observer.disconnect();
+  observersAndTimers.delete(observer);
+
+  seenPosts.push(postElement.dataset.id);
+  seenPosts.splice(0, seenPosts.length - 10000);
+  // browser.storage.local.set({ [storageKey]: seenPosts });
+};
+
+const observeUntilSeen = postElement => {
+  postElement.style.border = '5px dotted orange';
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      const isIntersecting = [...entries].every(({ isIntersecting }) => isIntersecting);
+
+      if (isIntersecting) {
+        postElement.style.border = '3px solid green';
+
+        observersAndTimers.set(observer, setTimeout(() => markAsSeen(postElement), 300));
+      } else {
+        clearTimeout(observersAndTimers.get(observer));
+      }
+    },
+    { rootMargin: '-20px 0px' }
+  );
+
+  observersAndTimers.set(observer, undefined);
+  observer.observe(postElement.querySelector('article'));
+};
+
 const dimPosts = function (postElements) {
   for (const postElement of filterPostElements(postElements, { excludeClass, timeline, includeFiltered })) {
     const { id } = postElement.dataset;
 
     if (seenPosts.includes(id)) {
+      postElement.style.border = '3px solid purple';
       postElement.classList.add(dimClass);
     } else {
-      seenPosts.push(id);
+      observeUntilSeen(postElement);
     }
   }
-
-  seenPosts.splice(0, seenPosts.length - 10000);
-
-  browser.storage.local.set({ [storageKey]: seenPosts });
 };
 
 export const onStorageChanged = async function (changes, areaName) {
@@ -58,6 +91,13 @@ export const main = async function () {
 
 export const clean = async function () {
   onNewPosts.removeListener(dimPosts);
+
+  observersAndTimers.forEach((timerId, observer) => {
+    clearTimeout(timerId);
+    observer.disconnect();
+  });
+  observersAndTimers.clear();
+
   $(`.${excludeClass}`).removeClass(excludeClass);
   $(`.${dimClass}`).removeClass(dimClass);
   $(`.${onlyDimAvatarsClass}`).removeClass(onlyDimAvatarsClass);

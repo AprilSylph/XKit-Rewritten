89948098db92dd4a2ed902df45f044f2b090122e
Marcus <marcustyphoon@gmail.com>
2022-08-19 13:16:51 -0700

    use consistent buildStyle indentation

--- src/scripts/accesskit/boring_tag_chiclets.js
+++ src/scripts/accesskit/boring_tag_chiclets.js
@@ -2,16 +2,16 @@ import { keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 
 const styleElement = buildStyle(`
-  ${keyToCss('tagChicletWrapper')} {
-    background-image: none !important;
-    color: rgb(var(--black));
-    background-color: rgb(var(--secondary-accent));
-  }
+${keyToCss('tagChicletWrapper')} {
+  background-image: none !important;
+  color: rgb(var(--black));
+  background-color: rgb(var(--secondary-accent));
+}
 
-  ${keyToCss('tagChicletWrapper')} > video,
-  ${keyToCss('tagChicletWrapper')} > ${keyToCss('tagChicletBgShader')} {
-    display: none !important;
-  }
+${keyToCss('tagChicletWrapper')} > video,
+${keyToCss('tagChicletWrapper')} > ${keyToCss('tagChicletBgShader')} {
+  display: none !important;
+}
 `);
 
 export const main = async () => document.head.append(styleElement);

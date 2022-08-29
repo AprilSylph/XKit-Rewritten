cf5f7a5deff9622561f5334eca1bb7b841702a10
Marcus <marcustyphoon@gmail.com>
2022-08-29 03:10:02 -0700

    Pause chiclet videos instead of hiding them
    surely it's not this easy

--- src/scripts/accesskit/boring_tag_chiclets.js
+++ src/scripts/accesskit/boring_tag_chiclets.js
@@ -1,18 +1,9 @@
 import { keyToCss } from '../../util/css_map.js';
-import { buildStyle } from '../../util/interface.js';
+import { pageModifications } from '../../util/mutations.js';
 
-const styleElement = buildStyle(`
-${keyToCss('tagChicletWrapper')} {
-  background-image: none !important;
-  color: rgb(var(--black));
-  background-color: rgb(var(--secondary-accent));
-}
+const processtagChicletVideos = videos => videos.forEach(video => video.pause());
 
-${keyToCss('tagChicletWrapper')} > video,
-${keyToCss('tagChicletWrapper')} > ${keyToCss('tagChicletBgShader')} {
-  display: none !important;
-}
-`);
+export const main = async () =>
+  pageModifications.register(`${keyToCss('tagChicletWrapper')} > video`, processtagChicletVideos);
 
-export const main = async () => document.head.append(styleElement);
-export const clean = async () => styleElement.remove();
+export const clean = async () => pageModifications.unregister(processtagChicletVideos);

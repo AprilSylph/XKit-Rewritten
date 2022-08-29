7102bcefd053914fd0ac77a7aa14554309bd0317
Marcus <marcustyphoon@gmail.com>
2022-08-29 03:16:29 -0700

    Play videos again in clean()

--- src/scripts/accesskit/boring_tag_chiclets.js
+++ src/scripts/accesskit/boring_tag_chiclets.js
@@ -1,9 +1,14 @@
 import { keyToCss } from '../../util/css_map.js';
 import { pageModifications } from '../../util/mutations.js';
 
+const tagChicletVideoSelector = `${keyToCss('tagChicletWrapper')} > video`;
+
 const processTagChicletVideos = videos => videos.forEach(video => video.pause());
 
 export const main = async () =>
-  pageModifications.register(`${keyToCss('tagChicletWrapper')} > video`, processTagChicletVideos);
+  pageModifications.register(tagChicletVideoSelector, processTagChicletVideos);
 
-export const clean = async () => pageModifications.unregister(processTagChicletVideos);
+export const clean = async () => {
+  pageModifications.unregister(processTagChicletVideos);
+  [...document.querySelectorAll(tagChicletVideoSelector)].forEach(video => video.play());
+};

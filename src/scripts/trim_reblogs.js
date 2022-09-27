a07756fa619fd7c0815e238826b1fd2f54266726
Marcus <marcustyphoon@gmail.com>
2022-09-27 12:48:54 -0700

    Revert "implement configurable form and preview"
    This reverts commit 8a859ba46fbafcb8d69b1f75027ce0b616c9b9b3.

--- src/scripts/trim_reblogs.js
+++ src/scripts/trim_reblogs.js
@@ -10,9 +10,6 @@ import { apiFetch } from '../util/tumblr_helpers.js';
 
 const symbolId = 'ri-scissors-cut-line';
 const buttonClass = 'xkit-trim-reblogs-button';
-const reblogPreviewClass = 'xkit-trim-reblogs-preview';
-const avatarPreviewClass = 'xkit-trim-reblogs-avatar-preview';
-const textPreviewClass = 'xkit-trim-reblogs-text-preview';
 
 const controlIconSelector = keyToCss('controlIcon');
 const reblogSelector = keyToCss('reblog');
@@ -46,7 +43,6 @@ const onButtonClicked = async function ({ currentTarget: controlButton }) {
 
   const {
     response: {
-      blog,
       content = [],
       layout,
       state = 'published',
@@ -56,61 +52,22 @@ const onButtonClicked = async function ({ currentTarget: controlButton }) {
       slug = '',
       trail = []
     }
-  } = await apiFetch(`/v2/blog/${uuid}/posts/${postId}?fields[blogs]=name,avatar`);
+  } = await apiFetch(`/v2/blog/${uuid}/posts/${postId}`);
 
   if (trail?.length < 2) {
     notify('This post is too short to trim!');
     return;
   }
 
-  const defaultKeep = 1;
-  const min = 1;
-  const max = trail.length - 1;
+  const excludeTrailItems = [];
 
-  const previewData = [...trail];
-  if (Object.entries(content).length) previewData.push({ blog, content });
+  for (const [index] of trail.entries()) {
+    excludeTrailItems.push(index);
+  }
 
-  const previewElements = previewData.map(({ blog: { avatar }, content }) => {
-    const { url } = avatar[avatar.length - 1];
-    const text = content.map(({ text }) => text).filter(Boolean).join(' / ');
-    return dom('div', {}, {}, [
-      dom('div', { class: avatarPreviewClass, style: `background-image: url(${url})` }),
-      dom('div', { class: textPreviewClass }, {}, [text])
-    ]);
-  });
-  const previewElement = dom('div', { class: reblogPreviewClass });
-  previewElement.append(...previewElements);
+  excludeTrailItems.pop();
 
-  const inputElement = dom('input', {
-    type: 'number',
-    name: 'keepItems',
-    value: defaultKeep,
-    min,
-    max,
-    required: true,
-    style: 'width: 20ch' // becomes tiny in chromium if not hard coded
-  });
-
-  let excludeTrailItems = [];
-
-  const updateExcluded = () => {
-    if (inputElement.matches(':valid')) {
-      const keptItems = parseInt(inputElement.value, 10);
-      if (isNaN(keptItems) || keptItems > max || keptItems < min) {
-        throw new Error(`Invalid kept items value: ${inputElement.value}`);
-      }
-      excludeTrailItems = [...trail.keys()].slice(0, -keptItems);
-
-      previewElements.forEach((element, i) => {
-        element.dataset.excluded = excludeTrailItems.includes(i);
-      });
-    }
-  };
-  inputElement.addEventListener('input', updateExcluded);
-  updateExcluded();
-
-  const confirmTrim = async event => {
-    event.preventDefault();
+  const onClickTrim = async () => {
     hideModal();
 
     try {
@@ -140,22 +97,17 @@ const onButtonClicked = async function ({ currentTarget: controlButton }) {
     }
   };
 
-  const form = dom('form', { id: 'xkit-trim-reblogs-form' }, { submit: confirmTrim }, [
-    dom('label', null, null, ['Keep this many trail items:', inputElement])
-  ]);
-
   showModal({
     title: 'Trim this post?',
     message: [
-      form,
-      previewElement,
+      'All but the last trail item will be removed.',
       ...(unsureOfLegacyStatus
         ? ['\n\n', "Warning: XKit can't tell if this post originated from the legacy post editor. Trimming may fail if so."]
         : [])
     ],
     buttons: [
       modalCancelButton,
-      dom('input', { type: 'submit', form: form.id, class: 'blue', value: 'Trim!' })
+      dom('button', { class: 'blue' }, { click: onClickTrim }, ['Trim!'])
     ]
   });
 };
@@ -184,5 +136,3 @@ export const clean = async function () {
   onNewPosts.removeListener(processPosts);
   $(`.${buttonClass}`).remove();
 };
-
-export const stylesheet = true;

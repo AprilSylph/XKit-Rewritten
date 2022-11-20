358ac7ac6d81fa6a09f2a355398a87c6ac57bf63
Marcus <marcustyphoon@gmail.com>
2022-11-19 21:59:50 -0800

    Update panorama.js

--- src/scripts/panorama.js
+++ src/scripts/panorama.js
@@ -6,21 +6,27 @@ const reblog = `${keyToCss('post')} ${keyToCss('reblog')}`;
 const videoBlock = keyToCss('videoBlock');
 const queueSettings = keyToCss('queueSettings');
 
+const maxPostWidth = '640px';
+
 const styleElement = buildStyle(`
 #base-container > div > div > header,
 ${container} {
-  max-width: 100vw;
+  max-width: min(100vw, 1716px);
   padding-left: ${85 - 64}px;
   padding-right: 30px;
 }
 
-${container} > :first-child {
-  min-width: 0;
-  max-width: none;
-  flex: 1;
+${container} {
+  justify-content: center;
 }
 
-${container} > :first-child > main { max-width: calc(100% - ${625 - 540}px); }
+${container} > :first-child {
+  min-width: 0;
+  max-width: ${maxPostWidth};
+  padding-left: ${625 - 540}px;
+  flex: 1;
+}
+${container} > :first-child > main { max-width: none; }
 ${container} > :first-child > main article { max-width: 100%; }
 ${container} > :first-child > main article > * { max-width: 100%; }
 
@@ -30,7 +36,7 @@ ${videoBlock} iframe { max-width: none !important; }
 
 ${queueSettings} {
   box-sizing: border-box;
-  width: calc(100% - ${625 - 540}px);
+  width: 100%;
 }
 `);
 styleElement.media = '(min-width: 990px)';

19fe53bbf0a72c4245a1335af9261f03aa96a368
marcustyphoon <marcustyphoon@gmail.com>
2022-11-23 09:05:53 -0800

    export event handler directly
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

--- src/util/tumblr_helpers.js
+++ src/util/tumblr_helpers.js
@@ -49,9 +49,8 @@ export const apiFetch = async function (...args) {
 export const navigate = location =>
   inject(location => window.tumblr.navigate(location), [location]);
 
-export const onClickNavigate = (location) =>
-  (event) => {
-    if (event.ctrlKey || event.metaKey || event.altKey || event.shiftKey) return;
-    event.preventDefault();
-    navigate(location);
-  };
+export const onClickNavigate = event => {
+  if (event.ctrlKey || event.metaKey || event.altKey || event.shiftKey) return;
+  event.preventDefault();
+  navigate(event.currentTarget.href);
+};

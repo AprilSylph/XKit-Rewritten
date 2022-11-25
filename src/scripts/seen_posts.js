91fe812ab4ddf5d185ba41113ae6a1df13bc6eff
C. Lockett <103963818+char-lock@users.noreply.github.com>
2022-11-25 17:53:27 -0500

    Implement hide seen posts in the Seen Posts script

--- src/scripts/seen_posts.js
+++ src/scripts/seen_posts.js
@@ -8,6 +8,7 @@ const includeFiltered = true;
 
 const dimClass = 'xkit-seen-posts-seen';
 const onlyDimAvatarsClass = 'xkit-seen-posts-only-dim-avatar';
+const hideClass = 'xkit-seen-posts-hide';
 
 const storageKey = 'seen_posts.seenPosts';
 let seenPosts = [];
@@ -53,9 +54,16 @@ const dimPosts = function (postElements) {
 
 export const onStorageChanged = async function (changes, areaName) {
   const {
+    'seen_posts.preferences.hideSeenPosts': hideSeenPostsChanges,
     'seen_posts.preferences.onlyDimAvatars': onlyDimAvatarsChanges,
     [storageKey]: seenPostsChanges
   } = changes;
+
+  if (hideSeenPostsChanges && hideSeenPostsChanges.oldValue !== undefined) {
+    const { newValue: hideSeenPosts } = hideSeenPostsChanges;
+    const addOrRemoveHide = hideSeenPosts ? 'add' : 'remove';
+    document.body.classList[addOrRemoveHide](hideClass);
+  }
 
   if (onlyDimAvatarsChanges && onlyDimAvatarsChanges.oldValue !== undefined) {
     const { newValue: onlyDimAvatars } = onlyDimAvatarsChanges;
@@ -71,7 +79,10 @@ export const onStorageChanged = async function (changes, areaName) {
 export const main = async function () {
   ({ [storageKey]: seenPosts = [] } = await browser.storage.local.get(storageKey));
 
-  const { onlyDimAvatars } = await getPreferences('seen_posts');
+  const { hideSeenPosts, onlyDimAvatars } = await getPreferences('seen_posts');
+  if (hideSeenPosts) {
+    document.body.classList.add(hideClass);
+  }
   if (onlyDimAvatars) {
     document.body.classList.add(onlyDimAvatarsClass);
   }
@@ -87,6 +98,7 @@ export const clean = async function () {
   timers.clear();
 
   $(`.${excludeClass}`).removeClass(excludeClass);
+  $(`.${hideClass}`).removeClass(hideClass);
   $(`.${dimClass}`).removeClass(dimClass);
   $(`.${onlyDimAvatarsClass}`).removeClass(onlyDimAvatarsClass);
 };

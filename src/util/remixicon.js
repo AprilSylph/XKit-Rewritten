0c8b86d521566d1a4df8b46f577e3ec718350eff
Marcus <marcustyphoon@gmail.com>
2022-12-05 18:27:37 -0800

    defer remixicon parsing

--- src/util/remixicon.js
+++ src/util/remixicon.js
@@ -2,16 +2,20 @@ import { dom } from './dom.js';
 
 const symbolsUrl = browser.runtime.getURL('/lib/remixicon/remixicon.symbol.svg');
 
-if (document.querySelector(`svg[data-src="${symbolsUrl}"]`) === null) {
-  fetch(symbolsUrl)
-    .then(response => response.text())
-    .then(responseText => {
-      const responseDocument = (new DOMParser()).parseFromString(responseText, 'image/svg+xml');
-      const symbols = responseDocument.firstElementChild;
-      symbols.dataset.src = symbolsUrl;
-      document.head.appendChild(symbols);
-    });
-}
+const requestIdleCallback = window.requestIdleCallback ?? window.requestAnimationFrame;
+
+requestIdleCallback(() => {
+  if (document.querySelector(`svg[data-src="${symbolsUrl}"]`) === null) {
+    fetch(symbolsUrl)
+      .then(response => response.text())
+      .then(responseText => {
+        const responseDocument = (new DOMParser()).parseFromString(responseText, 'image/svg+xml');
+        const symbols = responseDocument.firstElementChild;
+        symbols.dataset.src = symbolsUrl;
+        document.head.appendChild(symbols);
+      });
+  }
+});
 
 export const buildSvg = symbolId => dom('svg', { xmlns: 'http://www.w3.org/2000/svg' }, null, [
   dom('use', { xmlns: 'http://www.w3.org/2000/svg', href: `#${symbolId}` })

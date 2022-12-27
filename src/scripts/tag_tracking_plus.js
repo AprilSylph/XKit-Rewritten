fea5a4e6e90664783b0e4afcb5f8c941ae7aa234
Marcus <marcustyphoon@gmail.com>
2022-12-26 21:45:46 -0800

    Revert "tag tracking+: defer top level awaited fetch"
    This reverts commit 69d0a3c56c22aec3e52f7200d33fa2bcfe06c4bb.

--- src/scripts/tag_tracking_plus.js
+++ src/scripts/tag_tracking_plus.js
@@ -15,7 +15,8 @@ const includeFiltered = true;
 const tagLinkSelector = `${keyToCss('searchResult')} h3 ~ a${keyToCss('typeaheadRow')}[href^="/tagged/"]`;
 const tagTextSelector = keyToCss('tagText');
 
-let trackedTags;
+const trackedTagsData = await apiFetch('/v2/user/tags') ?? {};
+const trackedTags = trackedTagsData.response?.tags?.map(({ name }) => name) ?? [];
 const unreadCounts = new Map();
 
 let sidebarItem;
@@ -130,9 +131,6 @@ const processTagLinks = function (tagLinkElements) {
 };
 
 export const main = async function () {
-  const trackedTagsData = await apiFetch('/v2/user/tags') ?? {};
-  trackedTags = trackedTagsData.response?.tags?.map(({ name }) => name) ?? [];
-
   onNewPosts.addListener(processPosts);
   refreshAllCounts(true).then(startRefreshInterval);
 

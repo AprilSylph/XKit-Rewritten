23e9f7ea831e2307e947eac8b9ccb5af1676fe19
Marcus <marcustyphoon@gmail.com>
2023-02-08 02:06:47 -0800

    Update no_where_were_we.js

--- src/scripts/tweaks/no_where_were_we.js
+++ src/scripts/tweaks/no_where_were_we.js
@@ -1,7 +1,7 @@
 import { keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 
-const styleElement = buildStyle(`${keyToCss('wrapper')} ${keyToCss('newPostIndicator')} { display: none; }`);
+const styleElement = buildStyle(`${keyToCss('wrapper')} ${keyToCss('newPostIndicator')} { display: none !important; }`);
 
 export const main = async () => document.head.append(styleElement);
 export const clean = async () => styleElement.remove();

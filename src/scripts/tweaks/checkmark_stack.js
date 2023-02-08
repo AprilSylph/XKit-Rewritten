409480fd8d410faac789c11646972aae1c457dd4
Marcus <marcustyphoon@gmail.com>
2023-02-08 00:10:43 -0800

    cleanup

--- src/scripts/tweaks/checkmark_stack.js
+++ src/scripts/tweaks/checkmark_stack.js
@@ -2,7 +2,7 @@ import { keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 import { pageModifications } from '../../util/mutations.js';
 
-let styleElementMax = 0;
+let styleElementMaxCheckmarks = 0;
 const styleElement = buildStyle();
 
 const transitionStyleElement = buildStyle(`
@@ -11,37 +11,39 @@ const transitionStyleElement = buildStyle(`
   }
 `);
 
-const updateStyleElement = count => {
-  if (count > styleElementMax) {
-    styleElementMax = count;
+const updateStyleElement = maxCheckmarks => {
+  styleElementMaxCheckmarks = maxCheckmarks;
 
-    styleElement.textContent = `
-      *:not(:hover) > ${keyToCss('blueCheckmarksContainer')} ${keyToCss('blueCheckmarkContainer')} {
-        margin-right: -10px;
+  const range = [...Array(styleElementMaxCheckmarks).keys()].map(i => i + 1);
+  styleElement.textContent = `
+    *:not(:hover) > ${keyToCss('blueCheckmarksContainer')} ${keyToCss('blueCheckmarkContainer')} {
+      margin-right: -10px;
+    }
+
+    *:not(:hover) > ${keyToCss('blueCheckmarksContainer')} {
+      margin-right: 10px;
+    }
+
+    ${keyToCss('blueCheckmarkContainer')} > :is(svg, img) {
+      filter: drop-shadow(1px 0px 2px rgb(0 0 0 / 0.5));
+    }
+
+    ${keyToCss('blueCheckmarksContainer')} {
+      isolation: isolate;
+    }
+
+    ${range.map(i => `
+      ${keyToCss('blueCheckmarkContainer')}:nth-last-child(${i}) {
+        z-index: ${i};
       }
-
-      *:not(:hover) > ${keyToCss('blueCheckmarksContainer')} {
-        margin-right: 10px;
-      }
-
-      ${keyToCss('blueCheckmarkContainer')} > :is(svg, img) {
-        filter: drop-shadow(1px 0px 2px rgb(0 0 0 / 0.5));
-      }
-
-      ${keyToCss('blueCheckmarksContainer')} {
-        isolation: isolate;
-      }
-
-    ` + [...Array(styleElementMax).keys()].map(i => i + 1).map(i => `
-          ${keyToCss('blueCheckmarkContainer')}:nth-last-child(${i}) {
-            z-index: ${i};
-          }
-        `).join('');
-  }
+    `).join('')}
+  `;
 };
 
-const processBlueCheckmarksContainer = blueCheckmarksContainers =>
-  updateStyleElement(Math.max(...blueCheckmarksContainers.map(el => el.children.length)));
+const processBlueCheckmarksContainer = blueCheckmarksContainers => {
+  const maxCheckmarks = Math.max(...blueCheckmarksContainers.map(el => el.children.length));
+  maxCheckmarks > styleElementMaxCheckmarks && updateStyleElement(maxCheckmarks);
+};
 
 const waitForRender = () =>
   new Promise(resolve => requestAnimationFrame(() => requestAnimationFrame(resolve)));

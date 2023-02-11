4ca7361945b3e13c1924631931cf195bfbff80ce
marcustyphoon <marcustyphoon@gmail.com>
2023-02-11 18:49:01 -0500

    Ensure style elements override Tumblr styles (#963)

--- src/scripts/tweaks/slim_filtered_screens.js
+++ src/scripts/tweaks/slim_filtered_screens.js
@@ -31,5 +31,5 @@ ${filteredScreenSelector} ${keyToCss('viewPostLinkWrapper')} {
 }
 `);
 
-export const main = async () => document.head.append(styleElement);
+export const main = async () => document.documentElement.append(styleElement);
 export const clean = async () => styleElement.remove();

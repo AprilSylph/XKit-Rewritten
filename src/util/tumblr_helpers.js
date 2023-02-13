13e68784e1ae4dedaaee614c2197efd113dd8aa6
Marcus <marcustyphoon@gmail.com>
2023-02-13 12:23:04 -0800

    Update tumblr_helpers.js

--- src/util/tumblr_helpers.js
+++ src/util/tumblr_helpers.js
@@ -68,8 +68,10 @@ export const createEditRequestBody = postData => {
     interactabilityReblog,
 
     canBeTipped,
-    hasCommunityLabel,
-    communityLabelCategories
+    communityLabels: {
+      hasCommunityLabel,
+      communityLabelCategories
+    }
   } = postData;
 
   return {

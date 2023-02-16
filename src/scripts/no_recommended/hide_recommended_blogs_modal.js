e15ac6aaf547ec974f8d0ea2230929784816e9fb
Marcus <marcustyphoon@gmail.com>
2023-02-16 15:17:40 -0800

    duplicate file

---
+++ src/scripts/no_recommended/hide_recommended_blogs_modal.js
@@ -0,0 +1,32 @@
+import { keyToCss } from '../../util/css_map.js';
+import { pageModifications } from '../../util/mutations.js';
+import { translate } from '../../util/language_data.js';
+import { buildStyle } from '../../util/interface.js';
+
+const hiddenClass = 'xkit-no-recommended-blogs-hidden';
+
+const styleElement = buildStyle(`.${hiddenClass} { display: none; }`);
+
+const hideDashboardRecommended = function (sidebarTitles) {
+  sidebarTitles
+    .filter(h1 => h1.textContent === translate('Check out these blogs'))
+    .forEach(h1 => h1.parentNode.classList.add(hiddenClass));
+};
+
+const hideTagPageRecommended = topBlogsLists => topBlogsLists.forEach(ul => ul.parentNode.classList.add(hiddenClass));
+
+export const main = async function () {
+  pageModifications.register('aside > div > h1', hideDashboardRecommended);
+
+  const topBlogsSelector = `${keyToCss('desktopContainer')} > ${keyToCss('recommendedBlogs')}`;
+  pageModifications.register(topBlogsSelector, hideTagPageRecommended);
+
+  document.documentElement.append(styleElement);
+};
+
+export const clean = async function () {
+  pageModifications.unregister(hideDashboardRecommended);
+  pageModifications.unregister(hideTagPageRecommended);
+  styleElement.remove();
+  $(`.${hiddenClass}`).removeClass(hiddenClass);
+};

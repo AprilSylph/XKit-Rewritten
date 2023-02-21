d76cba8a518f1ca5d2fad4e58f2a64783482856b
Marcus <marcustyphoon@gmail.com>
2023-02-21 11:55:38 -0800

    New tweak: hide Tumblr Live carousel

---
+++ src/scripts/tweaks/no_live.js
@@ -0,0 +1,26 @@
+import { pageModifications } from '../../util/mutations.js';
+import { buildStyle } from '../../util/interface.js';
+import { keyToCss } from '../../util/css_map.js';
+
+const hiddenClass = 'xkit-tweaks-no-live-hidden';
+const styleElement = buildStyle(`.${hiddenClass} { display: none; }`);
+
+const processFrames = frames =>
+  frames.forEach(frame =>
+    frame.closest(keyToCss('listTimelineObjectInner')).classList.add(hiddenClass)
+  );
+
+export const main = async function () {
+  pageModifications.register(
+    'iframe[src^="https://api.gateway.tumblr-live.com/"]',
+    processFrames
+  );
+  document.documentElement.append(styleElement);
+};
+
+export const clean = async function () {
+  pageModifications.unregister(processFrames);
+  styleElement.remove();
+
+  $(`.${hiddenClass}`).removeClass(hiddenClass);
+};

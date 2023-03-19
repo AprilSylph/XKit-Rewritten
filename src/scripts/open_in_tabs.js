86a30943c44a431bc0ce83ce00e587a5dc5aea0b
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-03-19 20:52:47 +0800

    Open in Tabs: Fix in blog card modals/blog view modals

--- src/scripts/open_in_tabs.js
+++ src/scripts/open_in_tabs.js
@@ -1,4 +1,10 @@
+import { pageModifications } from '../../util/mutations.js'; 
+
+const linkSelector = 'a[role="link"][target="_blank"]';
+
 const onClickExternalLink = event => event.stopPropagation();
+
+const processLinks = links => links.forEach(link => link.addEventListener('click', onClickExternalLink));
 
 const onClickBlogViewLink = event => {
   event.stopPropagation();
@@ -11,11 +17,12 @@ const onClickBlogViewLink = event => {
 };
 
 export const main = async function () {
-  $('#base-container').on('click', 'a[role="link"][target="_blank"]', onClickExternalLink);
+  pageModifications.register(linkSelector, processLinks); 
   $('#base-container').on('click', 'a[href^="/blog/view/"]', onClickBlogViewLink);
 };
 
 export const clean = async function () {
-  $('#base-container').off('click', 'a[role="link"][target="_blank"]', onClickExternalLink);
+  pageModifications.unregister(processLinks); 
+  [...document.querySelectorAll(linkSelector)].forEach(link => link.removeEventListener('click', onClickExternalLink));
   $('#base-container').off('click', 'a[href^="/blog/view/"]', onClickBlogViewLink);
 };

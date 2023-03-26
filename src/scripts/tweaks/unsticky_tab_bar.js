5025f9106bae3589a2fd97a27d59d79bcf434393
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-03-26 08:14:29 +0800

    Fix visual oddity of tab bar
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

--- src/scripts/tweaks/unsticky_tab_bar.js
+++ src/scripts/tweaks/unsticky_tab_bar.js
@@ -2,7 +2,11 @@ import { keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 
 const styleElement = buildStyle(`
-  ${keyToCss('tabsHeader')} { position: relative !important; top: 0 !important;}
+  ${keyToCss('tabsHeader')} {
+    position: relative !important;
+    top: 0 !important;
+    transition: none !important;
+  }
   ${keyToCss('post')} ${keyToCss('stickyContainer')} > ${keyToCss('avatar')} { top: 69px !important; }
 `);
 

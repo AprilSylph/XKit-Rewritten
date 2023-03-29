ecb1cdf967746127f4b03b6483f01eaab1f69c97
Michelle Panzironi <michelle.panzironi@gmail.com>
2023-03-29 16:46:23 -0400

    target timelineHeaderNavInner to hide dash side scroll

--- src/scripts/accesskit/normal_width_scrollbar.js
+++ src/scripts/accesskit/normal_width_scrollbar.js
@@ -1,10 +1,21 @@
+import { keyToCss } from '../../util/css_map.js';
+import { buildStyle } from '../../util/interface.js';
+
 const className = 'accesskit-normal-width-scrollbar';
+const styleElement = buildStyle(`
+  .${className}
+  ${keyToCss('timelineHeaderNavInner')} { 
+    scrollbar-width: none !important; 
+}`
+);
 
 export const main = async () => {
   document.documentElement.classList.add(className);
   document.body.classList.add(className);
+  document.documentElement.append(styleElement);
 };
 export const clean = async () => {
   document.documentElement.classList.remove(className);
   document.body.classList.remove(className);
+  styleElement.remove();
 };

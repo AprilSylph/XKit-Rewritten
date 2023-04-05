bff0b8bbd898ad3787fbf5823239048502a4050b
Michelle Panzironi <michelle.panzironi@gmail.com>
2023-04-05 12:23:59 -0400

    Update src/scripts/accesskit/normal_width_scrollbar.js
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

--- src/scripts/accesskit/normal_width_scrollbar.js
+++ src/scripts/accesskit/normal_width_scrollbar.js
@@ -3,7 +3,6 @@ const className = 'accesskit-normal-width-scrollbar';
 export const main = async () => {
   document.documentElement.classList.add(className);
   document.body.classList.add(className);
-  document.documentElement.append(styleElement);
 };
 export const clean = async () => {
   document.documentElement.classList.remove(className);

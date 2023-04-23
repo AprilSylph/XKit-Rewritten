483a6d2240a1a660fd3b9052a917ad8dfe3833c6
Marcus <marcustyphoon@gmail.com>
2023-04-22 18:51:04 -0700

    work in radar post too

--- src/scripts/tweaks/hide_blaze_tip_labels.js
+++ src/scripts/tweaks/hide_blaze_tip_labels.js
@@ -1,8 +1,8 @@
 import { keyToCss } from '../../util/css_map.js';
-import { buildStyle, postSelector } from '../../util/interface.js';
+import { buildStyle } from '../../util/interface.js';
 
 const styleElement = buildStyle(`
-${postSelector} ${keyToCss('igniteButton', 'tippingButton')} > ${keyToCss('label')} {
+article ${keyToCss('igniteButton', 'tippingButton')} > ${keyToCss('label')} {
   display: none;
 }
 `);

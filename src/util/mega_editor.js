037310b5f166d17a88918ef7500082c9221ac50f
Marcus <marcustyphoon@gmail.com>
2023-05-09 17:14:32 -0700

    create bulkCommunityLabel helper

--- src/util/mega_editor.js
+++ src/util/mega_editor.js
@@ -56,3 +56,46 @@ export const megaEdit = async function (postIds, options) {
     [`https://www.tumblr.com/${pathname}`, $.param(requestBody)]
   );
 };
+
+/**
+ * @param {string} blogName - The name of the blog to edit posts on
+ * @param {string[]} postIds - Array of post IDs to edit (must not exceed 100 items)
+ * @param {object} options - Configuration object
+ * @param {boolean} options.hasCommunityLabel
+ * @param {string} options.categories - valid modes are:
+ *                                 1. "drug_use"
+ *                                 2. "violence"
+ *                                 3. "sexual_themes"
+ * @returns {Promise<Response>} Response from constructed request
+ */
+export const bulkCommunityLabel = async function (blogName, postIds, options) {
+  formKey ??= await fetch('https://www.tumblr.com/neue_web/iframe/new/text').then(response => {
+    if (response.ok) {
+      return response.text();
+    } else {
+      throw Object.assign(new Error(response.status), { response });
+    }
+  }).then(responseText => {
+    const responseDocument = (new DOMParser()).parseFromString(responseText, 'text/html');
+    return responseDocument.getElementById('tumblr_form_key').getAttribute('content');
+  }).catch(console.error);
+
+  const requestBody = {
+    form_key: formKey,
+    has_community_label: options.hasCommunityLabel,
+    categories: options.categories,
+    post_keys: postIds.map(id => ({ id }))
+  };
+
+  return inject(
+    async (resource, body) =>
+      fetch(resource, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+        body
+      }).then(async response =>
+        response.ok ? response.json() : Promise.reject(await response.json())
+      ),
+    [`https://www.tumblr.com/svc/blog/${blogName}/bulk_community_label_posts`, $.param(requestBody)]
+  );
+};

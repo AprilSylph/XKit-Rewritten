4a4e9cd7d4d3609fdeafdf9ed03e13db73350a11
Marcus <marcustyphoon@gmail.com>
2023-05-10 17:43:18 -0700

    use --badges-index to simplify

--- src/scripts/tweaks/badge_stack.js
+++ src/scripts/tweaks/badge_stack.js
@@ -1,13 +1,31 @@
 import { keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
-import { pageModifications } from '../../util/mutations.js';
-
-let styleElementMaxBadges = 0;
-const styleElement = buildStyle();
 
 const wrapper = `:is(article ${keyToCss('badgesContainer')})`;
 const badgeContainer = `${wrapper} > ${keyToCss('badgeContainer')}`;
 const badgeImage = `${badgeContainer} > :is(svg, img)`;
+
+const styleElement = buildStyle(`
+*:not(:hover) > ${wrapper} {
+  margin-right: 7px;
+}
+
+*:not(:hover) > ${badgeContainer} {
+  margin-right: -7px;
+}
+
+*:not(:hover) > ${badgeImage} {
+  filter: drop-shadow(1px 0px 2px rgb(0 0 0 / 0.5));
+}
+
+${wrapper} {
+  isolation: isolate;
+}
+
+${badgeContainer} {
+  z-index: calc(0 - var(--badges-index));
+}
+`);
 
 const transitionStyleElement = buildStyle(`
   ${wrapper}, ${badgeContainer} {
@@ -19,52 +37,15 @@ const transitionStyleElement = buildStyle(`
   }
 `);
 
-const updateStyleElement = maxBadges => {
-  styleElementMaxBadges = maxBadges;
-
-  const range = [...Array(styleElementMaxBadges).keys()].map(i => i + 1);
-  styleElement.textContent = `
-    *:not(:hover) > ${wrapper} {
-      margin-right: 7px;
-    }
-
-    *:not(:hover) > ${badgeContainer} {
-      margin-right: -7px;
-    }
-
-    *:not(:hover) > ${badgeImage} {
-      filter: drop-shadow(1px 0px 2px rgb(0 0 0 / 0.5));
-    }
-
-    ${wrapper} {
-      isolation: isolate;
-    }
-
-    ${range.map(i => `
-      ${badgeContainer}:nth-last-child(${i}) {
-        z-index: ${i};
-      }
-    `).join('')}
-  `;
-};
-
-const processWrappers = wrapperElements => {
-  const maxBadges = Math.max(...wrapperElements.map(el => el.children.length));
-  maxBadges > styleElementMaxBadges && updateStyleElement(maxBadges);
-};
-
 const waitForRender = () =>
   new Promise(resolve => requestAnimationFrame(() => requestAnimationFrame(resolve)));
 
 export const main = async () => {
   document.documentElement.append(styleElement);
-  pageModifications.register(wrapper, processWrappers);
-
   waitForRender().then(() => document.documentElement.append(transitionStyleElement));
 };
 
 export const clean = async () => {
-  pageModifications.unregister(processWrappers);
   styleElement.remove();
   transitionStyleElement.remove();
 };

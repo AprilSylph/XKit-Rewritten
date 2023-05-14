d170ade8de13a668ee81bb69ab21c3625bbf1028
Marcus <marcustyphoon@gmail.com>
2023-05-14 14:54:49 -0700

    copy postblock options
    Co-Authored-By: April Sylph <28949509+AprilSylph@users.noreply.github.com>

---
+++ src/scripts/notificationblock/options/index.js
@@ -0,0 +1,61 @@
+
+const postsBlockedCount = document.getElementById('posts-blocked-count');
+const blockedPostList = document.getElementById('blocked-posts');
+const blockedPostTemplate = document.getElementById('blocked-post');
+
+const storageKey = 'postblock.blockedPostRootIDs';
+const uuidsStorageKey = 'postblock.uuids';
+const toOpenStorageKey = 'postblock.toOpen';
+
+const unblockPost = async function ({ currentTarget }) {
+  let { [storageKey]: blockedPostRootIDs = [] } = await browser.storage.local.get(storageKey);
+
+  blockedPostRootIDs = blockedPostRootIDs.filter(id => id !== currentTarget.dataset.postId);
+  await browser.storage.local.set({ [storageKey]: blockedPostRootIDs });
+
+  currentTarget.remove();
+};
+
+const renderBlockedPosts = async function () {
+  const { [storageKey]: blockedPostRootIDs = [] } = await browser.storage.local.get(storageKey);
+  const { [uuidsStorageKey]: uuids = {} } = await browser.storage.local.get(uuidsStorageKey);
+
+  postsBlockedCount.textContent = `${blockedPostRootIDs.length} blocked ${blockedPostRootIDs.length === 1 ? 'post' : 'posts'}`;
+  blockedPostList.textContent = '';
+
+  for (const blockedPostID of blockedPostRootIDs) {
+    const templateClone = blockedPostTemplate.content.cloneNode(true);
+    const spanElement = templateClone.querySelector('span');
+    const unblockButton = templateClone.querySelector('button');
+
+    spanElement.textContent = blockedPostID;
+    unblockButton.dataset.postId = blockedPostID;
+    unblockButton.addEventListener('click', unblockPost);
+
+    if (uuids[blockedPostID]) {
+      const a = document.createElement('a');
+      a.href = 'javascript:void(0);';
+      a.addEventListener('click', async () => {
+        await browser.storage.local.set({
+          [toOpenStorageKey]: { uuid: uuids[blockedPostID], blockedPostID }
+        });
+        window.open('https://www.tumblr.com/');
+      });
+      spanElement.replaceWith(a);
+      a.append(spanElement);
+    }
+
+    blockedPostList.append(templateClone);
+  }
+};
+
+browser.storage.onChanged.addListener((changes, areaName) => {
+  if (
+    areaName === 'local' &&
+    (Object.keys(changes).includes(storageKey) || Object.keys(changes).includes(uuidsStorageKey))
+  ) {
+    renderBlockedPosts();
+  }
+});
+
+renderBlockedPosts();

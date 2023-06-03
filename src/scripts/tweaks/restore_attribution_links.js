aef0ad52fbf532316bc9cd637ebf3a8afdc0b16f
Rose Davidson <rose@metaclassical.com>
2023-06-03 13:53:39 -0400

    Restore post links in attribution header.

---
+++ src/scripts/tweaks/restore_attribution_links.js
@@ -0,0 +1,33 @@
+import { onNewPosts } from '../../util/mutations.js';
+import { filterPostElements } from '../../util/interface.js';
+import { keyToCss } from '../../util/css_map.js';
+import { timelineObject } from '../../util/react_props.js';
+
+const timeline = /\/v2\/timeline\/dashboard/;
+
+const postAttributionSelector = keyToCss('postAttribution');
+const reblogAttributionSelector = keyToCss('reblogAttribution');
+
+const processPosts = async function (postElements) {
+  filterPostElements(postElements, { timeline }).forEach(async postElement => {
+    const { postUrl, rebloggedFromUrl } = await timelineObject(postElement);
+    const postAttribution = postElement.querySelector(postAttributionSelector);
+    const postAttributionLink = postAttribution?.querySelector('a');
+    if (postAttributionLink !== undefined) {
+      postAttributionLink.href = postUrl;
+    }
+    const reblogAttribution = postElement.querySelector(reblogAttributionSelector);
+    const reblogAttributionLink = reblogAttribution?.querySelector('a');
+    if (reblogAttributionLink !== undefined && rebloggedFromUrl !== undefined) {
+      reblogAttributionLink.href = rebloggedFromUrl;
+    }
+  });
+};
+
+export const main = async function () {
+  onNewPosts.addListener(processPosts);
+};
+
+export const clean = async function () {
+  onNewPosts.removeListener(processPosts);
+};

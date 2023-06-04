51beb1b55d898b3fed8d792e075a3c55422f4aa0
Rose Davidson <rose@metaclassical.com>
2023-06-04 13:51:52 -0400

    Make attribution link selectors more readable.

--- src/scripts/tweaks/restore_attribution_links.js
+++ src/scripts/tweaks/restore_attribution_links.js
@@ -2,20 +2,18 @@ import { onNewPosts } from '../../util/mutations.js';
 import { keyToCss } from '../../util/css_map.js';
 import { timelineObject } from '../../util/react_props.js';
 
-const postAttributionSelector = keyToCss('postAttribution');
-const reblogAttributionSelector = keyToCss('reblogAttribution');
+const postAttributionLinkSelector = `${keyToCss('postAttribution')} a`;
+const reblogAttributionLinkSelector = `${keyToCss('reblogAttribution')} a`;
 
 const processPosts = async function (postElements) {
   postElements.forEach(async postElement => {
     const { postUrl, rebloggedFromUrl } = await timelineObject(postElement);
-    const postAttribution = postElement.querySelector(postAttributionSelector);
-    const postAttributionLink = postAttribution?.querySelector('a');
-    if (postAttributionLink !== undefined) {
+    const postAttributionLink = postElement.querySelector(postAttributionLinkSelector);
+    if (postAttributionLink !== null) {
       postAttributionLink.href = postUrl;
     }
-    const reblogAttribution = postElement.querySelector(reblogAttributionSelector);
-    const reblogAttributionLink = reblogAttribution?.querySelector('a');
-    if (reblogAttributionLink !== undefined && rebloggedFromUrl !== undefined) {
+    const reblogAttributionLink = postElement.querySelector(reblogAttributionLinkSelector);
+    if (reblogAttributionLink !== null && rebloggedFromUrl !== undefined) {
       reblogAttributionLink.href = rebloggedFromUrl;
     }
   });

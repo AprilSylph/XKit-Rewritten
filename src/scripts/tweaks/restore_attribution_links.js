4fc4028fb3a737e3d63a13126847de8b72caeeda
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2023-06-06 22:53:47 +0100

    add `navigate` calls

--- src/scripts/tweaks/restore_attribution_links.js
+++ src/scripts/tweaks/restore_attribution_links.js
@@ -1,20 +1,47 @@
 import { onNewPosts } from '../../util/mutations.js';
 import { keyToCss } from '../../util/css_map.js';
 import { timelineObject } from '../../util/react_props.js';
+import { navigate } from '../../util/tumblr_helpers.js';
 
 const postAttributionLinkSelector = `${keyToCss('postAttribution')} a`;
 const reblogAttributionLinkSelector = `${keyToCss('reblogAttribution')} a`;
 
+const onLinkClick = event => {
+  event.stopPropagation();
+  if (event.ctrlKey || event.metaKey || event.altKey || event.shiftKey) return;
+
+  const { blogName, postId } = event.currentTarget.dataset;
+  if (blogName && postId) {
+    event.preventDefault();
+    navigate(`/@${blogName}/${postId}`);
+  }
+};
+
 const processPosts = async function (postElements) {
   postElements.forEach(async postElement => {
-    const { postUrl, rebloggedFromUrl } = await timelineObject(postElement);
+    const {
+      blogName,
+      id,
+      postUrl,
+      rebloggedFromId,
+      rebloggedFromName,
+      rebloggedFromUrl
+    } = await timelineObject(postElement);
     const postAttributionLink = postElement.querySelector(postAttributionLinkSelector);
+
     if (postAttributionLink !== null) {
       postAttributionLink.href = postUrl;
+      postAttributionLink.dataset.blogName = blogName;
+      postAttributionLink.dataset.postId = id;
+      postAttributionLink.addEventListener('click', onLinkClick, { capture: true });
     }
+
     const reblogAttributionLink = postElement.querySelector(reblogAttributionLinkSelector);
     if (reblogAttributionLink !== null && rebloggedFromUrl !== undefined) {
       reblogAttributionLink.href = rebloggedFromUrl;
+      reblogAttributionLink.dataset.blogName = rebloggedFromName;
+      reblogAttributionLink.dataset.postId = rebloggedFromId;
+      reblogAttributionLink.addEventListener('click', onLinkClick, { capture: true });
     }
   });
 };

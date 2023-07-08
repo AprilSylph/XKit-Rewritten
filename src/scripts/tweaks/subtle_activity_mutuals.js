dcecf410ad715f9e436e39a2cf31e7ceb7b9432e
Marcus <marcustyphoon@gmail.com>
2023-07-08 00:48:29 -0700

    Create subtle_activity_mutuals and hide_activity_mutuals.js

---
+++ src/scripts/tweaks/subtle_activity_mutuals.js
@@ -0,0 +1,12 @@
+import { keyToCss } from '../../util/css_map.js';
+import { buildStyle } from '../../util/interface.js';
+
+const styleElement = buildStyle(`
+${keyToCss('activity')} ${keyToCss('generalLabelContainer')} {
+  color: RGB(var(--black));
+  background-color: RGB(var(--black), 0.07);
+}
+`);
+
+export const main = async () => document.documentElement.append(styleElement);
+export const clean = async () => styleElement.remove();

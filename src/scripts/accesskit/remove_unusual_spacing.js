01db7164223d46318ec785291299e818857574e1
Marcus <marcustyphoon@gmail.com>
2023-07-29 04:07:57 -0700

    cleanup

--- src/scripts/accesskit/remove_unusual_spacing.js
+++ src/scripts/accesskit/remove_unusual_spacing.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import {
   cloneControlButton,
   createControlButtonTemplate
@@ -7,7 +6,7 @@ import { keyToCss } from '../../util/css_map.js';
 import { buildStyle, postSelector } from '../../util/interface.js';
 import { pageModifications } from '../../util/mutations.js';
 
-const oldTextClass = 'accesskit-unusual-spacing';
+const oldTextClass = 'accesskit-has-unusual-spacing';
 const newTextClass = 'accesskit-removed-unusual-spacing';
 
 const enableButtonClass = 'accesskit-unusual-spacing-enable-button';
@@ -40,35 +39,17 @@ const styleElement = buildStyle(`
 [data-remove-unusual-spacing="off"] .${disableButtonClass} {
   display: none;
 }
-.${disableButtonClass} svg {
-  fill: rgba(var(--black));
-}
-
-/* temp */
-[data-remove-unusual-spacing="on"] .${oldTextClass}, [data-remove-unusual-spacing="off"] .${newTextClass}  { background: rgba(255, 0, 0, 0.05); }
-[data-remove-unusual-spacing="on"] .${newTextClass}, [data-remove-unusual-spacing="off"] .${oldTextClass}  { background: rgba(0, 255, 0, 0.05); }
 `);
 
-/*
-\s            whitespace regex: [\r\n\t\f\v \u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]
-
-\r            <Carriage Return> (CR)
-\n            <End of Line> (EOL, LF, NL)
-\t            <Character Tabulation> (HT, TAB)
-\f            <Form Feed> (FF)
-\v            <Line Tabulation> (VT)
-              Space (SP)
-\u00a0        No-Break Space (NBSP)
-\u1680        Ogham Space Mark
-\u2000-\u200a En Quad, Em Quad, En Space, Em Space, Three-Per-Em Space, Four-Per-Em Space, Six-Per-Em Space, Figure Space, Punctuation Space, Thin Space, Hair Space
-\u2028        Line Separator
-\u2029        Paragraph Separator
-\u202f        Narrow No-Break Space (NNBSP)
-\u205f        Medium Mathematical Space (MMSP)
-\u3000        Ideographic Space
-\ufeff        Zero Width No-Break Space (BOM, ZWNBSP)
-*/
-
+/**
+ * javascript regular expressions lack \h (horizontal whitespace)
+ *
+ * [\s]: whitespace (includes line break characters)
+ * [^\S]: not not whitespace; equivalent to [\s]
+ *
+ * \r\n\u2028\u2029: unambiguous line break characters
+ * [^\S\r\n\u2028\u2029]: whitespace excluding those line break characters
+ */
 const gapWithinSentence = /(?<=\w)[^\S\r\n\u2028\u2029]{2,}/g;
 const unusualWhitespace = /[^ \S\r\n\u2028\u2029]/g;
 
@@ -97,16 +78,16 @@ const processPostContainers = postContainers =>
   postContainers.forEach(postContainer => {
     let postEdited = false;
 
-    [...postContainer.querySelectorAll(keyToCss('rows'))].forEach(rows => {
+    [...postContainer.querySelectorAll(keyToCss('rows'))].forEach(rowsElement => {
       let gapWithinSentenceCount = 0;
 
-      const textBlocksToProcess = [...rows.querySelectorAll(keyToCss('textBlock'))].filter(
-        textBlock => {
-          const count = textBlock.innerText.match(gapWithinSentence)?.length ?? 0;
-          gapWithinSentenceCount += count;
-          return count;
-        }
-      );
+      const textBlocksToProcess = [
+        ...rowsElement.querySelectorAll(keyToCss('textBlock'))
+      ].filter(textBlock => {
+        const count = textBlock.innerText.match(gapWithinSentence)?.length ?? 0;
+        gapWithinSentenceCount += count;
+        return count;
+      });
 
       if (gapWithinSentenceCount < 3) return;
 
@@ -120,13 +101,10 @@ const processPostContainers = postContainers =>
         textBlock.classList.add(oldTextClass);
         newTextBlock.classList.add(newTextClass);
 
-        const nodeIterator = document.createTreeWalker(newTextBlock, NodeFilter.SHOW_TEXT);
+        const treeWalker = document.createTreeWalker(newTextBlock, NodeFilter.SHOW_TEXT);
         let textNode;
-        while ((textNode = nodeIterator.nextNode())) {
+        while ((textNode = treeWalker.nextNode())) {
           textNode.nodeValue = textNode.nodeValue.replaceAll(unusualWhitespace, ' ');
-
-          // textNode.nodeValue = textNode.nodeValue.replaceAll('❝', '“');
-          // textNode.nodeValue = textNode.nodeValue.replaceAll('❞', '”');
         }
       });
     });
@@ -154,7 +132,8 @@ export const main = async () => {
 export const clean = async () => {
   pageModifications.unregister(processPostContainers);
   styleElement.remove();
+
   $(`.${oldTextClass}`).removeClass(oldTextClass);
-  $(`.${newTextClass}, .${enableButtonClass}, .${disableButtonClass}`).remove();
   $('[data-remove-unusual-spacing]').removeAttr('data-remove-unusual-spacing');
+  $(`.${newTextClass}, .${enableButtonClass}, .${disableButtonClass}`).remove();
 };

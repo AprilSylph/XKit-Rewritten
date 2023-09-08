86c0091d90b379278dd4ec0ebe94a3d3cd3b6bfb
Marcus <marcustyphoon@gmail.com>
2023-09-08 03:01:54 -0700

    refine dom structure/css

--- src/util/control_buttons.js
+++ src/util/control_buttons.js
@@ -6,62 +6,44 @@ import { buildSvg } from './remixicon.js';
 $('.xkit-control-button-container').remove();
 
 document.documentElement.append(buildStyle(`
-:not(:hover) > .xkit-control-button-tooltip{
+:not(:hover) > .xkit-control-button-tooltip {
   display: none;
 }
+
 .xkit-control-button-tooltip {
+  position: absolute;
+  bottom: 50%;
+  left: 50%;
+  transform: translate(-50%, -20.5px);
+
   z-index: 2;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-.xkit-control-button-tooltip-popover-holder {
-  position: absolute;
-  inset: auto auto 0px 0px;
-  transform: translate3d(-23px, -10px, 0px);
+  pointer-events: none;
 }
 
-.xkit-control-button-tooltip-tooltip {
-  pointer-events: none;
-  font-family: var(--font-family);
-  overflow-wrap: break-word;
-  word-wrap: break-word;
-  -webkit-hyphens: auto;
-  hyphens: auto;
-  color: #fff;
-  text-align: center;
-  background-color: #000;
-  border-radius: 4px;
+.xkit-control-button-tooltip-box {
   min-width: 54px;
   padding: 4px 8px;
+
+  border-radius: 4px;
+  box-shadow: 0 0 4px rgba(0,0,0,.25);
+
+  background-color: black;
+  color: white;
+
   font-size: .875rem;
   font-weight: 500;
   line-height: 20px;
-  position: relative;
-  box-shadow: 0 0 4px rgba(0,0,0,.25);
+  text-align: center;
 }
-.xkit-control-button-tooltip-arrow {
-  position: absolute;
-  left: 0px;
-  transform: translate3d(27px, 0px, 0px);
-  bottom: -16px;
-  pointer-events: none;
-  width: 16px;
-  height: 16px;
-}
-.xkit-control-button-tooltip-arrow::after {
-  content: "";
-  pointer-events: none;
-  border: 8px solid transparent;
-  width: 0;
-  height: 0;
-  position: absolute;
 
-  border-top-color: #000;
-  border-bottom: none;
-  top: 0;
-  left: 0;
+.xkit-control-button-tooltip::after {
+  content: "";
+  position: absolute;
+  left: 50%;
+  transform: translateX(-8px);
+
+  border: 8px solid transparent;
+  border-top-color: black;
 }
 `));
 
@@ -75,10 +57,7 @@ document.documentElement.append(buildStyle(`
 export const createControlButtonTemplate = function (symbolId, buttonClass, tooltip) {
   return dom('div', { class: `xkit-control-button-container ${buttonClass}` }, null, [
     dom('div', { class: 'xkit-control-button-tooltip' }, null, [
-      dom('div', { class: 'xkit-control-button-tooltip-popover-holder' }, null, [
-        dom('div', { class: 'xkit-control-button-tooltip-tooltip' }, null, [tooltip]),
-        dom('div', { class: 'xkit-control-button-tooltip-arrow ' })
-      ])
+      dom('div', { class: 'xkit-control-button-tooltip-box' }, null, [tooltip])
     ]),
     dom('button', { class: 'xkit-control-button' }, null, [
       dom('span', { class: 'xkit-control-button-inner', tabindex: '-1' }, null, [

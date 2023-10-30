4069712eceb2e7a62353199c828922348c8e5127
marcustyphoon <marcustyphoon@gmail.com>
2023-10-30 15:42:02 -0700

    No Recommended: Fix blog sidebar/radar hiding (#1318)

--- src/scripts/no_recommended/hide_recommended_blogs.js
+++ src/scripts/no_recommended/hide_recommended_blogs.js
@@ -10,13 +10,13 @@ const styleElement = buildStyle(`[${hiddenAttribute}] { display: none; }`);
 const hideDashboardRecommended = function (sidebarTitles) {
   sidebarTitles
     .filter(h1 => h1.textContent === translate('Check out these blogs'))
-    .forEach(h1 => h1.parentNode.setAttribute(hiddenAttribute, ''));
+    .forEach(h1 => h1.closest('aside > *').setAttribute(hiddenAttribute, ''));
 };
 
 const hideTagPageRecommended = topBlogsLists => topBlogsLists.forEach(ul => ul.setAttribute(hiddenAttribute, ''));
 
 export const main = async function () {
-  pageModifications.register('aside > div > h1', hideDashboardRecommended);
+  pageModifications.register('aside h1', hideDashboardRecommended);
 
   const topBlogsSelector = `${keyToCss('desktopContainer')} > ${keyToCss('recommendedBlogs')}`;
   pageModifications.register(topBlogsSelector, hideTagPageRecommended);

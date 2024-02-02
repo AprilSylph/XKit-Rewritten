dfce8e94cddde47f67eb248f6f134686a0dfb787
Marcus <marcustyphoon@gmail.com>
2024-02-01 18:38:42 -0800

    apply both tweaks to mobile menu badge

--- src/scripts/tweaks/hide_following_notification_badge.js
+++ src/scripts/tweaks/hide_following_notification_badge.js
@@ -3,9 +3,10 @@ import { buildStyle } from '../../util/interface.js';
 import { translate } from '../../util/language_data.js';
 
 const followingHomeButton = `:is(li[title="${translate('Home')}"], button[aria-label="${translate('Home')}"], a[href="/dashboard/following"])`;
+const mobileMenuButton = `button[aria-label="${translate('Menu')}"]`;
 
 const styleElement = buildStyle(`
-${followingHomeButton} ${keyToCss('notificationBadge')} {
+:is(${followingHomeButton}, ${mobileMenuButton}) ${keyToCss('notificationBadge')} {
   display: none;
 }
 `);

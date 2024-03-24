744803ba46df968ff5e8851d1af80260581d62e1
Marcus <marcustyphoon@gmail.com>
2024-03-24 04:04:33 -0700

    format css

--- src/scripts/tweaks/hide_follower_counts.js
+++ src/scripts/tweaks/hide_follower_counts.js
@@ -2,7 +2,13 @@ import { keyToClasses } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 
 const countSelector = keyToClasses('count').map(className => `a[href$="/followers"] .${className}`).join(',');
-const styleElement = buildStyle(`${countSelector} { visibility: hidden; } a[href$="/activity/total"] { display: none; }`);
+const styleElement = buildStyle(`
+${countSelector} {
+  visibility: hidden;
+}
+a[href$="/activity/total"] {
+  display: none;
+}`);
 
 export const main = async () => document.documentElement.append(styleElement);
 export const clean = async () => styleElement.remove();

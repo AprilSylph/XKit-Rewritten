ac89adc8a440c38123dd1a87295f6f320ef6d758
Marcus <marcustyphoon@gmail.com>
2024-03-24 04:11:15 -0700

    use `keyToCss`

--- src/scripts/tweaks/no_focus_shadow.js
+++ src/scripts/tweaks/no_focus_shadow.js
@@ -1,9 +1,8 @@
-import { keyToClasses } from '../../util/css_map.js';
+import { keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 
-const selector = keyToClasses('listTimelineObject').map(className => `.${className}:focus > div`).join(',');
 const styleElement = buildStyle(`
-${selector} {
+${keyToCss('listTimelineObject')}:focus > div {
   box-shadow: none !important;
 }`);
 

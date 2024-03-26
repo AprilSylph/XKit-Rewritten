7bd7b1231f0088f867fcd814fa2d33b46aeaab02
Marcus <marcustyphoon@gmail.com>
2024-03-26 02:30:17 -0700

    fix selector

--- src/scripts/tweaks/subtle_activity_mutuals.js
+++ src/scripts/tweaks/subtle_activity_mutuals.js
@@ -3,7 +3,7 @@ import { keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 import { dom } from '../../util/dom.js';
 
-const labelSelector = keyToCss('generalLabelContainer');
+const labelSelector = keyToCss('followingBadgeContainer', 'mutualsBadgeContainer');
 
 const spanClass = 'xkit-tweaks-subtle-activity-span';
 

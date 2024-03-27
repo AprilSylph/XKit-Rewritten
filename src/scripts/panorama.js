23da43eafd080b4baabb96d1465014ee12095bed
Marcus <marcustyphoon@gmail.com>
2024-03-26 18:15:31 -0700

    significant rewrite; add expand toggle

--- src/scripts/panorama.js
+++ src/scripts/panorama.js
@@ -1,102 +1,100 @@
 import { keyToCss } from '../util/css_map.js';
-import { buildStyle } from '../util/interface.js';
+import { buildStyle, postSelector } from '../util/interface.js';
 import { pageModifications } from '../util/mutations.js';
 import { getPreferences } from '../util/preferences.js';
 
 const navigationWrapperBasis = 240;
 const navigationWrapperMargin = 20;
-const mainContentWrapperBasis = 966;
+const mainContentWrapperMinWidth = 902;
+const wideDashMinWidth = navigationWrapperBasis + navigationWrapperMargin + mainContentWrapperMinWidth;
 
-const styleElement = buildStyle();
+const sidebarMaxWidth = 320;
+const mainRightPadding = 20;
+const mainRightBorder = 1;
+const sidebarOffset = sidebarMaxWidth + mainRightPadding + mainRightBorder;
 
-const mainContentWrapper = `${keyToCss('mainContentWrapper')}:not(${keyToCss('mainContentIsMasonry', 'mainContentIs4ColumnMasonry')})`;
-const container = `${mainContentWrapper} > ${keyToCss('container')}`;
-const mainElement = `${container} > ${keyToCss('main')}`;
-const mainPostColumn = `${mainElement} > ${keyToCss('postColumn', 'postsColumn')}`;
+const expandClass = 'xkit-panorama-expand-media';
+const maxPostWidthVar = '--xkit-panorama-post-width';
+const aspectRatioVar = '--xkit-panorama-aspect-ratio';
+
+const mainContentWrapper =
+  `${keyToCss('mainContentWrapper')}:not(${keyToCss('mainContentIsMasonry', 'mainContentIsFullWidth')})`;
+const mainPostColumn = `main${keyToCss('postColumn', 'postsColumn')}`;
 const patioPostColumn = `[id]${keyToCss('columnWide')}`;
 
-const togglePanorama = async () => {
-  const hasMainColumn = document.querySelector(mainPostColumn);
-  const hasPatioColumn = document.querySelector(patioPostColumn);
+const wideDashStyleElement = buildStyle(`
+${mainContentWrapper}:has(> div > div > ${mainPostColumn}) {
+  flex-grow: 1;
+  max-width: calc(var(${maxPostWidthVar}) + ${sidebarOffset}px);
+}
+${mainContentWrapper} > div:has(> div > ${mainPostColumn}) {
+  max-width: unset;
+}
+${mainContentWrapper} > div > div:has(> ${mainPostColumn}) {
+  max-width: calc(100% - ${sidebarOffset}px);
+}
+${mainContentWrapper} > div > div > ${mainPostColumn} {
+  max-width: 100%;
+}
+${keyToCss('queueSettings')} {
+  box-sizing: border-box;
+  width: 100%;
+}
+`);
+wideDashStyleElement.media = `(min-width: ${wideDashMinWidth}px)`;
 
-  if (hasMainColumn || hasPatioColumn) {
-    styleElement.media = hasPatioColumn
-      ? ''
-      : `(min-width: ${navigationWrapperBasis + navigationWrapperMargin + mainContentWrapperBasis}px)`;
+const styleElement = buildStyle(`
+${patioPostColumn} {
+  width: min(var(${maxPostWidthVar}), 100vw);
+}
 
-    const { maxPostWidth: maxPostWidthString } = await getPreferences('panorama');
-    const maxPostWidth = Number(maxPostWidthString.trim().replace('px', '')) || 0;
+/* Widen posts */
+${keyToCss('cell')}, ${postSelector}
+  :is(
+    article,
+    article > header,
+    article ${keyToCss('reblog')}
+  ) {
+  max-width: unset;
+}
 
-    const sidebarMaxWidth = 320;
-    const mainRightPadding = 20;
-    const mainRightBorder = 1;
-    const sidebarOffset = sidebarMaxWidth + mainRightPadding + mainRightBorder;
+/* Center non-expanded content */
+body:not(.${expandClass}) ${postSelector}
+  :is(
+    ${keyToCss('videoBlock', 'audioBlock', 'link', 'pollBlock', 'imageBlockLink')},
+    figure${keyToCss('imageBlock')}:not(${keyToCss('unstretched')})
+  ) {
+  margin: 0 auto;
+  max-width: 540px;
+}
 
-    const postColumn = hasPatioColumn ? patioPostColumn : mainPostColumn;
+/* Widen + lock aspect ratios of expanded content */
+body.${expandClass} ${postSelector}
+  :is(
+    ${keyToCss('videoBlock', 'audioBlock', 'link', 'pollBlock')},
+    ${keyToCss('videoBlock')} iframe
+  ) {
+  max-width: unset !important;
+}
+body.${expandClass} ${postSelector} ${keyToCss('videoBlock')} iframe[style*="${aspectRatioVar}"] {
+  aspect-ratio: var(${aspectRatioVar});
+  height: unset !important;
+}
+body.${expandClass} ${postSelector} a > ${keyToCss('withImage')} {
+  aspect-ratio: 2;
+  height: unset !important;
+}
 
-    styleElement.textContent = hasPatioColumn
-      ? `
-        ${postColumn} {
-          width: min(${Math.max(maxPostWidth, 540)}px, 100vw);
-        }
-      `
-      : `
-        ${mainContentWrapper} {
-          flex-grow: 1;
-          max-width: ${Math.max(maxPostWidth, 540) + sidebarOffset}px;
-        }
-        ${container} {
-          max-width: unset;
-        }
-        ${mainElement} {
-          max-width: calc(100% - ${sidebarOffset}px);
-        }
-        ${postColumn} {
-          max-width: 100%;
-        }
-      `;
-
-    styleElement.textContent += `
-      ${postColumn}
-        :is(
-          ${keyToCss('cell')},
-          article,
-          article > header,
-          article ${keyToCss('reblog', 'videoBlock', 'audioBlock', 'link')}
-        ) {
-        max-width: 100%;
-      }
-
-      ${postColumn} article ${keyToCss('link')} ${keyToCss('header')}${keyToCss('withImage')} {
-        height: unset;
-        aspect-ratio: 2;
-      }
-      ${postColumn} article ${keyToCss('videoBlock')} iframe {
-        max-width: none !important;
-      }
-      ${postColumn}
-        :is(
-          [data-is-resizable="true"][style="width: 540px;"],
-          ${keyToCss('takeoverBanner')}
-        ) {
-        width: unset !important;
-      }
-      ${keyToCss('queueSettings')} {
-        box-sizing: border-box;
-        width: 100%;
-      }
-      iframe[style*="${aspectRatioVar}"] {
-        aspect-ratio: var(${aspectRatioVar});
-        height: unset !important;
-      }
-    `;
-    document.documentElement.append(styleElement);
-  } else {
-    styleElement.remove();
-  }
-};
-
-const aspectRatioVar = '--panorama-aspect-ratio';
+/* Fix ad containers */
+${keyToCss('adTimelineObject', 'instreamAd', 'nativeIponWebAd', 'takeoverBanner')},
+${keyToCss('adTimelineObject', 'instreamAd', 'nativeIponWebAd', 'takeoverBanner')} header {
+  max-width: unset;
+}
+[data-is-resizable="true"][style="width: 540px;"],
+${keyToCss('takeoverBanner')} {
+  width: unset !important;
+}
+`);
 
 const processVideoIframes = iframes => iframes.forEach(iframe => {
   const { maxWidth, height } = iframe.style;
@@ -109,24 +107,32 @@ const processVideoIframes = iframes => iframes.forEach(iframe => {
 });
 
 export const onStorageChanged = async (changes, areaName) =>
-  Object.keys(changes).some(key => key.startsWith('panorama')) && togglePanorama();
+  Object.keys(changes).some(key => key.startsWith('panorama')) && main();
 
 export const main = async () => {
-  pageModifications.register(`:is(${mainContentWrapper}, ${mainPostColumn}, ${patioPostColumn})`, togglePanorama);
+  const { maxPostWidth: maxPostWidthString, expandPostMedia } = await getPreferences('panorama');
+
+  const maxPostWidth = Number(maxPostWidthString.trim().replace('px', '')) || 0;
+  document.body.style.setProperty(maxPostWidthVar, `${Math.max(maxPostWidth, 540)}px`);
+  document.body.classList[expandPostMedia ? 'add' : 'remove'](expandClass);
+
+  document.documentElement.append(styleElement, wideDashStyleElement);
 
   pageModifications.register(
-    `${keyToCss('videoBlock')} iframe[style*="max-width"][style*="height"]`,
+    `${postSelector} ${keyToCss('videoBlock')} iframe[style*="max-width"][style*="height"]`,
     processVideoIframes
   );
 };
 
 export const clean = async () => {
-  pageModifications.unregister(togglePanorama);
-
   pageModifications.unregister(processVideoIframes);
   [...document.querySelectorAll(`iframe[style*="${aspectRatioVar}"]`)].forEach(el =>
     el.style.removeProperty(aspectRatioVar)
   );
 
+  document.body.style.removeProperty(maxPostWidthVar);
+  document.body.classList.remove(expandClass);
+
   styleElement.remove();
+  wideDashStyleElement.remove();
 };

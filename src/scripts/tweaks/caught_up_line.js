2be704b1bd236747e0c82859b4c29c86eba96af3
marcustyphoon <marcustyphoon@gmail.com>
2024-04-29 01:12:25 -0700

    Tweaks: Fix carousel hiding affecting "your tags" tab (#1426)

--- src/scripts/tweaks/caught_up_line.js
+++ src/scripts/tweaks/caught_up_line.js
@@ -23,7 +23,7 @@ const styleElement = buildStyle(`
 `);
 
 const listTimelineObjectSelector = keyToCss('listTimelineObject');
-const tagChicletCarouselLinkSelector = `${listTimelineObjectSelector} ${keyToCss('tagChicletLink')}`;
+const tagChicletCarouselLinkSelector = `[data-timeline="/v2/timeline/dashboard"] ${listTimelineObjectSelector} ${keyToCss('tagChicletLink')}`;
 
 const createCaughtUpLine = tagChicletCarouselItems => tagChicletCarouselItems
   .map(getTimelineItemWrapper)

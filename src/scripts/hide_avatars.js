19ce91fca7da098968e92500f08100e47e85eff8
Marcus <marcustyphoon@gmail.com>
2024-05-24 05:45:03 -0700

    Update hide_avatars.js

--- src/scripts/hide_avatars.js
+++ src/scripts/hide_avatars.js
@@ -2,7 +2,7 @@ import { getPreferences } from '../util/preferences.js';
 import { translate } from '../util/language_data.js';
 import { buildStyle } from '../util/interface.js';
 
-const styleElement = buildStyle();
+export const styleElement = buildStyle();
 
 export const main = async function () {
   const { hiddenAvatars } = await getPreferences('hide_avatars');
@@ -11,10 +11,4 @@ export const main = async function () {
     .split(',')
     .map(username => `[title="${username.trim()}"] img[alt="${translate('Avatar')}"] { display: none; }`)
     .join('\n');
-
-  document.documentElement.append(styleElement);
-};
-
-export const clean = async function () {
-  styleElement.remove();
 };

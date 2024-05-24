adc6a90753d88ffddb5885d90306e8172f775d8b
Marcus <marcustyphoon@gmail.com>
2024-05-24 05:44:05 -0700

    refactor accesskit scripts

--- src/scripts/accesskit/no_user_fonts.js
+++ src/scripts/accesskit/no_user_fonts.js
@@ -1,8 +1,8 @@
 import { keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 
-const selector = `${keyToCss('textBlock')} ${keyToCss('quote', 'chat', 'quirky')}`;
-const styleElement = buildStyle(`${selector} { font-family: var(--font-family); }`);
-
-export const main = async () => document.documentElement.append(styleElement);
-export const clean = async () => styleElement.remove();
+export const styleElement = buildStyle(`
+${keyToCss('textBlock')} ${keyToCss('quote', 'chat', 'quirky')} {
+  font-family: var(--font-family);
+}
+`);

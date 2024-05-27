acad51210e4e43d5ac583d4902cf0c31869a09f3
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2024-05-27 13:22:02 +0100

    path-based `inject()`

--- src/utils/inject.js
+++ src/utils/inject.js
@@ -1,60 +1,39 @@
 import { dom } from './dom.js';
 
+const { getURL } = browser.runtime;
 const { nonce } = [...document.scripts].find(script => script.getAttributeNames().includes('nonce'));
-const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
 
 /**
- * @param {Function} func - Function to run in the page context (can be async)
- * @param {Array} [args] - Array of arguments to pass to the function via spread
- * @param {Element} [target] - Element to append script to; will be accessible as
- *                             document.currentScript.parentElement in the injected function.
- * @returns {Promise<any>} The return value of the function, or the caught exception
+ * @param {string} path - Absolute path of script to inject (will be fed to `runtime.getURL()`)
+ * @param {Array} [args] - Array of arguments to pass to the script
+ * @param {Element} [target] - Element to append the `<script>` to; will be accessible as
+ *                             `document.currentScript.parentElement` in the injected script
+ * @returns {Promise<any>} The transmitted result of the script
  */
-export const inject = async (func, args = [], target = document.documentElement) => {
-  const name = `xkit$${func.name || 'injected'}`;
-  const async = func instanceof AsyncFunction;
+export const inject = async (path, args = [], target = document.documentElement) => {
+  const script = dom('script', {
+    'data-arguments': JSON.stringify(args),
+    nonce,
+    src: getURL(path)
+  });
 
-  const script = dom('script', { nonce }, null, [`{
-    const { dataset } = document.currentScript;
-    const ${name} = ${func.toString()};
-    const returnValue = ${name}(...${JSON.stringify(args)});
-    ${async
-      ? `returnValue
-          .then(result => { dataset.result = JSON.stringify(result || null); })
-          .catch(exception => { dataset.exception = JSON.stringify({
-            message: exception.message,
-            name: exception.name,
-            stack: exception.stack,
-            ...exception
-          })})
-        `
-      : 'dataset.result = JSON.stringify(returnValue || null);'
-    }
-  }`]);
-
-  if (async) {
-    return new Promise((resolve, reject) => {
-      const attributeObserver = new MutationObserver((mutations, observer) => {
-        if (mutations.some(({ attributeName }) => attributeName === 'data-result')) {
-          observer.disconnect();
-          resolve(JSON.parse(script.dataset.result));
-          script.remove();
-        } else if (mutations.some(({ attributeName }) => attributeName === 'data-exception')) {
-          observer.disconnect();
-          reject(JSON.parse(script.dataset.exception));
-          script.remove();
-        }
-      });
-
-      attributeObserver.observe(script, {
-        attributes: true,
-        attributeFilter: ['data-result', 'data-exception']
-      });
-      target.append(script);
+  return new Promise((resolve, reject) => {
+    const attributeObserver = new MutationObserver((mutations, observer) => {
+      if (mutations.some(({ attributeName }) => attributeName === 'data-result')) {
+        observer.disconnect();
+        resolve(JSON.parse(script.dataset.result));
+        script.remove();
+      } else if (mutations.some(({ attributeName }) => attributeName === 'data-exception')) {
+        observer.disconnect();
+        reject(JSON.parse(script.dataset.exception));
+        script.remove();
+      }
     });
-  } else {
+
+    attributeObserver.observe(script, {
+      attributes: true,
+      attributeFilter: ['data-result', 'data-exception']
+    });
     target.append(script);
-    script.remove();
-    return JSON.parse(script.dataset.result);
-  }
+  });
 };

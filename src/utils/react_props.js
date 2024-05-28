cd1cf2e162313f94acfacc49e57de8c621a33479
Marcus <marcustyphoon@gmail.com>
2024-05-28 02:43:03 -0700

    migrate missed utils to path-based `inject()`

--- src/utils/react_props.js
+++ src/utils/react_props.js
@@ -18,28 +18,16 @@ export const timelineObject = async function (postElement) {
   return timelineObjectCache.get(postElement);
 };
 
-const unburyNotification = async () => {
-  const notificationElement = document.currentScript.parentElement;
-  const reactKey = Object.keys(notificationElement).find(key => key.startsWith('__reactFiber'));
-  let fiber = notificationElement[reactKey];
-
-  while (fiber !== null) {
-    const { notification } = fiber.memoizedProps || {};
-    if (notification !== undefined) {
-      return notification;
-    } else {
-      fiber = fiber.return;
-    }
-  }
-};
-
 /**
  * @param {Element} notificationElement - An on-screen notification
  * @returns {Promise<object>} - The notification's buried notification property
  */
 export const notificationObject = function (notificationElement) {
   if (!notificationObjectCache.has(notificationElement)) {
-    notificationObjectCache.set(notificationElement, inject(unburyNotification, [], notificationElement));
+    notificationObjectCache.set(
+      notificationElement,
+      inject('/utils/inject/unbury_notification.js', [], notificationElement)
+    );
   }
   return notificationObjectCache.get(notificationElement);
 };
@@ -70,28 +58,6 @@ export const isMyPost = async (postElement) => {
   return false;
 };
 
-const controlTagsInput = async ({ add, remove }) => {
-  add = add.map(tag => tag.trim()).filter((tag, index, array) => array.indexOf(tag) === index);
-
-  const selectedTagsElement = document.getElementById('selected-tags');
-  if (!selectedTagsElement) { return; }
-
-  const reactKey = Object.keys(selectedTagsElement).find(key => key.startsWith('__reactFiber'));
-  let fiber = selectedTagsElement[reactKey];
-
-  while (fiber !== null) {
-    let tags = fiber.stateNode?.state?.tags;
-    if (Array.isArray(tags)) {
-      tags.push(...add.filter(tag => tags.includes(tag) === false));
-      tags = tags.filter(tag => remove.includes(tag) === false);
-      fiber.stateNode.setState({ tags });
-      break;
-    } else {
-      fiber = fiber.return;
-    }
-  }
-};
-
 /**
  * Manipulate post form tags
  * @param {object} options - Tags to add/remove to/from the current post form
@@ -99,4 +65,5 @@ const controlTagsInput = async ({ add, remove }) => {
  * @param {string[]} [options.remove] - Tags to remove
  * @returns {Promise<void>} Resolves when finished
  */
-export const editPostFormTags = async ({ add = [], remove = [] }) => inject(controlTagsInput, [{ add, remove }]);
+export const editPostFormTags = async ({ add = [], remove = [] }) =>
+  inject('/utils/inject/control_tags_input.js', [{ add, remove }]);

4116b9fc135735f4456df1dfe0371d33ae917078
Marcus <marcustyphoon@gmail.com>
2024-05-27 19:16:16 -0700

    use `script type="module"`

--- src/page_context.js
+++ src/page_context.js
@@ -1,34 +1,30 @@
-'use strict';
+const moduleCache = {};
 
-{
-  const moduleCache = {};
+document.documentElement.addEventListener('xkitinjectionrequest', async event => {
+  const { detail, target } = event;
+  const { id, path, args } = JSON.parse(detail);
 
-  document.documentElement.addEventListener('xkitinjectionrequest', async event => {
-    const { detail, target } = event;
-    const { id, path, args } = JSON.parse(detail);
+  try {
+    moduleCache[path] ??= await import(path);
+    const func = moduleCache[path].default;
 
-    try {
-      moduleCache[path] ??= await import(path);
-      const func = moduleCache[path].default;
-
-      const result = await (func.bind(target))(...args);
-      target.dispatchEvent(
-        new CustomEvent('xkitinjectionresponse', { detail: JSON.stringify({ id, result }) })
-      );
-    } catch (exception) {
-      target.dispatchEvent(
-        new CustomEvent('xkitinjectionresponse', {
-          detail: JSON.stringify({
-            id,
-            exception: {
-              message: exception.message,
-              name: exception.name,
-              stack: exception.stack,
-              ...exception
-            }
-          })
+    const result = await (func.bind(target))(...args);
+    target.dispatchEvent(
+      new CustomEvent('xkitinjectionresponse', { detail: JSON.stringify({ id, result }) })
+    );
+  } catch (exception) {
+    target.dispatchEvent(
+      new CustomEvent('xkitinjectionresponse', {
+        detail: JSON.stringify({
+          id,
+          exception: {
+            message: exception.message,
+            name: exception.name,
+            stack: exception.stack,
+            ...exception
+          }
         })
-      );
-    }
-  });
-}
+      })
+    );
+  }
+});

34d4bca838a49822d2b358756e23b4c054a84158
Marcus <marcustyphoon@gmail.com>
2024-06-10 01:56:46 -0700

    remove tumblr patio processing for now

--- src/util/timeline_id.js
+++ src/util/timeline_id.js
@@ -6,29 +6,25 @@ const anyBlog = '[a-z0-9-]{1,32}';
 
 export const followingTimelineFilter = ({ dataset: { timeline, timelineId } }) =>
   timeline === '/v2/timeline/dashboard' ||
-  timelineId === '/dashboard/following' ||
-  timelineId?.startsWith('following-');
+  timelineId === '/dashboard/following';
 
 export const followingTimelineSelector = createSelector(
   `[data-timeline="${'/v2/timeline/dashboard'}"]`,
-  `[data-timeline-id="${'/dashboard/following'}"]`,
-  `[data-timeline-id^="${'following-'}"]`
+  `[data-timeline-id="${'/dashboard/following'}"]`
 );
 
 // includes "channel" user blog view page
 export const anyBlogTimelineFilter = ({ dataset: { timeline, timelineId } }) =>
   timeline?.match(`/v2/blog/${anyBlog}/posts`) ||
   timelineId?.match(`peepr-posts-${anyBlog}-undefined-undefined-undefined-undefined-undefined-undefined`) ||
-  timelineId?.match(`blog-view-${anyBlog}`) ||
-  timelineId?.startsWith('blog-'); // partially functional
+  timelineId?.match(`blog-view-${anyBlog}`);
 
 // includes "channel" user blog view page
 export const blogTimelineFilter = blog =>
   ({ dataset: { timeline, timelineId } }) =>
     timeline === `/v2/blog/${blog}/posts` ||
     timelineId === `peepr-posts-${blog}-undefined-undefined-undefined-undefined-undefined-undefined` ||
-    timelineId === `blog-view-${blog}` ||
-    (timelineId?.startsWith('blog-') && timelineId?.endsWith(`-${blog}`)); // partially functional
+    timelineId === `blog-view-${blog}`;
 
 export const blogSubsTimelineFilter = ({ dataset: { timeline, which, timelineId } }) =>
   timeline === '/v2/timeline?which=blog_subscriptions' ||
@@ -36,17 +32,11 @@ export const blogSubsTimelineFilter = ({ dataset: { timeline, which, timelineId 
   timelineId === '/dashboard/blog_subs';
 
 export const anyDraftsTimelineFilter = ({ dataset: { timeline, timelineId } }) =>
-  timeline?.match(`/v2/blog/${anyBlog}/posts/draft`) ||
-  // timelineId === ???? ||
-  timelineId?.startsWith('drafts-');
+  timeline?.match(`/v2/blog/${anyBlog}/posts/draft`);
 
 export const anyQueueTimelineFilter = ({ dataset: { timeline, timelineId } }) =>
-  timeline?.match(`/v2/blog/${anyBlog}/posts/queue`) ||
-  // timelineId === ???? ||
-  timelineId?.startsWith('queue-');
+  timeline?.match(`/v2/blog/${anyBlog}/posts/queue`);
 
 export const tagTimelineFilter = encodedTag =>
   ({ dataset: { timeline, timelineId } }) =>
-    timeline === `/v2/hubs/${encodedTag}/timeline` ||
-    // timelineId === ??? ||
-    (timelineId?.startsWith('tag-') && timelineId?.endsWith(`-${encodedTag}-recent`));
+    timeline === `/v2/hubs/${encodedTag}/timeline`;

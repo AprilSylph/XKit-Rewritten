9009ad02cc0d0a400a3e8f06dd59f1762aa4fe0d
Marcus <marcustyphoon@gmail.com>
2024-06-21 08:54:45 -0700

    label styleElements

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -22,6 +22,7 @@
       document.documentElement.appendChild(link);
     }
     if (styleElement) {
+      styleElement.dataset.xkitFeature = name;
       document.documentElement.append(styleElement);
     }
 

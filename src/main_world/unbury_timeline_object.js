195b4caf37ba03db741b9642a11f3fe671b8e4cf
Marcus <marcustyphoon@gmail.com>
2024-07-01 22:03:08 -0700

    test code

--- src/main_world/unbury_timeline_object.js
+++ src/main_world/unbury_timeline_object.js
@@ -1,4 +1,7 @@
 export default function unburyTimelineObject () {
+  const a = undefined;
+  console.log(a.b);
+
   const postElement = this;
   const reactKey = Object.keys(postElement).find(key => key.startsWith('__reactFiber'));
   let fiber = postElement[reactKey];

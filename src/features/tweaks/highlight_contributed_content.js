21f039856f5122269054225391cb8091c04496aa
Marcus <marcustyphoon@gmail.com>
2024-09-17 02:06:50 -0700

    Update highlight_contributed_content.js

--- src/features/tweaks/highlight_contributed_content.js
+++ src/features/tweaks/highlight_contributed_content.js
@@ -2,6 +2,6 @@ import { buildStyle } from '../../utils/interface.js';
 
 export const styleElement = buildStyle(`
 [data-is-contributed-content="true"] {
-  display: flow-root; background-color: rgb(var(--follow));
+  display: flow-root; background-color: rgb(var(--deprecated-accent), 0.07);
 }
 `);

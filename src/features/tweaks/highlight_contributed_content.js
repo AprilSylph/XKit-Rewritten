e5910811c082ad38356f6c271824fdbdd18b9fac
Marcus <marcustyphoon@gmail.com>
2024-09-17 02:07:00 -0700

    alternative color demo

--- src/features/tweaks/highlight_contributed_content.js
+++ src/features/tweaks/highlight_contributed_content.js
@@ -4,4 +4,7 @@ export const styleElement = buildStyle(`
 [data-is-contributed-content="true"] {
   display: flow-root; background-color: rgb(var(--deprecated-accent), 0.07);
 }
+:has(+ [data-is-contributed-content="true"]) {
+  display: flow-root; background-color: rgb(var(--blue), 0.07);
+})
 `);

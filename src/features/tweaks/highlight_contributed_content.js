90bb241acad6a02b69519a69277e28a73e8cb8c2
marcustyphoon <marcustyphoon@gmail.com>
2024-10-28 00:26:25 -0700

    darken further
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

--- src/features/tweaks/highlight_contributed_content.js
+++ src/features/tweaks/highlight_contributed_content.js
@@ -2,6 +2,6 @@ import { buildStyle } from '../../utils/interface.js';
 
 export const styleElement = buildStyle(`
 [data-is-contributed-content="true"]:not([data-xkit-themed]) {
-  display: flow-root; background-color: rgb(var(--deprecated-accent), 0.1);
+  display: flow-root; background-color: rgb(var(--deprecated-accent), 0.13);
 }
 `);

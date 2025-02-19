e0f114938f1ccb9cd84afcfd3bc1f7d635d80343
Marcus <marcustyphoon@gmail.com>
2025-02-19 13:21:04 -0800

    apply feature to new headers

--- src/features/tweaks/hide_mini_follow.js
+++ src/features/tweaks/hide_mini_follow.js
@@ -1,6 +1,24 @@
 import { keyToCss } from '../../utils/css_map.js';
-import { buildStyle } from '../../utils/interface.js';
+import { buildStyle, postSelector } from '../../utils/interface.js';
+import { pageModifications } from '../../utils/mutations.js';
+import { timelineObject } from '../../utils/react_props.js';
+
+const hiddenAttribute = 'data-tweaks-hide-mini-follow-hidden';
 
 export const styleElement = buildStyle(`
-article ${keyToCss('followButton')}:not(${keyToCss('postMeatballsContainer')} *) { display: none; }
+article ${keyToCss('followButton')}:not(${keyToCss('postMeatballsContainer')} *), [${hiddenAttribute}] { display: none; }
 `);
+
+const processButtons = buttons => buttons.forEach(async button => {
+  const { headerCta } = await timelineObject(button.closest(postSelector));
+  headerCta?.action?.action === 'follow' && button.setAttribute(hiddenAttribute, '');
+});
+
+export const main = async () => {
+  pageModifications.register(`${postSelector} ${keyToCss('rightContent')} > button`, processButtons);
+};
+
+export const clean = async () => {
+  pageModifications.unregister(processButtons);
+  $(`[${hiddenAttribute}]`).removeAttr(hiddenAttribute);
+};

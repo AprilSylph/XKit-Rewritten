08c6db2f7d12d75a978e0b9cdf186223ae3672d4
Marcus <marcustyphoon@gmail.com>
2025-03-17 16:00:08 -0400

    target buttons based on text

--- src/features/tweaks/hide_mini_follow.js
+++ src/features/tweaks/hide_mini_follow.js
@@ -1,7 +1,7 @@
 import { keyToCss } from '../../utils/css_map.js';
 import { buildStyle, postSelector } from '../../utils/interface.js';
+import { translate } from '../../utils/language_data.js';
 import { pageModifications } from '../../utils/mutations.js';
-import { timelineObject } from '../../utils/react_props.js';
 
 const hiddenAttribute = 'data-tweaks-hide-mini-follow-hidden';
 
@@ -9,19 +9,17 @@ export const styleElement = buildStyle(`
 article ${keyToCss('followButton')}:not(${keyToCss('postMeatballsContainer')} *), [${hiddenAttribute}] { display: none; }
 `);
 
-const processHeaderButtons = buttons => buttons.forEach(async button => {
-  const postElement = button.closest(postSelector);
-  if (!postElement) { return; }
-
-  const { headerCta } = await timelineObject(postElement);
-  headerCta?.action?.action === 'follow' && button.setAttribute(hiddenAttribute, '');
+const processButtons = buttons => buttons.forEach(button => {
+  if (button.textContent === translate('Follow')) {
+    button.setAttribute(hiddenAttribute, '');
+  }
 });
 
 export const main = async () => {
-  pageModifications.register(`${postSelector} article > header ${keyToCss('rightContent')} > button`, processHeaderButtons);
+  pageModifications.register(`${postSelector} ${keyToCss('rightContent')} > button`, processButtons);
 };
 
 export const clean = async () => {
-  pageModifications.unregister(processHeaderButtons);
+  pageModifications.unregister(processButtons);
   $(`[${hiddenAttribute}]`).removeAttr(hiddenAttribute);
 };

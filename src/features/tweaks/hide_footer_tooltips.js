302416775f76331ae4df42db230685bc2cc4ec81
Marcus <marcustyphoon@gmail.com>
2025-05-27 20:37:00 -0700

    cheapest reasonable sanity check

--- src/features/tweaks/hide_footer_tooltips.js
+++ src/features/tweaks/hide_footer_tooltips.js
@@ -2,7 +2,7 @@ import { keyToCss } from '../../utils/css_map.js';
 import { buildStyle } from '../../utils/interface.js';
 
 export const styleElement = buildStyle(`
-article footer :is(${keyToCss('tooltip')}, ${keyToCss('tooltip')} ~ *) {
+article footer ${keyToCss('footerRow', 'footerContent')} :is(${keyToCss('tooltip')}, ${keyToCss('tooltip')} ~ *) {
   display: none;
 }
 `);

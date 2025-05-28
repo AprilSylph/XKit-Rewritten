86364a34837bc6a0b0bb83217d6a22005cbe77c4
Marcus <marcustyphoon@gmail.com>
2025-05-27 20:33:00 -0700

    look. do I look like I care? tbh? tbh.

--- src/features/tweaks/hide_footer_tooltips.js
+++ src/features/tweaks/hide_footer_tooltips.js
@@ -2,9 +2,7 @@ import { keyToCss } from '../../utils/css_map.js';
 import { buildStyle } from '../../utils/interface.js';
 
 export const styleElement = buildStyle(`
-article footer ${keyToCss('controlIcon')} ${keyToCss('tooltip')},
-article footer ${keyToCss('controlIcon')} ${keyToCss('tooltip')} ~ *,
-article footer ${keyToCss('controls', 'footerControl')} ${keyToCss('tooltip')} {
+article footer :is(${keyToCss('tooltip')}, ${keyToCss('tooltip')} ~ *) {
   display: none;
 }
 `);

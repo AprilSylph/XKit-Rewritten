0d731450fd348430e677515cea9c977ddf06bf59
marcustyphoon <marcustyphoon@gmail.com>
2025-06-11 11:34:36 -0700

    remove outdated selector
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

--- src/features/anti_capitalism.js
+++ src/features/anti_capitalism.js
@@ -16,7 +16,9 @@ const processVideoCTAs = videoCTAs => videoCTAs
 
 export const main = async () => {
   const { includeBlazed } = await getPreferences('anti_capitalism');
-  const blazeFilter = includeBlazed ? '' : ':not(:has(header use[href="#managed-icon__badge-blaze"], header use[href="#managed-icon__ds-blaze-filled-16"]))';
+  const blazeFilter = includeBlazed
+    ? ''
+    : ':not(:has(header use[href="#managed-icon__ds-blaze-filled-16"]))';
 
   styleElement.textContent = `
     [${hiddenAttribute}] > div,

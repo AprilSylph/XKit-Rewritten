8c55f31768db24863a2e1e407c3855d1a51a0d92
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2025-06-16 11:15:23 +0100

    chore(deps): add `neostandard` dev dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,17 @@
-import js from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
 import jsdocPlugin from 'eslint-plugin-jsdoc';
-import globals from 'globals';
-
-const languageOptions = {
-  globals: {
-    ...globals.browser,
-    ...globals.jquery,
-    ...globals.webextensions,
-  },
-  sourceType: 'module',
-}
+import neostandard from 'neostandard';
 
 export default [
-  { ignores: ['src/lib/**'] },
-  { ...js.configs.recommended, languageOptions },
-  { ...importPlugin.flatConfigs.recommended, languageOptions, rules: { 'import/no-cycle': 'error' } },
-  jsdocPlugin.configs["flat/recommended"],
+  ...neostandard({
+    env: ['browser', 'jquery', 'webextensions'],
+    ignores: ['src/lib/**'],
+    semi: true,
+  }),
+  {
+    ...importPlugin.flatConfigs.recommended,
+    languageOptions: { sourceType: 'module' },
+    rules: { 'import/no-cycle': 'error' }
+  },
+  jsdocPlugin.configs['flat/recommended'],
 ];

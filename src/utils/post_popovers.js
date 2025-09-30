7fc853ccfd899713acc96a4a2385aca95d961d29
marcustyphoon <marcustyphoon@gmail.com>
2025-09-30 00:53:43 -0700

    numbers

--- src/utils/post_popovers.js
+++ src/utils/post_popovers.js
@@ -2,12 +2,12 @@ import { keyToCss } from './css_map.js';
 
 /**
  * Detects Chromium <130, Firefox <130, Safari <26, or another old or non-compliant browser. Note
- * that ideally this would also include Firefox 130-133 (which were not ESR).
+ * that ideally this would also include Firefox 130-132 (which were not ESR).
  */
 const oldBrowser = !CSS.supports('text-wrap-style', 'balance') || typeof ImageDecoder !== 'function';
 
 /**
- * In older browsers (Chromium 105-129, Firefox 110-133, Safari 16-18.5), `container-type` creates a
+ * In older browsers (Chromium <129, Firefox <133, Safari <18.5), `container-type` creates a
  * stacking context, so the popup z-index does not apply outside of the footer.
  *
  * Removing the container fixes this, but causes a minor regression: disabling Tumblr's

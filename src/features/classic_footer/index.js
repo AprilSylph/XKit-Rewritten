6f4b2efb6609e8c7270ead0fa2b1c1477094354b
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2025-10-07 12:49:20 +0100

    feat(Classic Footer): add note counts to post footers

--- src/features/classic_footer/index.js
+++ src/features/classic_footer/index.js
@@ -1,9 +1,18 @@
 import { keyToCss } from '../../utils/css_map.js';
-import { buildStyle } from '../../utils/interface.js';
+import { dom } from '../../utils/dom.js';
+import { buildStyle, filterPostElements, postSelector } from '../../utils/interface.js';
+import { onNewPosts } from '../../utils/mutations.js';
+import { timelineObject } from '../../utils/react_props.js';
 
-const footerContentSelector = `article footer ${keyToCss('footerContent')}`;
+const noteCountClass = 'xkit-classic-footer-note-count';
+const footerContentSelector = `${postSelector} article footer ${keyToCss('footerContent')}`;
 
 export const styleElement = buildStyle(`
+  ${postSelector} article footer {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
   ${footerContentSelector} {
     justify-content: flex-end;
     gap: 0;
@@ -22,4 +31,33 @@ export const styleElement = buildStyle(`
   ${footerContentSelector} ${keyToCss('engagementCount')} {
     display: none;
   }
+  .${noteCountClass} {
+    padding: 8px;
+    margin-inline: 8px;
+
+    color: var(--content-fg-secondary);
+    font-family: var(--font-family-modern);
+    font-size: 1rem;
+    font-weight: 500;
+    line-height: 1.5rem;
+  }
 `);
+
+const processPosts = (postElements) => filterPostElements(postElements).forEach(async postElement => {
+  postElement.querySelector(`.${noteCountClass}`)?.remove();
+
+  const { noteCount } = await timelineObject(postElement);
+  const noteCountButton = dom('span', { class: noteCountClass }, undefined, [`${noteCount} ${noteCount === 1 ? 'note' : 'notes'}`]);
+
+  const footerContent = postElement.querySelector(footerContentSelector);
+  footerContent?.before(noteCountButton);
+});
+
+export const main = async function () {
+  onNewPosts.addListener(processPosts);
+};
+
+export const clean = async function () {
+  onNewPosts.removeListener(processPosts);
+  $(`.${noteCountClass}`).remove();
+};

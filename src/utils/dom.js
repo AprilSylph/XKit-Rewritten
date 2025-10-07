215447cb9e8e8c3ce4746ced4065385e1ff1a1fe
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2025-10-07 11:17:51 +0100

    feat: split `children` back into separate prop

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,12 +1,13 @@
 /** @typedef {Record<string, string>} Attributes */
 /** @typedef {Record<string, () => void>} Events */
+/** @typedef {(string | Node)[]} Children */
 
 /**
  * Create elements with simple syntax
  * @param {string} tagName - Type of element to create
  * @param {Record<string, string>} [attributes] - Property-value pairs to set as HTML/XML attributes (e.g. { href: '/' })
  * @param {Record<string, () => void>} [events] - Property-value pairs to set as event listeners (e.g. { click: () => {} })
- * @param {(string | Node)[]} [children] - Zero or more valid children
+ * @param {Children} [children] - Zero or more valid children
  * @returns {Element} Element created to specification
  */
 export function dom (tagName, attributes = {}, events = {}, children = []) {
@@ -22,24 +23,21 @@ export function dom (tagName, attributes = {}, events = {}, children = []) {
   return element;
 }
 
-/** @typedef {Record<"children", (string | Node)[] | undefined>} Children */
-/** @typedef {Attributes & Events & Children} Properties */
-
 /**
  * Create elements with even simpler syntax
  * @param {string} tagName Type of element to create
- * @param {Properties} properties Property-value pairs to set as HTML/XML attributes, event listeners, or children
+ * @param {Attributes & Events} [properties] Property-value pairs to set as HTML/XML attributes or event listeners
+ * @param {Children} [children] Zero or more valid children
  * @returns {Element} Element created to specification
  * @example element('a', { href: '/' }) => <a href="/">
  */
-function element (tagName, properties = {}) {
+function element (tagName, properties = {}, children = []) {
   const element = properties?.xmlns
     ? document.createElementNS(properties.xmlns, tagName)
     : document.createElement(tagName);
 
-  const { children = [], ...propertiesWithoutChildren } = properties;
-  const attributes = Object.entries(propertiesWithoutChildren).filter(([key, value]) => typeof value === 'string');
-  const events = Object.entries(propertiesWithoutChildren).filter(([key, value]) => typeof value === 'function');
+  const attributes = Object.entries(properties).filter(([key, value]) => typeof value === 'string');
+  const events = Object.entries(properties).filter(([key, value]) => typeof value === 'function');
 
   element.replaceChildren(children);
   attributes.forEach(([name, value]) => element.setAttribute(name, value));
@@ -49,16 +47,16 @@ function element (tagName, properties = {}) {
   return element;
 }
 
-export const button = (props = {}) => element('button', props);
-export const div = (props = {}) => element('div', props);
-export const h3 = (props = {}) => element('h3', props);
-export const img = (props = {}) => element('img', props);
-export const input = (props = {}) => element('input', props);
-export const label = (props = {}) => element('label', props);
-export const p = (props = {}) => element('p', props);
-export const span = (props = {}) => element('span', props);
-export const strong = (props = {}) => element('strong', props);
-export const style = (props = {}) => element('style', props);
+export const button = (props = {}, children = []) => element('button', props, children);
+export const div = (props = {}, children = []) => element('div', props, children);
+export const h3 = (props = {}, children = []) => element('h3', props, children);
+export const img = (props = {}, children = []) => element('img', props, children);
+export const input = (props = {}, children = []) => element('input', props, children);
+export const label = (props = {}, children = []) => element('label', props, children);
+export const p = (props = {}, children = []) => element('p', props, children);
+export const span = (props = {}, children = []) => element('span', props, children);
+export const strong = (props = {}, children = []) => element('strong', props, children);
+export const style = (props = {}, children = []) => element('style', props, children);
 
-export const svg = (props = {}) => element('svg', { xmlns: 'http://www.w3.org/2000/svg', ...props });
-export const use = (props = {}) => element('use', { xmlns: 'http://www.w3.org/2000/svg', ...props });
+export const svg = (props = {}, children = []) => element('svg', { xmlns: 'http://www.w3.org/2000/svg', ...props }, children);
+export const use = (props = {}, children = []) => element('use', { xmlns: 'http://www.w3.org/2000/svg', ...props }, children);

4eb2729e6e3a0d77759539bc8949d7934120452d
marcustyphoon <marcustyphoon@gmail.com>
2025-10-29 13:20:45 -0700

    remove jsdoc hyphens

--- src/utils/text_format.js
+++ src/utils/text_format.js
@@ -36,8 +36,8 @@ export const dateTimeFormat = new Intl.DateTimeFormat(document.documentElement.l
 /**
  * Adds string elements between an array's items to format it as an English prose list.
  * The Oxford comma is included.
- * @param {any[]} array - Input array of any number of items
- * @param {string} andOr - String 'and' or 'or', used before the last item
+ * @param {any[]} array Input array of any number of items
+ * @param {string} andOr String 'and' or 'or', used before the last item
  * @returns {any[]} An array alternating between the input items and strings
  */
 export const elementsAsList = (array, andOr) =>

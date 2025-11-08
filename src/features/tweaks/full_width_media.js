81ecfc47881547c9786da2f09fc6c9b9b36f122b
marcustyphoon <marcustyphoon@gmail.com>
2025-11-08 15:22:20 -0800

    initial commit

---
+++ src/features/tweaks/full_width_media.js
@@ -0,0 +1,28 @@
+import { keyToCss } from '../../utils/css_map.js';
+import { buildStyle } from '../../utils/interface.js';
+
+const rowWithImages = `${keyToCss('rows')}${keyToCss('rows')} ${keyToCss('rowWithImages')}`;
+const videoBlock = `${keyToCss('rows')}${keyToCss('rows')} ${keyToCss('videoBlock')}`;
+
+export const styleElement = buildStyle(`
+${rowWithImages}  {
+  padding-inline: unset;
+}
+${rowWithImages} ${keyToCss('imageBlock')}${keyToCss('unstretched')} {
+  padding-inline: 12px;
+}
+${rowWithImages} :is(${keyToCss('imageBlockButton')}, ${keyToCss('imageBlockButton')} img) {
+  border-radius: unset !important;
+}
+${rowWithImages} ${keyToCss('imageBlockButton')}:after {
+  border: none !important;
+}
+
+${videoBlock} {
+  margin-inline: unset;
+  width: 100%;
+}
+${videoBlock} ${keyToCss('videoPlayer', 'embeddedPlayer')} {
+  border-radius: 0;
+}
+`);

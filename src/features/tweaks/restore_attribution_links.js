2905912be41520335c257ccc6d2340162c8511ae
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2025-12-02 10:34:36 +0000

    feat(Tweaks): apply restore attribution tweak to trail items

--- src/features/tweaks/restore_attribution_links.js
+++ src/features/tweaks/restore_attribution_links.js
@@ -1,13 +1,12 @@
 import { onNewPosts } from '../../utils/mutations.js';
 import { keyToCss } from '../../utils/css_map.js';
-import { timelineObject } from '../../utils/react_props.js';
+import { timelineObject, trailItem } from '../../utils/react_props.js';
 import { navigate } from '../../utils/tumblr_helpers.js';
+import { trailItemSelector } from '../../utils/interface.js';
 
 const postAttributionLinkSelector = 'header a[rel="author"]';
-const reblogAttributionLinkSelector = `
-  header ${keyToCss('rebloggedFromName')} a,
-  header ${keyToCss('subheader')} a${keyToCss('blogLink')}
-`;
+const reblogAttributionLinkSelector = `header ${keyToCss('subheader')} a${keyToCss('blogLink')}`;
+const trailAttributionLinkSelector = `${keyToCss('trailHeader')} a[rel="author"]`;
 
 const onLinkClick = event => {
   event.stopPropagation();
@@ -30,10 +29,11 @@ const processPosts = async function (postElements) {
       postUrl,
       rebloggedFromId,
       rebloggedFromName,
-      rebloggedFromUrl
+      rebloggedFromUrl,
     } = await timelineObject(postElement);
     const postAttributionLink = postElement.querySelector(postAttributionLinkSelector);
     const reblogAttributionLink = postElement.querySelector(reblogAttributionLinkSelector);
+    const trailItemElements = [...postElement.querySelectorAll(trailItemSelector)];
 
     if (postAttributionLink && postAttributionLink.textContent === blogName) {
       postAttributionLink.dataset.originalHref ??= postAttributionLink.getAttribute('href');
@@ -50,6 +50,19 @@ const processPosts = async function (postElements) {
       reblogAttributionLink.dataset.postId = rebloggedFromId;
       reblogAttributionLink.addEventListener('click', onLinkClick, listenerOptions);
     }
+
+    trailItemElements.forEach(async trailItemElement => {
+      const { blog, post } = await trailItem(trailItemElement);
+      const trailAttributionLink = trailItemElement.querySelector(trailAttributionLinkSelector);
+
+      if (trailAttributionLink && trailAttributionLink.textContent === blog?.name && post?.id) {
+        trailAttributionLink.dataset.originalHref ??= trailAttributionLink.getAttribute('href');
+        trailAttributionLink.href = `/@${blog.name}/${post.id}`;
+        trailAttributionLink.dataset.blogName = blog.name;
+        trailAttributionLink.dataset.postId = post.id;
+        trailAttributionLink.addEventListener('click', onLinkClick, listenerOptions);
+      }
+    });
   });
 };
 
@@ -61,7 +74,7 @@ export const clean = async function () {
   onNewPosts.removeListener(processPosts);
 
   [...document.querySelectorAll(
-    `[data-original-href]:is(${postAttributionLinkSelector}, ${reblogAttributionLinkSelector})`
+    `[data-original-href]:is(${postAttributionLinkSelector}, ${reblogAttributionLinkSelector}, ${trailAttributionLinkSelector})`
   )].forEach(anchorElement => {
     anchorElement.setAttribute('href', anchorElement.dataset.originalHref);
     anchorElement.removeEventListener('click', onLinkClick, listenerOptions);

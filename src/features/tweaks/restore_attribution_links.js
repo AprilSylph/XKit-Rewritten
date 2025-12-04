b8ecf6a3f39c8227105cfd1d026431adccf3472c
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2025-12-04 13:13:18 +0000

    feat(Tweaks): apply restore attribution tweak to trail items (#2018)
    Co-authored-by: marcustyphoon <marcustyphoon@gmail.com>

--- src/features/tweaks/restore_attribution_links.js
+++ src/features/tweaks/restore_attribution_links.js
@@ -1,23 +1,24 @@
 import { onNewPosts } from '../../utils/mutations.js';
 import { keyToCss } from '../../utils/css_map.js';
-import { timelineObject } from '../../utils/react_props.js';
+import { timelineObject, trailItem } from '../../utils/react_props.js';
 import { navigate } from '../../utils/tumblr_helpers.js';
+import { trailItemSelector } from '../../utils/interface.js';
 
 const postAttributionLinkSelector = 'header a[rel="author"]';
-const reblogAttributionLinkSelector = `
-  header ${keyToCss('rebloggedFromName')} a,
-  header ${keyToCss('subheader')} a${keyToCss('blogLink')}
-`;
+const reblogAttributionLinkSelector = `header ${keyToCss('subheader')} a${keyToCss('blogLink')}`;
+const trailAttributionLinkSelector = `${keyToCss('trailHeader')} a[rel="author"]`;
 
 const onLinkClick = event => {
   event.stopPropagation();
   if (event.ctrlKey || event.metaKey || event.altKey || event.shiftKey) return;
 
-  const { blogName, postId } = event.currentTarget.dataset;
-  if (blogName && postId) {
-    event.preventDefault();
-    navigate(`/@${blogName}/${postId}`);
-  }
+  const { routerUrl } = event.currentTarget.dataset;
+  if (!routerUrl.startsWith('https://www.tumblr.com/')) return;
+
+  event.preventDefault();
+  if (routerUrl === location.href) return;
+
+  navigate(new URL(routerUrl).pathname);
 };
 
 const listenerOptions = { capture: true };
@@ -25,31 +26,60 @@ const listenerOptions = { capture: true };
 const processPosts = async function (postElements) {
   postElements.forEach(async postElement => {
     const {
+      authorBlog,
+      blog,
       blogName,
+      community,
       id,
+      parentPostUrl,
       postUrl,
-      rebloggedFromId,
       rebloggedFromName,
-      rebloggedFromUrl
+      rebloggedFromUrl,
+      slug,
     } = await timelineObject(postElement);
+    const postAttributionName = (!!community && authorBlog?.name) || blogName;
+    const hasCustomTheme = new URL(postUrl).hostname !== 'www.tumblr.com';
+    const postRouterUrl = hasCustomTheme
+      ? `${blog.blogViewUrl.replace(/\/$/, '')}/${id}${slug ? `/${slug}` : ''}`
+      : postUrl;
+
     const postAttributionLink = postElement.querySelector(postAttributionLinkSelector);
     const reblogAttributionLink = postElement.querySelector(reblogAttributionLinkSelector);
+    const trailItemElements = [...postElement.querySelectorAll(trailItemSelector)];
 
-    if (postAttributionLink && postAttributionLink.textContent === blogName) {
+    if (postAttributionLink && postAttributionLink.textContent === postAttributionName) {
       postAttributionLink.dataset.originalHref ??= postAttributionLink.getAttribute('href');
+      postAttributionLink.dataset.routerUrl = postRouterUrl;
       postAttributionLink.href = postUrl;
-      postAttributionLink.dataset.blogName = blogName;
-      postAttributionLink.dataset.postId = id;
       postAttributionLink.addEventListener('click', onLinkClick, listenerOptions);
     }
 
     if (reblogAttributionLink && reblogAttributionLink.textContent === rebloggedFromName) {
       reblogAttributionLink.dataset.originalHref ??= reblogAttributionLink.getAttribute('href');
+      reblogAttributionLink.dataset.routerUrl = parentPostUrl;
       reblogAttributionLink.href = rebloggedFromUrl;
-      reblogAttributionLink.dataset.blogName = rebloggedFromName;
-      reblogAttributionLink.dataset.postId = rebloggedFromId;
       reblogAttributionLink.addEventListener('click', onLinkClick, listenerOptions);
     }
+
+    trailItemElements.forEach(async trailItemElement => {
+      const { blog, post, isContributedContent } = await trailItem(trailItemElement);
+      const trailAttributionLink = trailItemElement.querySelector(trailAttributionLinkSelector);
+
+      if (trailAttributionLink && trailAttributionLink.textContent === blog?.name && post?.id) {
+        trailAttributionLink.dataset.originalHref ??= trailAttributionLink.getAttribute('href');
+
+        if (isContributedContent) {
+          trailAttributionLink.dataset.routerUrl = postRouterUrl;
+          trailAttributionLink.href = postUrl;
+        } else {
+          const hasCustomTheme = new URL(blog.url).hostname !== 'www.tumblr.com';
+          trailAttributionLink.dataset.routerUrl = `${blog.blogViewUrl.replace(/\/$/, '')}/${post.id}`;
+          trailAttributionLink.href = `${blog.url.replace(/\/$/, '')}${hasCustomTheme ? `/post/${post.id}` : `/${post.id}`}`;
+        }
+
+        trailAttributionLink.addEventListener('click', onLinkClick, listenerOptions);
+      }
+    });
   });
 };
 
@@ -61,10 +91,11 @@ export const clean = async function () {
   onNewPosts.removeListener(processPosts);
 
   [...document.querySelectorAll(
-    `[data-original-href]:is(${postAttributionLinkSelector}, ${reblogAttributionLinkSelector})`
+    `[data-original-href]:is(${postAttributionLinkSelector}, ${reblogAttributionLinkSelector}, ${trailAttributionLinkSelector})`
   )].forEach(anchorElement => {
     anchorElement.setAttribute('href', anchorElement.dataset.originalHref);
     anchorElement.removeEventListener('click', onLinkClick, listenerOptions);
     delete anchorElement.dataset.originalHref;
+    delete anchorElement.dataset.routerUrl;
   });
 };

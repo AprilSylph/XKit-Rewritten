85f90f49d9da46f6207e24a44021f8ac85dc92b5
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2025-12-15 14:48:33 +0000

    Update src/utils/inject.js
    Co-authored-by: marcustyphoon <marcustyphoon@gmail.com>

--- src/utils/inject.js
+++ src/utils/inject.js
@@ -3,7 +3,7 @@
  * its result. This permits access to variables exposed by the Tumblr web
  * platform that are normally inaccessible in the content script sandbox.
  * @see [src/main_world/index.js](../main_world/index.js) and named scripts in the same directory
- * @param {string} path Absolute path of script to inject; will be fed to {@linkcode https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/getURL|browser.runtime.getURL()}
+ * @param {string} path Absolute path of script file to inject; will be fed to {@linkcode https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/getURL|browser.runtime.getURL()}
  * @param {Array} [args] Array of arguments to pass to the injected function
  * @param {Element} [target] Target element; will be accessible as the `this` value in the injected function
  * @returns {Promise<any>} The transmitted result of the function call

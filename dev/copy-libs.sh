741ff1cd06bbae4efedd49e9941690ac1c0614c3
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2026-01-14 11:18:14 +0000

    build: vendor `lit-core@3.3.1`

--- dev/copy-libs.sh
+++ dev/copy-libs.sh
@@ -6,4 +6,6 @@ cp node_modules/remixicon/fonts/*.{css,eot,woff2,woff,ttf,svg} src/lib/remixicon
 cp node_modules/sortablejs/modular/sortable.esm.js src/lib/
 cp node_modules/webextension-polyfill/dist/browser-polyfill.min.js src/lib/
 
+curl "https://cdn.jsdelivr.net/gh/lit/dist@3.3.1/core/lit-core.min.js" --output-dir src/lib/ --remote-name --silent
+
 chmod +x src/lib/**.js

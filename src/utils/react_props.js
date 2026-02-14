3fd457a4384f3b73e4b041001e36a15c5af92fe3
marcustyphoon <marcustyphoon@gmail.com>
2026-02-14 12:44:45 -0800

    fix jsdoc

--- src/utils/react_props.js
+++ src/utils/react_props.js
@@ -14,7 +14,7 @@ export const timelineObject = postElement => {
 
 /**
  * @param {Element} cellElement An on-screen timeline cell
- * @returns {Promise<object>} The post's buried item property
+ * @returns {Promise<object>} The element's buried item property
  */
 export const cellItem = cellElement => {
   cellElement.cellItemPromise ??= inject('/main_world/unbury_cell_item.js', [], cellElement);

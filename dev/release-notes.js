f5ca07a80a92699eac374aeb2e239c1ede8a6264
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2026-02-16 11:14:38 +0000

    ci: create new release notes script; use GitHub mentions

---
+++ dev/release-notes.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+import { exec } from 'child_process';
+
+const {
+  GITHUB_API_URL = 'https://api.github.com',
+  GITHUB_REPOSITORY = 'AprilSylph/XKit-Rewritten',
+} = process.env;
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+exec('git log $(git describe --tags --abbrev=0)..HEAD --reverse --pretty --format="%H" --follow src/', async (error, stdout, stderr) => {
+  if (error) {
+    console.error('error: ', error);
+    return;
+  }
+
+  if (stderr) {
+    console.error('stderr: ', stderr);
+  }
+
+  let fatalError = null;
+
+  console.log('```md');
+  for (const ref of stdout.split('\n')) {
+    if (!ref) { continue; }
+
+    const [response] = await Promise.all([
+      fetch(`${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/commits/${ref}`, {
+        headers: {
+          Accept: 'application/vnd.github+json',
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      }),
+      sleep(1000),
+    ]);
+    if (!response.ok) {
+      fatalError = `Error ${response.status}: ${response.statusText}`;
+      break;
+    }
+
+    const { author, commit } = await response.json();
+    if (!commit) {
+      fatalError = `Fatal: API returned no commit info for ref ${ref}`;
+      break;
+    }
+
+    console.log(`- ${
+      ref
+    } ${
+      author.type === 'User'
+        ? `@${author.login}`
+        : `**[${author.login}](${author.html_url})**`
+    } ${
+      commit.message.includes('\n')
+        ? commit.message.slice(0, commit.message.indexOf('\n'))
+        : commit.message
+    }`);
+  }
+  console.log('```');
+
+  if (fatalError) {
+    console.log(`\`${fatalError}\``);
+  }
+});

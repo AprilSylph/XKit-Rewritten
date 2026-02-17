c97a6216b97a0cff0f2db8d55de486418d0f9fc3
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2026-02-17 09:49:21 +0000

    ci(Release Notes): fetch 100 most recent commits at once
    Co-authored-by: marcustyphoon <marcustyphoon@gmail.com>

--- dev/release-notes.js
+++ dev/release-notes.js
@@ -2,8 +2,6 @@
 
 import { execSync } from 'node:child_process';
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-
 const {
   GITHUB_API_URL = 'https://api.github.com',
   GITHUB_REPOSITORY = 'AprilSylph/XKit-Rewritten',
@@ -17,26 +15,21 @@ GITHUB_TOKEN && headers.append('Authorization', `Bearer ${GITHUB_TOKEN}`);
 
 try {
   const latestTag = execSync('git describe --tags --abbrev=0', { encoding: 'utf8' }).trim();
-  const refs = execSync(`git log ${latestTag}..HEAD --reverse --pretty --format="%H" --follow src/`, { encoding: 'utf8' }).trim().split('\n');
+  const refs = new Set(execSync(`git log ${latestTag}..HEAD --reverse --pretty --format="%H" --follow src/`, { encoding: 'utf8' }).trim().split('\n'));
+  const commits = new Map();
+
+  const response = await fetch(`${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/commits?per_page=100`, { headers });
+  if (!response.ok) { throw new Error(`Error ${response.status}: ${response.statusText}`); }
+
+  for (const commit of await response.json()) {
+    commits.set(commit.sha, commit);
+  }
+
+  if (!refs.isSubsetOf(commits)) { console.warn('Could not find commit info for one or more commits!'); }
 
   console.log('```md');
   for (const ref of refs) {
-    if (!ref) { continue; }
-
-    const [response] = await Promise.all([
-      fetch(`${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/commits/${ref}`, { headers }),
-      sleep(GITHUB_TOKEN ? 0 : 1000),
-    ]);
-    if (!response.ok) {
-      console.log('```');
-      throw new Error(`Error ${response.status}: ${response.statusText}`);
-    }
-
-    const { author, commit } = await response.json();
-    if (!commit) {
-      console.log('```');
-      throw new Error(`Fatal: API returned no commit info for ref ${ref}`);
-    }
+    const { author, commit } = commits.get(ref);
 
     console.log(`- ${
       ref

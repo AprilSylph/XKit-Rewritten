ccba758dbca1bbdca0442a3b17a176efc3d05ea3
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2026-02-18 10:02:43 +0000

    build: vendor `modern-normalize`

--- dev/copy-libs.sh
+++ dev/copy-libs.sh
@@ -1,6 +1,7 @@
 cp node_modules/@melloware/coloris/dist/coloris.css src/lib/
 cp node_modules/@melloware/coloris/dist/esm/coloris.js src/lib/
 cp node_modules/jquery/dist/jquery.slim.min.js src/lib/
+cp node_modules/modern-normalize/modern-normalize.css src/lib/
 cp node_modules/moment/dist/moment.js src/lib/
 cp node_modules/remixicon/fonts/*.{css,eot,woff2,woff,ttf,svg} src/lib/remixicon/
 cp node_modules/sortablejs/modular/sortable.esm.js src/lib/

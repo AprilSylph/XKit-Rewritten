4ba66ac94b1b36f272c232ab7730bd7dc1e51fdc
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2026-02-23 10:38:02 +0000

    style(Quick Reblog): `const inputEvents` → `function onTextFieldKeyDown`

--- src/features/quick_reblog/index.js
+++ src/features/quick_reblog/index.js
@@ -19,26 +19,12 @@ const quickTagsPanelId = 'xkit-quick-reblog-quick-tags-panel';
 const suggestedTagsTabId = 'xkit-quick-reblog-suggested-tags-tab';
 const suggestedTagsPanelId = 'xkit-quick-reblog-suggested-tags-panel';
 
-const inputEvents = (event) => {
-  event.stopPropagation();
-
-  if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) {
-    actionButtons.elements.item(0).click();
-  }
-
-  if (event.key === 'Escape') {
-    const { parentNode } = popupElement;
-    parentNode?.removeEventListener('mouseleave', removePopupOnLeave);
-    popupElement.remove();
-  }
-};
-
 const stopEventPropagation = event => event.stopPropagation();
 
 const blogSelector = select({ change: onBlogSelectorChange });
 const blogAvatar = div({ class: 'avatar' });
 const blogSelectorContainer = div({ class: 'select-container' }, [blogAvatar, blogSelector]);
-const commentInput = input({ autocomplete: 'off', placeholder: 'Comment', keydown: inputEvents });
+const commentInput = input({ autocomplete: 'off', placeholder: 'Comment', keydown: onTextFieldKeyDown });
 const tagsTabList = div({ role: 'tablist' }, [
   button({
     'aria-controls': quickTagsPanelId,
@@ -60,7 +46,7 @@ const tagsInput = input({
   list: 'quick-reblog-tag-suggestions',
   placeholder: 'Tags (comma separated)',
   input: onTagsInput,
-  keydown: inputEvents,
+  keydown: onTextFieldKeyDown,
 });
 const actionButtons = fieldset({ class: 'action-buttons' }, [
   button({ 'data-state': 'published', click: reblogPost }, ['Reblog']),
@@ -138,6 +124,19 @@ function onTabClick ({ currentTarget }) {
   currentTarget.getAttribute('aria-controls').split(',')
     .map(elementId => document.getElementById(elementId))
     .forEach(tabPanel => tabPanel.removeAttribute('hidden'));
+}
+
+/** @param {KeyboardEvent} event commentInput/tagsInput keydown event object */
+function onTextFieldKeyDown (event) {
+  event.stopPropagation();
+
+  if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) {
+    actionButtons.elements.item(0).click();
+  }
+
+  if (event.key === 'Escape') {
+    removePopupOnClick();
+  }
 }
 
 /** @param {InputEvent} event tagsInput input event object */

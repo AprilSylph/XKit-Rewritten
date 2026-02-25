cef142f89862a4816fb06b2e03d3a7b87fda9dd5
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2026-02-25 12:36:07 +0000

    fix(MIrror Posts): remove `archive.today` option

--- src/features/mirror_posts/index.js
+++ src/features/mirror_posts/index.js
@@ -1,39 +1,40 @@
-import { dom } from '../../utils/dom.js';
+import { button, form, input } from '../../utils/dom.js';
 import { registerMeatballItem, unregisterMeatballItem } from '../../utils/meatballs.js';
-import { showModal, modalCancelButton } from '../../utils/modals.js';
+import { showModal, modalCompleteButton } from '../../utils/modals.js';
 
 const meatballButtonId = 'mirror_posts';
 const meatballButtonLabel = 'Mirror this post';
 
-const archiveDotOrgForm = dom('form', {
-  method: 'post',
+const waybackMachineForm = form({
   action: 'https://web.archive.org/save',
+  hidden: true,
+  method: 'post',
   target: '_blank',
-  tabindex: -1,
-}, null, [
-  dom('input', { hidden: true, name: 'url_preload', type: 'text' }),
-  dom('button', { class: 'blue', type: 'submit' }, null, ['Wayback Machine']),
+}, [
+  input({ name: 'url_preload', type: 'text' }),
+  button({ type: 'submit' }),
 ]);
+document.documentElement.append(waybackMachineForm);
 
-const onButtonClicked = async function ({ currentTarget }) {
-  const { postUrl } = currentTarget.__timelineObjectData;
-  const ampUrl = `${postUrl}/amp`;
+/** @type {(event: PointerEvent) => void} */
+function onButtonClicked (event) {
+  const { blog, community, postUrl } = event.currentTarget.__timelineObjectData;
+  const isLoggedInOnly = !!blog.isHiddenFromBlogNetwork;
+  const isPrivateBlog = !!blog.isPasswordProtected && !community;
+  const isPrivateCommunity = !!community && community.visibility !== 'public';
 
-  const archiveTodayButton = dom(
-    'button',
-    { class: 'blue' },
-    { click: () => window.open(`https://archive.today/?run=1&url=${encodeURIComponent(ampUrl)}`, '_blank') },
-    ['archive.today'],
-  );
+  if (isLoggedInOnly || isPrivateBlog || isPrivateCommunity) {
+    showModal({
+      title: 'Can’t mirror this post!',
+      message: [`This ${community ? 'community' : 'blog'}’s privacy settings does not allow archiving.`],
+      buttons: [modalCompleteButton],
+    });
+    return;
+  }
 
-  archiveDotOrgForm.elements.url_preload.value = ampUrl;
-
-  showModal({
-    title: meatballButtonLabel,
-    message: ['Note: this will not work for dashboard-only blogs.'],
-    buttons: [modalCancelButton, archiveDotOrgForm, archiveTodayButton],
-  });
-};
+  waybackMachineForm.elements.url_preload.value = postUrl;
+  waybackMachineForm.requestSubmit();
+}
 
 export const main = async () => registerMeatballItem({ id: meatballButtonId, label: meatballButtonLabel, onclick: onButtonClicked });
 export const clean = async () => unregisterMeatballItem(meatballButtonId);

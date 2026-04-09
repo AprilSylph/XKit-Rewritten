3b56a8a147992bd4e3f11d4258a68a40c15081e0
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2026-04-09 09:50:30 +0100

    chore(PostBlock): convert iframe-type preference to component

--- src/features/postblock/options/index.js
+++ src/features/postblock/options/index.js
@@ -1,39 +1,78 @@
-const postsBlockedCount = document.getElementById('posts-blocked-count');
-const blockedPostList = document.getElementById('blocked-posts');
-const blockedPostTemplate = document.getElementById('blocked-post');
+import { CustomElement, fetchStyleSheets } from '../../../action/components/index.js';
+
+const localName = 'postblock-blocked-posts';
+
+const templateDocument = new DOMParser().parseFromString(`
+  <template id="${localName}">
+    <h3 id="posts-blocked-count">Loading…</h3>
+    <ul id="blocked-posts"></ul>
+    <template id="blocked-post">
+      <li class="blocked-post">
+        <span></span>
+        <button data-post-id="">Unblock</button>
+      </li>
+    </template>
+  </template>
+`, 'text/html');
+
+const adoptedStyleSheets = await fetchStyleSheets([
+  '/lib/modern-normalize.css',
+  './index.css',
+].map(import.meta.resolve));
 
 const storageKey = 'postblock.blockedPostRootIDs';
 
-const unblockPost = async function ({ currentTarget }) {
-  let { [storageKey]: blockedPostRootIDs = [] } = await browser.storage.local.get(storageKey);
+class PostBlockBlockedPostsElement extends CustomElement {
+  /** @type {HTMLHeadingElement}  */ #postsBlockedCount;
+  /** @type {HTMLUListElement}    */ #blockedPostList;
+  /** @type {HTMLTemplateElement} */ #blockedPostTemplate;
 
-  blockedPostRootIDs = blockedPostRootIDs.filter(id => id !== currentTarget.dataset.postId);
-  await browser.storage.local.set({ [storageKey]: blockedPostRootIDs });
+  constructor () {
+    super(templateDocument, adoptedStyleSheets);
 
-  currentTarget.remove();
-};
-
-const renderBlockedPosts = async function () {
-  const { [storageKey]: blockedPostRootIDs = [] } = await browser.storage.local.get(storageKey);
-
-  postsBlockedCount.textContent = `${blockedPostRootIDs.length} blocked ${blockedPostRootIDs.length === 1 ? 'post' : 'posts'}`;
-  blockedPostList.replaceChildren(...blockedPostRootIDs.map(blockedPostID => {
-    const templateClone = blockedPostTemplate.content.cloneNode(true);
-    const spanElement = templateClone.querySelector('span');
-    const unblockButton = templateClone.querySelector('button');
-
-    spanElement.textContent = blockedPostID;
-    unblockButton.dataset.postId = blockedPostID;
-    unblockButton.addEventListener('click', unblockPost);
-
-    return templateClone;
-  }));
-};
-
-browser.storage.local.onChanged.addListener((changes) => {
-  if (Object.keys(changes).includes(storageKey)) {
-    renderBlockedPosts();
+    this.#postsBlockedCount = this.shadowRoot.getElementById('posts-blocked-count');
+    this.#blockedPostList = this.shadowRoot.getElementById('blocked-posts');
+    this.#blockedPostTemplate = this.shadowRoot.getElementById('blocked-post');
   }
-});
 
-renderBlockedPosts();
+  unblockPost = async ({ currentTarget }) => {
+    const { [storageKey]: blockedPostRootIDs = [] } = await browser.storage.local.get(storageKey);
+    await browser.storage.local.set({ [storageKey]: blockedPostRootIDs.filter(id => id !== currentTarget.dataset.postId) });
+    currentTarget.remove();
+  };
+
+  renderBlockedPosts = async () => {
+    const { [storageKey]: blockedPostRootIDs = [] } = await browser.storage.local.get(storageKey);
+
+    this.#postsBlockedCount.textContent = `${blockedPostRootIDs.length} blocked ${blockedPostRootIDs.length === 1 ? 'post' : 'posts'}`;
+    this.#blockedPostList.replaceChildren(...blockedPostRootIDs.map(blockedPostID => {
+      const templateClone = this.#blockedPostTemplate.content.cloneNode(true);
+      const spanElement = templateClone.querySelector('span');
+      const unblockButton = templateClone.querySelector('button');
+
+      spanElement.textContent = blockedPostID;
+      unblockButton.dataset.postId = blockedPostID;
+      unblockButton.addEventListener('click', this.unblockPost);
+
+      return templateClone;
+    }));
+  };
+
+  onStorageChanged = (changes) => {
+    if (Object.keys(changes).includes(storageKey)) {
+      this.renderBlockedPosts();
+    }
+  };
+
+  connectedCallback () {
+    this.role ||= 'listitem';
+    this.slot ||= 'preferences';
+
+    browser.storage.local.onChanged.addListener(this.onStorageChanged);
+    this.renderBlockedPosts();
+  }
+}
+
+customElements.define(localName, PostBlockBlockedPostsElement);
+
+export default () => document.createElement(localName);

220c7b9c86c74a7c01190b3a5fb5aff6aa4864bc
GHX5T-SOL <200635707+GHX5T-SOL@users.noreply.github.com>
2026-05-18 01:17:50 +0200

    fix no recommended sidebar targets

--- src/features/no_recommended/hide_recommended_blogs_modal.js
+++ src/features/no_recommended/hide_recommended_blogs_modal.js
@@ -1,8 +1,13 @@
 import { keyToCss } from '../../utils/css_map.js';
 import { blogViewSelector, buildStyle } from '../../utils/interface.js';
+import { translate } from '../../utils/language_data.js';
 import { pageModifications } from '../../utils/mutations.js';
 
 const hiddenAttribute = 'data-no-recommended-blogs-modal-hidden';
+const recommendedBlogTitles = [
+  'Check out these blogs',
+  'Check these out',
+].map(translate);
 
 export const styleElement = buildStyle(`[${hiddenAttribute}] { display: none; }`);
 
@@ -11,12 +16,20 @@ const hideModalRecommended = blogsLists =>
     .filter(ul => ul.matches(blogViewSelector))
     .forEach(ul => ul.parentNode.setAttribute(hiddenAttribute, ''));
 
+const hideModalRecommendedTitles = sidebarTitles =>
+  sidebarTitles
+    .filter(h1 => h1.matches(blogViewSelector))
+    .filter(h1 => recommendedBlogTitles.includes(h1.textContent.trim()))
+    .forEach(h1 => h1.closest('aside > *').setAttribute(hiddenAttribute, ''));
+
 export const main = async function () {
   const blogsListSelector = `${keyToCss('desktopContainer')} > ${keyToCss('recommendedBlogs')}`;
   pageModifications.register(blogsListSelector, hideModalRecommended);
+  pageModifications.register('aside h1', hideModalRecommendedTitles);
 };
 
 export const clean = async function () {
   pageModifications.unregister(hideModalRecommended);
+  pageModifications.unregister(hideModalRecommendedTitles);
   $(`[${hiddenAttribute}]`).removeAttr(hiddenAttribute);
 };
--- src/features/no_recommended/hide_search_communities.js
+++ src/features/no_recommended/hide_search_communities.js
@@ -3,5 +3,5 @@ import { buildStyle } from '../../utils/interface.js';
 import { translate } from '../../utils/language_data.js';
 
 export const styleElement = buildStyle(`
-${keyToCss('typeaheadSection')}:has(> [aria-label="${translate('Suggested communities')}"]) { display: none; }
+${keyToCss('typeaheadSection')}:has([aria-label="${translate('Suggested communities')}"]) { display: none; }
 `);

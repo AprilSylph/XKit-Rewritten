f4dc6d1db835e5b7790b43a8cc2848fbf7c4c99f
theIncandescentAnteater <59981704+theIncandescentAnteater@users.noreply.github.com>
2026-05-21 05:43:38 -0400

    feat(Vanilla Audio, Vanilla Video): allow fine-tuning of default volume (#2255)
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

---
+++ src/action/components/percent-preference/index.js
@@ -0,0 +1,72 @@
+import { CustomElement, fetchStyleSheets } from '../index.js';
+
+const localName = 'percent-preference';
+
+const templateDocument = new DOMParser().parseFromString(`
+  <template id="${localName}">
+    <label for="percent"></label>
+    <input id="percent" type="text" size="28" spellcheck="false">
+  </template>
+`, 'text/html');
+
+const adoptedStyleSheets = await fetchStyleSheets([
+  '/lib/modern-normalize.css',
+  '/action/acorn.css',
+  './index.css',
+].map(import.meta.resolve));
+
+class PercentPreferenceElement extends CustomElement {
+  /** @type {string} */ featureName;
+  /** @type {string} */ preferenceName;
+
+  /** @type {HTMLInputElement} */ #inputElement;
+  /** @type {HTMLLabelElement} */ #labelElement;
+  /** @type {ReturnType<Window['setTimeout']>} */ #timeoutID;
+
+  constructor () {
+    super(templateDocument, adoptedStyleSheets);
+
+    this.#inputElement = this.shadowRoot.getElementById('percent');
+    this.#labelElement = this.shadowRoot.querySelector('label[for="percent"]');
+  }
+
+  /** @param {string} label Label displayed to the user to describe the preference. */
+  set label (label) { this.#labelElement.textContent = label; }
+  get label () { return this.#labelElement.textContent; }
+
+  /** @param {string} value The saved or default value of this preference. */
+  set value (value = '') { this.#inputElement.value = value; }
+  get value () { return this.#inputElement.value; }
+
+  /** @type {(event: InputEvent) => void} */ #onInput = () => {
+    const storageKey = `${this.featureName}.preferences.${this.preferenceName}`;
+    this.#inputElement.value = this.#inputElement.value.match(/\d+/) ? Math.min(this.#inputElement.value.match(/\d+/)[0], 100) : 0;
+    const storageValue = this.#inputElement.value;
+
+    clearTimeout(this.#timeoutID);
+    this.#timeoutID = setTimeout(() => browser.storage.local.set({ [storageKey]: storageValue }), 500);
+  };
+
+  connectedCallback () {
+    this.role ||= 'listitem';
+    this.slot ||= 'preferences';
+    this.#inputElement.addEventListener('input', this.#onInput);
+  }
+
+  disconnectedCallback () {
+    this.#inputElement.removeEventListener('input', this.#onInput);
+  }
+}
+
+customElements.define(localName, PercentPreferenceElement);
+
+/**
+ * @typedef PercentPreferenceProps
+ * @property {string} featureName The feature's internal name (e.g. `"vanilla_audio"`).
+ * @property {string} preferenceName The preference's internal name (e.g. `"defaultVolume"`).
+ * @property {string} label The preference's label (e.g. `"Default Volume"`).
+ * @property {string} value The preference's current value (as set by the user, or the preference's default).
+ */
+
+/** @type {(props: PercentPreferenceProps) => PercentPreferenceElement} */
+export const PercentPreference = (props = {}) => Object.assign(document.createElement(localName), props);

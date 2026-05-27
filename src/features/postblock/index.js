50501a16f28290c34933941540e75a7e6e3ec3a9
marcustyphoon <marcustyphoon@gmail.com>
2026-05-27 15:00:42 -0700

    feat(Postblock): Make options UI items links if possible (#719)
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

--- src/features/postblock/index.js
+++ src/features/postblock/index.js
@@ -4,71 +4,148 @@ import { registerMeatballItem, unregisterMeatballItem } from '../../utils/meatba
 import { showModal, hideModal, modalCancelButton } from '../../utils/modals.js';
 import { onNewPosts, pageModifications } from '../../utils/mutations.js';
 import { timelineObject } from '../../utils/react_props.js';
+import { postPermalinkTimelineFilter, timelineSelector } from '../../utils/timeline_id.js';
+import { navigate } from '../../utils/tumblr_helpers.js';
 
-const meatballButtonId = 'postblock';
-const meatballButtonLabel = 'Block this post';
+const meatballButtonBlockId = 'postblock-block';
+const meatballButtonBlockLabel = 'Block this post';
+const meatballButtonUnblockId = 'postblock-unblock';
+const meatballButtonUnblockLabel = 'Unblock this post';
+
 const hiddenAttribute = 'data-postblock-hidden';
+const controlsClass = 'xkit-postblock-hidden-post-controls';
+const controlledHiddenAttribute = 'data-xkit-postblock-hidden-controlled';
 const storageKey = 'postblock.blockedPostRootIDs';
+const blogUuidsStorageKey = 'postblock.blockedPostBlogUUIDs';
+
+// Remove outdated elements when loading module
+$(`.${controlsClass}`).remove();
+
+let blogUuids = {};
+
+const addPermalinkPageControls = timelineElement => {
+  const controlsElement = dom('div', { class: controlsClass }, null, [
+    'This post is hidden by PostBlock.',
+    dom('button', null, { click: () => controlsElement.remove() }, 'View post'),
+  ]);
+  timelineElement.prepend(controlsElement);
+};
 
 let blockedPostRootIDs = [];
+
+const saveUuidPair = (postId, blogUuid) => {
+  if (blockedPostRootIDs.includes(postId) && !blogUuids[postId]) {
+    blogUuids[postId] = blogUuid;
+    browser.storage.local.set({ [blogUuidsStorageKey]: blogUuids });
+  }
+};
 
 const processPosts = postElements =>
   filterPostElements(postElements, { includeFiltered: true }).forEach(async postElement => {
     const postID = postElement.dataset.id;
-    const { rebloggedRootId } = await timelineObject(postElement);
+    const {
+      rebloggedRootId,
+      rebloggedRootUuid,
+      id,
+      blog: { uuid },
+      rebloggedFromId,
+      rebloggedFromUuid,
+    } = await timelineObject(postElement);
 
     const rootID = rebloggedRootId || postID;
 
     if (blockedPostRootIDs.includes(rootID)) {
-      getTimelineItemWrapper(postElement).setAttribute(hiddenAttribute, '');
+      const timelineElement = postElement.closest(timelineSelector);
+      if (postPermalinkTimelineFilter(postID)(timelineElement)) {
+        getTimelineItemWrapper(postElement).setAttribute(controlledHiddenAttribute, '');
+        addPermalinkPageControls(timelineElement);
+      } else {
+        getTimelineItemWrapper(postElement).setAttribute(hiddenAttribute, '');
+      }
     } else {
       getTimelineItemWrapper(postElement).removeAttribute(hiddenAttribute);
     }
+
+    saveUuidPair(id, uuid);
+    saveUuidPair(rebloggedFromId, rebloggedFromUuid);
+    saveUuidPair(rebloggedRootId, rebloggedRootUuid);
   });
 
 const onButtonClicked = ({ currentTarget }) => {
   const { id, rebloggedRootId } = currentTarget.__timelineObjectData;
   const rootID = rebloggedRootId || id;
+  const shouldBlockPost = blockedPostRootIDs.includes(rootID) === false;
+
+  const title = shouldBlockPost
+    ? 'Block this post?'
+    : 'Unblock this post?';
+  const message = shouldBlockPost
+    ? 'All instances of this post (including reblogs) will be hidden.'
+    : 'All instances of this post (including reblogs) will become visible again.';
+  const textContent = shouldBlockPost
+    ? 'Block this post'
+    : 'Unblock this post';
+  const className = shouldBlockPost
+    ? 'red'
+    : 'blue';
+  const saveNotificationPreference = shouldBlockPost
+    ? () => { blockedPostRootIDs.push(rootID); browser.storage.local.set({ [storageKey]: blockedPostRootIDs }); }
+    : () => browser.storage.local.set({ [storageKey]: blockedPostRootIDs.filter(blockedId => blockedId !== rootID) });
 
   showModal({
-    title: 'Block this post?',
-    message: [
-      'All instances of this post (including reblogs) will be hidden.',
-    ],
+    title,
+    message,
     buttons: [
       modalCancelButton,
-      dom('button', { class: 'red' }, { click: () => blockPost(rootID) }, ['Block this post']),
+      dom('button', { class: className }, {
+        click () {
+          hideModal();
+          saveNotificationPreference();
+        },
+      }, [textContent]),
     ],
   });
 };
 
-const blockPost = async rootID => {
-  hideModal();
-  const { [storageKey]: blockedPostRootIDs = [] } = await browser.storage.local.get(storageKey);
-  blockedPostRootIDs.push(rootID);
-  browser.storage.local.set({ [storageKey]: blockedPostRootIDs });
-};
-
 export const onStorageChanged = async function (changes) {
-  if (Object.keys(changes).includes(storageKey)) {
-    ({ newValue: blockedPostRootIDs = [] } = changes[storageKey]);
+  const { [storageKey]: blockedPostChanges, [blogUuidsStorageKey]: blogUuidsChanges } = changes;
+
+  if (blogUuidsChanges) {
+    ({ newValue: blogUuids } = blogUuidsChanges);
+  }
+
+  if (blockedPostChanges) {
+    ({ newValue: blockedPostRootIDs = [] } = blockedPostChanges);
     pageModifications.trigger(processPosts);
   }
 };
 
+const blockPostFilter = ({ id, rebloggedRootId }) => blockedPostRootIDs.includes(rebloggedRootId || id) === false;
+const unblockPostFilter = ({ id, rebloggedRootId }) => blockedPostRootIDs.includes(rebloggedRootId || id);
+
 export const main = async function () {
   ({ [storageKey]: blockedPostRootIDs = [] } = await browser.storage.local.get(storageKey));
+  ({ [blogUuidsStorageKey]: blogUuids = {} } = await browser.storage.local.get(blogUuidsStorageKey));
 
-  registerMeatballItem({ id: meatballButtonId, label: meatballButtonLabel, onclick: onButtonClicked });
+  registerMeatballItem({ id: meatballButtonBlockId, label: meatballButtonBlockLabel, onclick: onButtonClicked, postFilter: blockPostFilter });
+  registerMeatballItem({ id: meatballButtonUnblockId, label: meatballButtonUnblockLabel, onclick: onButtonClicked, postFilter: unblockPostFilter });
 
   onNewPosts.addListener(processPosts);
+
+  const blockedPostID = location.hash.match(/(?<=^#postblock:)\d{1,20}$/)?.[0];
+
+  if (blockedPostID) {
+    navigate(`./${blockedPostID}`);
+  }
 };
 
 export const clean = async function () {
-  unregisterMeatballItem(meatballButtonId);
+  unregisterMeatballItem(meatballButtonBlockId);
+  unregisterMeatballItem(meatballButtonUnblockId);
   onNewPosts.removeListener(processPosts);
 
   $(`[${hiddenAttribute}]`).removeAttr(hiddenAttribute);
+  $(`.${controlsClass}`).remove();
 };
 
 export const stylesheet = true;

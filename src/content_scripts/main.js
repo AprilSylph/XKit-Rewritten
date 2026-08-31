eaf75fca0adfc4be32ae0d1c29c0a572569311c8
marcustyphoon <marcustyphoon@gmail.com>
2026-08-31 07:53:53 -0700

    feat: Run features in alphabetical order; make control button order consistent (#2359)
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -12,6 +12,9 @@
 
   const timestamp = Date.now(); // Prevent referencing outdated resources after Firefox extension update/restart
 
+  const importFeature = name => import(browser.runtime.getURL(`/features/${name}/index.js`));
+  const importUtil = name => import(browser.runtime.getURL(`/utils/${name}.js`));
+
   const runFeature = async function (name) {
     const {
       main,
@@ -19,7 +22,7 @@
       stylesheet,
       styleElement,
       onStorageChanged,
-    } = await import(browser.runtime.getURL(`/features/${name}/index.js`));
+    } = await importFeature(name);
 
     if (main) {
       main().catch(console.error);
@@ -57,7 +60,7 @@
       clean,
       stylesheet,
       styleElement,
-    } = await import(browser.runtime.getURL(`/features/${name}/index.js`));
+    } = await importFeature(name);
 
     if (clean) {
       clean().catch(console.error);
@@ -79,11 +82,11 @@
     if (enabledFeatures) {
       const { oldValue = [], newValue = [] } = enabledFeatures;
 
-      const newlyEnabled = newValue.filter(x => oldValue.includes(x) === false);
       const newlyDisabled = oldValue.filter(x => newValue.includes(x) === false);
+      const newlyEnabled = newValue.filter(x => oldValue.includes(x) === false);
 
-      newlyEnabled.forEach(runFeature);
       newlyDisabled.forEach(destroyFeature);
+      newlyEnabled.forEach(runFeature);
     }
   };
 
@@ -139,15 +142,21 @@
       initMainWorld(),
     ]);
 
-    /**
-     * Fixes WebKit (Chromium, Safari) simultaneous import failure of files with unresolved top level await
-     * @see https://github.com/sveltejs/kit/issues/7805#issuecomment-1330078207
-     */
-    await Promise.all(['css_map', 'language_data', 'user'].map(name => import(browser.runtime.getURL(`/utils/${name}.js`))));
+    const orderedEnabledFeatures = installedFeatures.filter(name => enabledFeatures.includes(name));
 
-    installedFeatures
-      .filter(featureName => enabledFeatures.includes(featureName))
-      .forEach(runFeature);
+    /**
+     * Fixes WebKit (Chromium, Safari) simultaneous import failure of files with unresolved top level await.
+     * @see https://bugs.webkit.org/show_bug.cgi?id=242740
+     */
+    await Promise.all(['css_map', 'language_data', 'user'].map(importUtil));
+
+    /**
+     * Populates the module cache, ensuring that feature run order is unaffected by module resolution.
+     * @see https://github.com/AprilSylph/XKit-Rewritten/discussions/2357
+     */
+    await Promise.all(orderedEnabledFeatures.map(importFeature));
+
+    orderedEnabledFeatures.forEach(runFeature);
 
     warnOnExtensionContextInvalidated();
   };

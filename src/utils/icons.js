39f620c454d53e37bd91d5a5c3490d8c6b572123
marcustyphoon <marcustyphoon@gmail.com>
2026-08-31 02:49:36 -0700

    fix(Icons util): Multiple duplicate symbol elements being created (#2363)

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,4 +1,5 @@
 import { aside, svg, use } from './dom.js';
+import { memoize } from './memoize.js';
 
 const symbolsClassName = 'xkit-symbols';
 
@@ -8,21 +9,22 @@ $(`.${symbolsClassName}`).remove();
 const symbols = aside({ class: symbolsClassName });
 document.head.append(symbols);
 
+const getSymbolId = memoize(featureName => {
+  const symbolId = `${symbolsClassName}__${featureName}`;
+
+  fetch(browser.runtime.getURL(`/features/${featureName}/icon.svg`))
+    .then(iconResponse => iconResponse.text())
+    .then(iconText => new DOMParser().parseFromString(iconText, 'image/svg+xml').firstElementChild)
+    .then(iconElement => {
+      iconElement.setAttribute('id', symbolId);
+      symbols.append(iconElement);
+    });
+
+  return symbolId;
+});
+
 /**
  * @param {string} featureName The internal name of a feature calling this utility (e.g. `"quick_tags"`)
  * @returns {SVGSVGElement} An SVG element that renders the icon for the specified feature
  */
-export const getIcon = featureName => {
-  const symbolId = `${symbolsClassName}__${featureName}`;
-  if (document.getElementById(symbolId) === null) {
-    fetch(browser.runtime.getURL(`/features/${featureName}/icon.svg`))
-      .then(iconResponse => iconResponse.text())
-      .then(iconText => new DOMParser().parseFromString(iconText, 'image/svg+xml').firstElementChild)
-      .then(iconElement => {
-        iconElement.setAttribute('id', symbolId);
-        symbols.append(iconElement);
-      });
-  }
-
-  return svg({}, [use({ href: `#${symbolId}` })]);
-};
+export const getIcon = featureName => svg({}, [use({ href: `#${getSymbolId(featureName)}` })]);

273785d025433c859c28045a604ccde0d4f24bb0
marcustyphoon <marcustyphoon@gmail.com>
2026-09-08 19:02:49 -0700

    Revert "test code"
    This reverts commit 2fce56060244b40bd76ec962ea6814d048e5c78e.

--- src/features/tag_tracking_plus/index.js
+++ src/features/tag_tracking_plus/index.js
@@ -21,8 +21,6 @@ let sidebarItem;
 const refreshCount = async function (tag) {
   if (!trackedTags.includes(tag)) return;
 
-  console.info(`Tag Tracking+: REFRESHING ${tag}`);
-
   let unreadCountString = '⚠️';
 
   try {
@@ -97,7 +95,7 @@ const startRefreshLoop = async () => {
       if (currentRefreshLoop !== thisRefreshLoop) return;
       await Promise.all([
         refreshCount(tag),
-        new Promise(resolve => setTimeout(resolve, 5000)),
+        new Promise(resolve => setTimeout(resolve, 30000)),
       ]);
     }
   }

67a9fd774e01e58d033c70e7e09badad5681ef8e
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2026-09-25 13:11:56 +0100

    build: rewrite `copy-libs` dev script in JS

--- dev/copy-libs.sh
+++
@@ -1,6 +0,0 @@
-cp node_modules/@melloware/coloris/dist/coloris.css src/lib/
-cp node_modules/@melloware/coloris/dist/esm/coloris.js src/lib/
-cp node_modules/jquery/dist/jquery.slim.min.js src/lib/
-cp node_modules/modern-normalize/modern-normalize.css src/lib/
-cp node_modules/moment/dist/moment.js src/lib/
-cp node_modules/sortablejs/modular/sortable.esm.js src/lib/
